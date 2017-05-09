ee50b835c8cac2714ea2f4f01978daa322109a78
jesse <jesse.read@gmail.com>
2017-05-09 09:49:25 +0200

    first classifier and generator, migrated from molearn project

---
+++ skmultiflow/data/synth.py
@@ -0,0 +1,29 @@
+from numpy import *
+
+def make_logical(n_tiles=1):
+    '''
+        Make a toy dataset with three labels that represent the logical functions: OR, XOR, AND (functions of the 2D input).
+    '''
+
+    pat = array([
+       # X X Y Y Y
+        [0,0,0,0,0],
+        [0,1,1,1,0],
+        [1,0,1,1,0],
+        [1,1,1,0,1]
+    ],dtype=int)
+
+    N,E = pat.shape
+    D = 2
+    L = E - D
+
+    pat2 = zeros((N,E))
+    pat2[:,0:L] = pat[:,D:E]
+    pat2[:,L:E] = pat[:,0:D]
+    pat2 = tile(pat2, (n_tiles, 1))
+    random.shuffle(pat2)
+
+    Y = array(pat2[:,0:L],dtype=float)
+    X = array(pat2[:,L:E],dtype=float)
+
+    return X, Y
