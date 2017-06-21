ab7bacc1edbe70a8a13cdd6fb31ae5de92807bdf
guimatsumoto <guilhermekmatsumoto@gmail.com>
2017-06-21 14:57:34 +0200

    Added a module to remove the evaluation dump file header

---
+++ skmultiflow/core/utils/file_scripts.py
@@ -0,0 +1,54 @@
+__author__ = 'Guilherme Matsumoto'
+
+from skmultiflow.core.BaseObject import BaseObject
+
+
+class RemoveHeader(BaseObject):
+    def __init__(self, base_file, new_file = None, ignore_char = '#'):
+        super().__init__()
+        # default values
+        self.base_file = None
+        self.new_file = None
+        self.ignore_char = None
+
+        self.configure(base_file, new_file, ignore_char)
+
+    def configure(self, base_file, new_file, ignore_char):
+        self.base_file = base_file
+        self.ignore_char = ignore_char
+        if new_file is not None:
+            self.new_file = new_file
+        else:
+            path = self.base_file.split(".")
+            base_path = path[0]
+            if len(path) > 1:
+                file_type = path[1]
+            else:
+                file_type = None
+            base_path += '_no_header'
+            self.new_file = base_path + '.' + file_type if file_type is not None else base_path
+
+
+    def get_info(self):
+        return 'Remove Header: base_file: ' + self.base_file + '  -  new_file: ' + self.new_file + \
+               '  -  ignore_char: ' + self.ignore_char
+
+    def get_class_type(self):
+        return 'file_utils'
+
+    def clean_file(self):
+        with open(self.base_file, 'r') as in_file:
+            with open(self.new_file, 'w+') as out_file:
+                for line in in_file:
+                    if (line[0] == self.ignore_char):
+                        pass
+                    else:
+                        out_file.write(line)
+
+
+if __name__ == '__main__':
+    remover = RemoveHeader('~/Desktop/PRE/scikit-multiflow/n10.csv', None, '#')
+    print('base: ' + remover.base_file)
+    print('new: ' + remover.new_file)
+    remover.clean_file()
+    print('done')
---
+++ skmultiflow/filtering/BasePreprocessing.py
@@ -0,0 +1,33 @@
+__author__ = 'Guilherme Matsumoto'
+
+from skmultiflow.core.BaseObject import BaseObject
+from abc import ABCMeta, abstractmethod
+
+
+class BasePreprocessing(BaseObject, metaclass=ABCMeta):
+    def __init__(self):
+        super().__init__()
+        pass
+
+
+    def get_class_type(self):
+        return 'transform'
+
+    @abstractmethod
+    def get_info(self):
+        pass
+
+    @abstractmethod
+    def transform(self, X):
+        pass
+
+    @abstractmethod
+    def partial_fit_transform(self, X, y=None):
+        pass
+
+    @abstractmethod
+    def partial_fit(self, X, y=None):
+        pass
+
+
+
