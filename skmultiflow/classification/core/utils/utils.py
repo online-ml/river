4a49d1c2cbab46b8c07639519e7d4c37b1ac74d1
Jacob Montiel <jacob.montiel@gmail.com>
2017-10-16 16:42:43 +0200

    Hoeffding Tree - Fixed bugs
    Former-commit-id: 6c0b55a12832bcb3992ef1cc14bd2eea534331dc

--- skmultiflow/classification/core/utils/utils.py
+++ skmultiflow/classification/core/utils/utils.py
@@ -2,7 +2,7 @@ __author__ = 'Jacob Montiel'
 
 from skmultiflow.classification.core.attribute_class_observers.attribute_class_observer import AttributeClassObserver
 
-def do_naive_bayes_prediction(X, observed_class_distribution:dict, attribute_observers:list(AttributeClassObserver)):
+def do_naive_bayes_prediction(X, observed_class_distribution:dict, attribute_observers:list):
     votes = {}
     observed_class_sum = sum(observed_class_distribution.values())
     for class_index, observed_class_val in observed_class_distribution.items():
