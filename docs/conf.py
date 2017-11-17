88f65d0e31a120f4b3269cff631b6e4c7240e886
Jacob Montiel <jacob.montiel@gmail.com>
2017-11-17 17:28:33 +0100

    Updated documentation
    Former-commit-id: 7a670fd567ef351499f98d8c14884f4ad7a37d50

--- docs/conf.py
+++ docs/conf.py
@@ -125,7 +125,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
