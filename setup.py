cb74944d90f7ed46e29cda4d42770a89a6ea8721
Jacob Montiel <jacob.montiel@gmail.com>
2017-11-20 16:52:54 +0100

    Updated install script and documentation.
    Former-commit-id: b2d0082e45478f029e04d578b73b3a9df92c6e0d

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-import os
-import numpy as np
+import os, sys
 from setuptools import setup, find_packages
 from distutils.core import Extension
 from distutils.sysconfig import get_python_inc
@@ -18,23 +17,43 @@ from distutils.sysconfig import get_python_inc
 
 """
 
+DISTNAME = 'scikit-multiflow'
+DESCRIPTION = 'Stream data and multi-label framework'
+MAINTAINER = 'Jacob MONTIEL'
+MAINTAINER_EMAIL = ''
+URL = ''
+LICENSE = '3-Clause BSD'
+VERSION = '0.1.0'
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+with open('requirements.txt') as fid:
+    INSTALL_REQUIRES = [l.strip() for l in fid.readlines() if l]
 
-nnExtension = Extension('libNearestNeighbor',
-                        include_dirs=[get_python_inc(), np.get_include()],
-                        libraries=[],
-                        library_dirs=[],
-                        extra_compile_args=['-O3'],
-                        sources=['skmultiflow/classification/lazy/libNearestNeighbors/nearestNeighbor.cpp'])
+if __name__ == "__main__":
+    try:
+        from numpy import get_include
+    except ImportError:
+        print('To install scikit-multiflow first install numpy.\n' +
+              'For example, using pip:\n' +
+              '$ pip install -U numpy')
+        sys.exit(1)
 
-setup(name="scikit-multiflow",
-      version="0.1.0",
-      author="Jacob MONTIEL",
-      # author_email="",
-      description="Setup for the scikit-multiflow package",
-      packages=find_packages(exclude=['docs', 'tests', '*.tests']),
-      long_description=read('README.md'),ext_modules=[nnExtension], install_requires=['sortedcontainers', 'numpy']
-      )
+    nnExtension = Extension('libNearestNeighbor',
+                            include_dirs=[get_python_inc(), get_include()],
+                            libraries=[],
+                            library_dirs=[],
+                            extra_compile_args=['-O3'],
+                            sources=['skmultiflow/classification/lazy/libNearestNeighbors/nearestNeighbor.cpp'])
+
+    setup(name=DISTNAME,
+          version=VERSION,
+          maintainer=MAINTAINER,
+          maintainer_email=MAINTAINER_EMAIL,
+          description=DESCRIPTION,
+          long_description=read('README.md'),
+          packages=find_packages(exclude=['docs', 'tests', '*.tests']),
+          ext_modules=[nnExtension],
+          install_requires=INSTALL_REQUIRES
+          )
