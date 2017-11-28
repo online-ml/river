e283b391d358f494367279431ec0aef7e0df63d6
Jacob Montiel <jacob.montiel@gmail.com>
2017-11-28 22:38:38 +0100

    Updated API documentation.
    Former-commit-id: a0d6615b13b3e20b84c0a0e78aac81d77730137e

--- docs/conf.py
+++ docs/conf.py
@@ -112,7 +112,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = "skmultiflow-logo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
--- docs/intro.rst
+++
@@ -1,7 +0,0 @@
-************
-Introduction
-************
-
-*scikit-multiflow* is a multi-label and stream data framework. *scikit-multiflow* is inpired in the
-`MOA <https://moa.cms.waikato.ac.nz/>`_ and `MEKA <http://meka.sourceforge.net/>`_ projects, and follows the pythonic
-philosophy of `scikit-learn <http://scikit-learn.org/stable/>`_.
