add4552ec4f3ca47e88e74a68dcacca62a56ae8f
Jacob Montiel <jacob.montiel@gmail.com>
2017-12-01 19:02:07 +0100

    Demo cleanup.
    Former-commit-id: aea6e479529d1cdae36c2fa38ad463ebf5a64188

--- skmultiflow/demos/_test_prequential.py
+++ skmultiflow/demos/_test_prequential.py
@@ -58,10 +58,11 @@ def demo(output_file=None, instances=40000):
 
     # Setup the evaluator
     eval = EvaluatePrequential(pretrain_size=200, max_instances=instances, batch_size=1, n_wait=100, max_time=1000,
-                               output_file=output_file, task_type='classification', show_plot=True, plot_options=['kappa', 'kappa_t', 'performance'])
+                               output_file=output_file, task_type='classification', show_plot=True,
+                               plot_options=['kappa', 'kappa_t', 'performance'])
 
     # Evaluate
     eval.eval(stream=stream, classifier=pipe)
 
 if __name__ == '__main__':
-    demo('log1.csv', 20000)
+    demo('log_test_prequential.csv', 20000)
