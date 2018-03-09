db224843aba1e28bdad45b12a03efb6092f1486d
Jacob Montiel <jacob.montiel@gmail.com>
2018-03-09 19:25:30 +0100

    Hoeffding Tree updates - Improve documentation adding default values. - Remove size option when creating split nodes since now we use dictionaries. - Replace key test 'in' dictionaries for 'try'.

--- skmultiflow/classification/trees/hoeffding_tree.py
+++ skmultiflow/classification/trees/hoeffding_tree.py
@@ -34,36 +34,36 @@ class HoeffdingTree(BaseClassifier):
 
     Parameters
     ----------
-    max_byte_size: int
+    max_byte_size: int (default=33554432)
         Maximum memory consumed by the tree.
-    memory_estimate_period: int
+    memory_estimate_period: int (default=1000000)
         Number of instances between memory consumption checks.
-    grace_period: int
+    grace_period: int (default=200)
         Number of instances a leaf should observe between split attempts.
-    split_criterion: string
+    split_criterion: string (default='info_gain')
         | Split criterion to use.
         | 'gini' - Gini
         | 'info_gain' - Information Gain
-    split_confidence: float
+    split_confidence: float (default=0.0000001)
         Allowed error in split decision, a value closer to 0 takes longer to decide.
-    tie_threshold: float
+    tie_threshold: float (default=0.05)
         Threshold below which a split will be forced to break ties.
-    binary_split: boolean
+    binary_split: boolean (default=False)
         If True, only allow binary splits.
-    stop_mem_management: boolean
+    stop_mem_management: boolean (default=False)
         If True, stop growing as soon as memory limit is hit.
-    remove_poor_atts: boolean
+    remove_poor_atts: boolean (default=False)
         If True, disable poor attributes.
-    no_preprune: boolean
+    no_preprune: boolean (default=False)
         If True, disable pre-pruning.
-    leaf_prediction: string
+    leaf_prediction: string (default='mc')
         | Prediction mechanism used at leafs.
         | 'mc' - Majority Class
         | 'nb' - Naive Bayes
         | 'nba' - Naive Bayes Adaptive
-    nb_threshold: int
+    nb_threshold: int (default=0)
         Number of instances a leaf should observe before allowing Naive Bayes.
-    nominal_attributes: list
+    nominal_attributes: list, optional
         List of Nominal attributes. If emtpy, then assume that all attributes are numerical.
 
     Notes
@@ -279,12 +279,10 @@ class HoeffdingTree(BaseClassifier):
             Split test.
         class_observations: dict (class_value, weight) or None
             Class observations
-        size: int
-            Number of splits.
 
         """
 
-        def __init__(self, split_test, class_observations, size=-1):
+        def __init__(self, split_test, class_observations):
             """SplitNode class constructor."""
             super().__init__(class_observations)
             self._split_test = split_test
@@ -503,9 +501,10 @@ class HoeffdingTree(BaseClassifier):
             """
             if y > len(self._observed_class_distribution) - 1:
                 return
-            if y not in self._observed_class_distribution:
-                self._observed_class_distribution[y] = 0.0
-            self._observed_class_distribution[y] += weight
+            try:
+                self._observed_class_distribution[y] += weight
+            except KeyError:
+                self._observed_class_distribution[y] = weight
 
     class ActiveLearningNode(LearningNode):
         """Learning node that supports growth.
@@ -541,9 +540,10 @@ class HoeffdingTree(BaseClassifier):
             if not self._is_initialized:
                 self._attribute_observers = [None] * len(X)
                 self._is_initialized = True
-            if y not in self._observed_class_distribution:
-                self._observed_class_distribution[y] = 0.0
-            self._observed_class_distribution[y] += weight
+            try:
+                self._observed_class_distribution[y] += weight
+            except KeyError:
+                self._observed_class_distribution[y] = weight
 
             for i in range(len(X)):
                 obs = self._attribute_observers[i]
@@ -741,10 +741,20 @@ class HoeffdingTree(BaseClassifier):
     # ====================================
     # == Hoeffding Tree implementation ===
     # ====================================
-    def __init__(self, max_byte_size=33554432, memory_estimate_period=1000000, grace_period=200,
-                 split_criterion='info_gain', split_confidence=0.0000001, tie_threshold=0.05, binary_split=False,
-                 stop_mem_management=False, remove_poor_atts=False, no_preprune=False, leaf_prediction='mc',
-                 nb_threshold=0, nominal_attributes=None):
+    def __init__(self,
+                 max_byte_size=33554432,
+                 memory_estimate_period=1000000,
+                 grace_period=200,
+                 split_criterion='info_gain',
+                 split_confidence=0.0000001,
+                 tie_threshold=0.05,
+                 binary_split=False,
+                 stop_mem_management=False,
+                 remove_poor_atts=False,
+                 no_preprune=False,
+                 leaf_prediction='mc',
+                 nb_threshold=0,
+                 nominal_attributes=None):
         """HoeffdingTree class constructor."""
         super().__init__()
         self.max_byte_size = max_byte_size
@@ -1165,9 +1175,9 @@ class HoeffdingTree(BaseClassifier):
         """
         return np.sqrt((range_val * range_val * np.log(1.0 / confidence)) / (2.0 * n))
 
-    def new_split_node(self, split_test, class_observations, size=-1):
+    def new_split_node(self, split_test, class_observations):
         """Create a new split node."""
-        return self.SplitNode(split_test, class_observations, size)
+        return self.SplitNode(split_test, class_observations)
 
     def _attempt_to_split(self, node: ActiveLearningNode, parent: SplitNode, parent_idx: int):
         """Attempt to split a node.
@@ -1238,8 +1248,7 @@ class HoeffdingTree(BaseClassifier):
                     self._deactivate_learning_node(node, parent, parent_idx)
                 else:
                     new_split = self.new_split_node(split_decision.split_test,
-                                                    node.get_observed_class_distribution(),
-                                                    split_decision.num_splits())
+                                                    node.get_observed_class_distribution())
                     for i in range(split_decision.num_splits()):
                         new_child = self._new_learning_node(split_decision.resulting_class_distribution_from_split(i))
                         new_split.set_child(i, new_child)
