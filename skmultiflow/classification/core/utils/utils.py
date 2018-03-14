0e11337833455553aac123cde393ce75ebcacf3a
Jacob Montiel <jacob.montiel@gmail.com>
2018-03-14 15:10:17 +0100

    Adds support for the case where there are less attributes observes than the number of total attributes.
    Former-commit-id: c3fc7ce8b1f11d728d1e8631d35ef66630163d25

--- skmultiflow/classification/core/utils/utils.py
+++ skmultiflow/classification/core/utils/utils.py
@@ -9,9 +9,12 @@ def do_naive_bayes_prediction(X, observed_class_distribution: dict, attribute_ob
     observed_class_sum = sum(observed_class_distribution.values())
     for class_index, observed_class_val in observed_class_distribution.items():
         votes[class_index] = observed_class_val / observed_class_sum
-        if attribute_observers != []:
+        if attribute_observers:
             for att_idx in range(len(X)):
-                obs = attribute_observers[att_idx]
+                try:
+                    obs = attribute_observers[att_idx]
+                except IndexError:
+                    obs = None
                 if obs is not None:
                     votes[class_index] *= obs.probability_of_attribute_value_given_class(X[att_idx], class_index)
     return votes
