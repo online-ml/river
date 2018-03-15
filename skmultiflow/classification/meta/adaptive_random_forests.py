8ddb1f78383b69dd88a8f00d4b0a69fd4d2ef9c1
Jacob Montiel <jacob.montiel@gmail.com>
2018-03-15 20:21:35 +0100

    Minor updates. - Improve documentation. - Remove redundant code. - Improve PEP8 compliance.
    Former-commit-id: 988129bfba353dfc802641df2976716ca1ccd7a7

--- skmultiflow/classification/meta/adaptive_random_forests.py
+++ skmultiflow/classification/meta/adaptive_random_forests.py
@@ -1,6 +1,8 @@
 __author__ = 'Anderson Carlos Ferreira da Silva'
 
+from copy import deepcopy
 from skmultiflow.core.base_object import BaseObject
+from skmultiflow.classification.core.driftdetection.base_drift_detector import BaseDriftDetector
 from skmultiflow.classification.trees.hoeffding_tree import *
 from skmultiflow.classification.core.driftdetection.adwin import ADWIN
 from skmultiflow.classification.trees.arf_hoeffding_tree import ARFHoeffdingTree
@@ -17,7 +19,34 @@ class AdaptiveRandomForest(BaseClassifier):
 
         Parameters
         ----------
-        # TODO
+        nb_ensemble: int (default=10)
+            Number of trees oin the ensemble
+        feature_mode: string (default='sqrt')
+            / How the subspace size value is interpreted to calculate the number of attributes.
+            / 'sqrt' - use the square root value ```sqrt(M)+1```
+            / 'sqrt_inv' - ```M-(sqrt(M)+1)```
+            / 'percentage' - Use as percentage
+            / '' - use subspace value as the number of attributes to use.
+        subspace_size: int or float (default=2)
+            Number of attributes per subset for each node split.
+        disable_background_learner: bool (default=False)
+            Background learner option.
+        disable_drift_detection: bool (default=False)
+            Drift detection option.
+        disable_weighted_vote: bool (default=False)
+            Weighted vote option.
+        lambda_value: int (default=6)
+            The lambda value for bagging (lambda=6 corresponds to Leverage Bagging).
+        evaluator_method_metric: string (default='acc')
+            / Metric used to track tree performance.
+            / 'acc' - Accuracy
+        drift_detection_method: BaseDriftDetector default(ADWIN(0.001))
+            Drift Detection method
+        warning_detection_method: BaseDriftDetector default(ADWIN(0.01))
+            Warning Detection method
+        nominal_attributes: list, optional
+            List of Nominal attributes. If emtpy, then assume that all attributes are numerical.
+
 
         Notes
         -----
@@ -41,37 +70,31 @@ class AdaptiveRandomForest(BaseClassifier):
     def __init__(self,
                  nb_ensemble=10,
                  feature_mode='sqrt',
-                 nb_attributes=2,
+                 subspace_size=2,
                  disable_background_learner=False,
                  disable_drift_detection=False,
                  disable_weighted_vote=False,
                  lambda_value=6,
-                 evaluator_method=None,
-                 drift_detection_method=ADWIN,
-                 warning_detection_method=ADWIN,
-                 drift_delta=0.001,
-                 warning_delta=0.01,
+                 evaluator_method_metric='acc',
+                 drift_detection_method: BaseDriftDetector=ADWIN(0.001),
+                 warning_detection_method: BaseDriftDetector=ADWIN(0.01),
                  nominal_attributes=None):
         """AdaptiveRandomForest class constructor."""
         super().__init__()          
         self.nb_ensemble = nb_ensemble        
         self.feature_mode = feature_mode
-        self.total_attributes = nb_attributes
+        self.subspace_size = subspace_size
         self.disable_background_learner = disable_background_learner   
         self.disable_drift_detection = disable_drift_detection        
         self.disable_weighted_vote = disable_weighted_vote
         self.lambda_value = lambda_value
-        if evaluator_method is None:
-            self.evaluator_method = ARFBaseClassifierEvaluator
         self.drift_detection_method = drift_detection_method
         self.warning_detection_method = warning_detection_method
         self.instances_seen = 0
         self._train_weight_seen_by_model = 0.0
-        self.nb_attributes = None
         self.ensemble = None
-        self.warning_delta = warning_delta
-        self.drift_delta = drift_delta
         self.nominal_attributes = nominal_attributes
+        self._evaluator_method = ARFBaseClassifierEvaluator  # TODO use skmultiflow evaluator
 
     def fit(self, X, y, classes=None, weight=None):
         raise NotImplementedError
@@ -135,10 +158,10 @@ class AdaptiveRandomForest(BaseClassifier):
     def reset(self):        
         """Reset ARF."""
         self.ensemble = None
-        self.nb_attributes = 0
+        self.subspace_size = 0
         self.instances_seen = 0
         self._train_weight_seen_by_model = 0.0
-        self.evaluator_method = ARFBaseClassifierEvaluator
+        self._evaluator_method = ARFBaseClassifierEvaluator
         
     def score(self, X, y):
         raise NotImplementedError
@@ -160,46 +183,42 @@ class AdaptiveRandomForest(BaseClassifier):
         
     def init_ensemble(self, X):
         self.ensemble = [None] * self.nb_ensemble
-        
-        self.nb_attributes = self.total_attributes
-        
+
         # The m (total number of attributes) depends on:
         _, n = get_dimensions(X)
         
         if self.feature_mode == FEATURE_MODE_SQRT:
-            self.nb_attributes = int(round(math.sqrt(n)) + 1)            
+            self.subspace_size = int(round(math.sqrt(n)) + 1)
         elif self.feature_mode == FEATURE_MODE_SQRT_INV:
-            self.nb_attributes = n - int(round(math.sqrt(n) + 1))
+            self.subspace_size = n - int(round(math.sqrt(n) + 1))
         elif self.feature_mode == FEATURE_MODE_PERCENTAGE:            
-            percent = (100 + self.nb_attributes) / 100.0 if self.nb_attributes < 0 else self.nb_attributes / 100.0
-            self.nb_attributes = int(round(n * percent))
+            percent = (100 + self.subspace_size) / 100.0 if self.subspace_size < 0 else self.subspace_size / 100.0
+            self.subspace_size = int(round(n * percent))
             
-        # Notice that if the selected feature_mode was FEATURE_MODE_M then nothing is performed,
-        # still it is necessary to check (and adjusted) for when a negative value was used.
+        # Notice that if the selected feature_mode is FEATURE_MODE_M then nothing is performed,
+        # still it is necessary to check (and adjusted) for when a negative value is used.
         
         # m is negative, use size(features) + -m
-        if self.nb_attributes < 0:
-            self.nb_attributes += n
+        if self.subspace_size < 0:
+            self.subspace_size += n
         # Other sanity checks to avoid runtime errors.
-        # m <= 0 (m can be negative if nb_attributes was negative and abs(m) > n), then use m = 1
-        if self.nb_attributes <= 0:
-            self.nb_attributes = 1
-        # m > n, then it should use n
-        if self.nb_attributes > n:
-            self.nb_attributes = n
+        # m <= 0 (m can be negative if subspace_size is negative and abs(m) > n), then use m = 1
+        if self.subspace_size <= 0:
+            self.subspace_size = 1
+        # m > n, then use n
+        if self.subspace_size > n:
+            self.subspace_size = n
                                
         for i in range(self.nb_ensemble):            
             self.ensemble[i] = ARFBaseLearner(i,
                                               ARFHoeffdingTree(nominal_attributes=self.nominal_attributes,
-                                                               nb_attributes=self.nb_attributes),
+                                                               subspace_size=self.subspace_size),
                                               self.instances_seen,
                                               not self.disable_background_learner,
                                               not self.disable_drift_detection,
-                                              self.evaluator_method,
+                                              self._evaluator_method,
                                               self.drift_detection_method,
                                               self.warning_detection_method,
-                                              self.drift_delta,
-                                              self.warning_delta,
                                               False)            
                     
     @staticmethod
@@ -208,21 +227,44 @@ class AdaptiveRandomForest(BaseClassifier):
 
 
 class ARFBaseLearner(BaseObject):
-    """
+    """ARF Base Learner class.
+
+    Parameters
+    ----------
+    index_original: int
+        Tree index within the ensemble
+    classifier: ARFHoeffdingTree
+        Tree classifier
+    instances_seen: int
+        Number of instances seen by the tree
+    use_background_learner: bool
+        True if a background learner shall be used.
+    use_drift_detector: bool
+        True if drift detection shall be used.
+    evaluator_method: ARFBaseClassifierEvaluator  # TODO change to skmultiflow evaluator
+        Evaluator for classifier performance
+    drift_detection_method: BaseDriftDetector
+        Drift Detection method
+    warning_detection_method: BaseDriftDetector
+        Warning Detection method
+    is_background_learner: bool
+        True if the tree is a background learner.
+
+    Notes
+    -----
     Inner class that represents a single tree member of the forest.
-    Contains analysis information, such as the numberOfDriftsDetected,
+    Contains analysis information, such as the numberOfDriftsDetected.
+
     """
     def __init__(self,
                  index_original,
-                 classifier,
+                 classifier: ARFHoeffdingTree,
                  instances_seen,
                  use_background_learner,
                  use_drift_detector,
                  evaluator_method,
-                 drift_detection_method,
-                 warning_detection_method,
-                 drift_delta,
-                 warning_delta,
+                 drift_detection_method: BaseDriftDetector,
+                 warning_detection_method: BaseDriftDetector,
                  is_background_learner):
         self.index_original = index_original
         self.classifier = classifier 
@@ -233,10 +275,8 @@ class ARFBaseLearner(BaseObject):
         self.evaluator_method = evaluator_method
 
         # Drift and warning
-        self.drift_detection_method = drift_detection_method
-        self.warning_detection_method = warning_detection_method
-        self.drift_delta = drift_delta
-        self.warning_delta = warning_delta
+        self.drift_detection_method = deepcopy(drift_detection_method)
+        self.warning_detection_method = deepcopy(warning_detection_method)
 
         self.last_drift_on = 0
         self.last_warning_on = 0
@@ -248,13 +288,14 @@ class ARFBaseLearner(BaseObject):
         self.background_learner = None
         
         self.evaluator = evaluator_method()
+        # TODO add code to support the selection of evaluation metric
 
         # Initialize drift and warning detectors
         if use_drift_detector:
-            self.drift_detection = drift_detection_method(self.drift_delta)
+            self.drift_detection = deepcopy(drift_detection_method)
 
         if use_background_learner:
-            self.warning_detection = warning_detection_method(self.warning_delta)
+            self.warning_detection = deepcopy(warning_detection_method)
             
     def reset(self, instances_seen):
         if self.use_background_learner and self.background_learner is not None:
@@ -267,7 +308,7 @@ class ARFBaseLearner(BaseObject):
         else:
             self.classifier.reset()
             self.created_on = instances_seen
-            self.drift_detection = self.drift_detection_method(self.drift_delta)
+            self.drift_detection.reset()  # TODO check
         self.evaluator = self.evaluator_method()
 
     def partial_fit(self, X, y, weight, instances_seen):
@@ -276,6 +317,7 @@ class ARFBaseLearner(BaseObject):
         if self.background_learner:
             self.background_learner.classifier.partial_fit(X, y, INSTANCE_WEIGHT)
 
+        correctly_classifies = False
         if self.use_drift_detector and not self.is_background_learner:
             correctly_classifies = self.classifier.predict(X) == y
             # Check for warning only if use_background_learner is active
@@ -289,16 +331,18 @@ class ARFBaseLearner(BaseObject):
                     background_learner = self.classifier.copy()
                     background_learner.reset() 
                     # Create a new background learner object
-                    self.background_learner = ARFBaseLearner(self.index_original, background_learner, instances_seen,
-                                                             self.use_background_learner, self.use_drift_detector,
-                                                             self.evaluator_method, self.drift_detection_method,
+                    self.background_learner = ARFBaseLearner(self.index_original,
+                                                             background_learner,
+                                                             instances_seen,
+                                                             self.use_background_learner,
+                                                             self.use_drift_detector,
+                                                             self.evaluator_method,
+                                                             self.drift_detection_method,
                                                              self.warning_detection_method,
-                                                             self.drift_delta, self.warning_delta,
                                                              True)
-                    """Update the warning detection object for the current object 
-                    (this effectively resets changes made to the object while it was still a bkg learner). 
-                    """
-                    self.warning_detection = self.warning_detection_method(self.warning_delta)
+                    # Update the warning detection object for the current object
+                    # (this effectively resets changes made to the object while it was still a bkg learner).
+                    self.warning_detection.reset()  # TODO check
 
         # Update the drift detection
         self.drift_detection.add_element(int(not correctly_classifies))
