b86f892603b15c17aa12e71090eed976d6838b26
jesse <jesse.read@gmail.com>
2018-04-03 15:46:34 +0200

    split into three parameterized sub-demos

--- skmultiflow/demos/_test_file_stream_multiple_cfier.py
+++ skmultiflow/demos/_test_file_stream_multiple_cfier.py
@@ -1,6 +1,5 @@
 import time
 import numpy as np
-from copy import copy
 
 from skmultiflow.classification.trees.hoeffding_tree import HoeffdingTree
 from skmultiflow.classification.lazy.sam_knn import SAMKNN
@@ -11,20 +10,30 @@ from skmultiflow.evaluation.evaluate_prequential import EvaluatePrequential
 from skmultiflow.options.file_option import FileOption
 from skmultiflow.data.file_stream import FileStream
 
-def demo(): 
-
-    # The classifier we will use (other options: SAMKNN, LeverageBagging, SGD)
-    h = [HoeffdingTree(), SAMKNN(), LeverageBagging(), SGDClassifier()]
-
+def demo_parameterized(h, dset="sea_stream.csv", show_plot=True): 
     # Setup Stream
-    opt = FileOption("FILE", "OPT_NAME", "../datasets/sea_stream.csv", "CSV", False)
+    opt = FileOption("FILE", "OPT_NAME", "../datasets/"+dset, "CSV", False)
     stream = FileStream(opt, -1, 1)
     stream.prepare_for_use()
 
     # For each classifier, e...
     T_init = 100
-    eval = EvaluatePrequential(pretrain_size=T_init, output_file='output.csv', max_instances=10000, batch_size=1, n_wait=1000, task_type='classification', show_plot=False, plot_options=['performance'])
+    eval = EvaluatePrequential(pretrain_size=T_init, output_file='output.csv', max_instances=10000, batch_size=1, n_wait=1000, task_type='classification', show_plot=show_plot, plot_options=['performance'])
     eval.eval(stream=stream, classifier=h)
+
+def demo():
+
+    # The classifier we will use (other options: SAMKNN, LeverageBagging, SGD)
+    h = [HoeffdingTree(), SAMKNN(), LeverageBagging(), SGDClassifier()]
+
+    # Demo 1 -- plot should not fail
+    demo_parameterized(h)
+
+    # Demo 2 -- csv output should look nice
+    demo_parameterized(h, "sea_stream.csv", False)
+
+    # Demo 3 -- should not give "'NoneType' object is not iterable" error
+    demo_parameterized(h, "covtype.csv", False)
 
 if __name__ == '__main__':
     demo()
