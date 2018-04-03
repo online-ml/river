bfff4487f2764f603b18fc54e14fd168179e0e30
jesse <jesse.read@gmail.com>
2018-04-03 11:45:41 +0200

    new tests/demos

---
+++ skmultiflow/demos/_test_file_stream.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from skmultiflow.classification.trees.hoeffding_tree import HoeffdingTree
+from skmultiflow.classification.lazy.sam_knn import SAMKNN
+from skmultiflow.classification.meta.leverage_bagging import LeverageBagging
+from sklearn.linear_model.stochastic_gradient import SGDClassifier
+
+from skmultiflow.evaluation.evaluate_prequential import EvaluatePrequential
+from skmultiflow.options.file_option import FileOption
+from skmultiflow.data.file_stream import FileStream
+
+def demo(): 
+
+    # The classifier we will use (other options: SAMKNN, LeverageBagging, SGD)
+    h = HoeffdingTree()
+
+    # Setup Stream
+    opt = FileOption("FILE", "OPT_NAME", "../datasets/sea_stream.csv", "CSV", False)
+    stream = FileStream(opt, -1, 1)
+    stream.prepare_for_use()
+
+    T_init = 100
+    eval = EvaluatePrequential(pretrain_size=T_init, output_file='output.csv', max_instances=10000, batch_size=1, n_wait=1000, task_type='classification', show_plot=True, plot_options=['performance'])
+    eval.eval(stream=stream, classifier=h)
+
+if __name__ == '__main__':
+    demo()
