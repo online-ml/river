b641b1a41c40a410c4b12d36c067186e3c719d18
Jacob Montiel <jacob.montiel@gmail.com>
2018-04-04 18:53:37 +0200

    Stream generator test: Random RBF drift - Minor changes for PEP8 compliance and consistency - Bug fixes
    Former-commit-id: c766ac74073f218ec8fda988ebd9ddb1af4b482c

--- src/skmultiflow/data/generators/random_rbf_generator_drift.py
+++ src/skmultiflow/data/generators/random_rbf_generator_drift.py
@@ -42,7 +42,7 @@ class RandomRBFGeneratorDrift(RandomRBFGenerator, BaseObject):
     Examples
     --------
     >>> # Imports
-    >>> from src.skmultiflow.data import RandomRBFGeneratorDrift
+    >>> from skmultiflow.data.generators.random_rbf_generator_drift import RandomRBFGeneratorDrift
     >>> # Setting up the stream
     >>> stream = RandomRBFGeneratorDrift(model_seed=99, instance_seed = 50, num_classes = 4, num_att = 10, 
     ... num_centroids = 50, change_speed=0.87, num_drift_centroids=50)
@@ -72,7 +72,8 @@ class RandomRBFGeneratorDrift(RandomRBFGenerator, BaseObject):
        [ 0.27969972, -0.06563579,  0.02834469,  0.05250523,  0.52713213,
          0.73472713,  0.15381198, -0.07735765,  0.9792027 ,  0.92673772],
        [ 0.52641196,  0.3009952 ,  0.56104759,  0.40478501,  0.63097374,
-         0.3797032 , -0.00446842,  0.52913688,  0.24908855,  0.22779074]]), array([ 3.,  3.,  3.,  2.,  3.,  2.,  0.,  2.,  0.,  2.]))
+         0.3797032 , -0.00446842,  0.52913688,  0.24908855,  0.22779074]]),
+        array([ 3.,  3.,  3.,  2.,  3.,  2.,  0.,  2.,  0.,  2.]))
     >>> # Generators will have infinite remaining instances, so it returns -1
     >>> stream.estimated_remaining_instances()
     -1
@@ -84,10 +85,14 @@ class RandomRBFGeneratorDrift(RandomRBFGenerator, BaseObject):
     def __init__(self, model_seed=21, instance_seed=5, num_classes=2, num_att=10, num_centroids=50,
                  change_speed=0.0, num_drift_centroids=50):
         super().__init__(model_seed, instance_seed, num_classes, num_att, num_centroids)
-        #default values
+
+        # Default values
         self.change_speed = change_speed
         self.num_drift_centroids = num_drift_centroids
         self.centroid_speed = None
+
+    def get_plot_name(self):
+        return "Random RBF Generator with drift - {} class labels".format(self.num_classes)
 
     def next_instance(self, batch_size=1):
         """ next_instance
@@ -113,21 +118,23 @@ class RandomRBFGeneratorDrift(RandomRBFGenerator, BaseObject):
         """
         data = np.zeros([batch_size, self.num_numerical_attributes + 1])
         for k in range(batch_size):
-            len = self.num_drift_centroids
-            if (len > self.num_centroids):
-                len = self.num_centroids
+            num_drift_centroids = self.num_drift_centroids
+            if num_drift_centroids > self.num_centroids:
+                num_drift_centroids = self.num_centroids
 
-            for i in range(len):
+            for i in range(num_drift_centroids):
                 for j in range(self.num_numerical_attributes):
                     self.centroids[i].centre[j] += self.centroid_speed[i][j] * self.change_speed
 
-                    if ((self.centroids[i].centre[j] > 1) | (self.centroids[i].centre[j] < 0)):
+                    if (self.centroids[i].centre[j] > 1) or (self.centroids[i].centre[j] < 0):
                         self.centroids[i].centre[j] = 1 if (self.centroids[i].centre[j] > 1) else 0
                         self.centroid_speed[i][j] = -self.centroid_speed[i][j]
             X, y = super().next_instance(1)
             data[k, :] = np.concatenate((X[0], y[0]))
 
-        return (data[:, :self.num_numerical_attributes], data[:, self.num_numerical_attributes:].flatten())
+        self.current_instance_x = data[:, :self.num_numerical_attributes]
+        self.current_instance_y = data[:, self.num_numerical_attributes:].flatten()
+        return self.current_instance_x, self.current_instance_y
 
     def generate_centroids(self):
         """ generate_centroids
@@ -144,12 +151,12 @@ class RandomRBFGeneratorDrift(RandomRBFGenerator, BaseObject):
         super().generate_centroids()
         model_random = np.random
         model_random.seed(self.model_seed)
-        len = self.num_drift_centroids
+        num_drift_centroids = self.num_drift_centroids
         self.centroid_speed = []
-        if (len > self.num_centroids):
-            len = self.num_centroids
+        if num_drift_centroids > self.num_centroids:
+            num_drift_centroids = self.num_centroids
 
-        for i in range(len):
+        for i in range(num_drift_centroids):
             rand_speed = []
             norm_speed = 0.0
 
@@ -182,11 +189,3 @@ class RandomRBFGeneratorDrift(RandomRBFGenerator, BaseObject):
 
     def get_num_targeting_tasks(self):
         return 1
-
-if __name__ == '__main__':
-    stream = RandomRBFGeneratorDrift(change_speed=0.02, num_drift_centroids=50)
-    stream.prepare_for_use()
-
-    X, y = stream.next_instance(4)
-    print(X)
-    print(y)
