0460b9d82b5e76d1a0ecbd4dde8753ffcfc6d82c
Jacob Montiel <jacob.montiel@gmail.com>
2018-04-04 21:03:30 +0200

    Stream generator test: Multilabel - Minor changes for PEP8 compliance and consistency - Bug fixes

--- tests/data/generators/test_multilabel_generator.py
+++ tests/data/generators/test_multilabel_generator.py
@@ -8,13 +8,16 @@ def test_multilabel_generator():
     assert stream.estimated_remaining_instances() == 100
 
     expected_header = ['att_num_0', 'att_num_1', 'att_num_2', 'att_num_3', 'att_num_4',
-                       'att_num_5', 'att_num_6', 'att_num_7', 'att_num_8', 'att_num_9']
-    assert stream.get_attributes_header() == None
+                       'att_num_5', 'att_num_6', 'att_num_7', 'att_num_8', 'att_num_9',
+                       'att_num_10', 'att_num_11', 'att_num_12', 'att_num_13', 'att_num_14',
+                       'att_num_15', 'att_num_16', 'att_num_17', 'att_num_18', 'att_num_19']
+    assert stream.get_attributes_header() == expected_header
 
     expected_classes = [0, 1]
     assert stream.get_classes() == expected_classes
 
-    assert stream.get_classes_header() == None  # ['class']
+    expected_header = ['label_0', 'label_1', 'label_2', 'label_3',]
+    assert stream.get_classes_header() == expected_header
 
     assert stream.get_num_attributes() == 20
 
