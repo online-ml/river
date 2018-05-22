feecc42c060dedefad564f9d236e8b50ab157ab4
darkmyter <bader.d24@gmail.com>
2018-05-22 10:56:06 +0200

    Added support for dataset to stream generator
    Former-commit-id: 4d1db4906551f7e380b8706266bad266cb44d30f

---
+++ src/skmultiflow/data/dataset_stream.py
@@ -0,0 +1,193 @@
+import pandas as pd
+import numpy as np
+from skmultiflow.data.base_stream import Stream
+
+
+class DatasetStream(Stream):
+    """ DatasetStream
+
+    A stream generated from the entries of a dataset ( numpy array or pandas
+    DataFrame).
+
+    The stream is able to provide, as requested, a number of samples, in
+    a way that old samples cannot be accessed in a later time. This is done
+    so that a stream context can be correctly simulated.
+
+    Parameters
+    ----------
+    raw_data:
+        The dataset.
+    target_idx: int, optional (default=-1)
+        The column index from which the targets start.
+
+    n_targets: int, optional (default=1)
+        The number of targets.
+
+    cat_features_idx: list, optional (default=None)
+        A list of indices corresponding to the location of categorical features.
+    """
+
+    CLASSIFICATION = 'classification'
+    REGRESSION = 'regression'
+
+    def __init__(self, raw_data, target_idx=-1, n_targets=1, cat_features_idx=None):
+        super().__init__()
+        self.X = None
+        self.y = None
+        self.cat_features_idx = [] if cat_features_idx is None else cat_features_idx
+        self.n_targets = n_targets
+        self.target_idx = target_idx
+        self.task_type = None
+        self.n_classes = 0
+        self.datatype = type(raw_data)
+        self.raw_data = raw_data
+
+        self.__configure()
+
+    def __configure(self):
+        if not (self.datatype == type(pd.DataFrame())) and not(self.datatype == type(np.array([]))):
+            raise ValueError('Unsupported format:', type(self.raw_data), ' Supported formats: Pandas '
+                                                                         'DataFrame and numpy array')
+        else:
+            self.raw_data = pd.DataFrame(self.raw_data)
+
+    def prepare_for_use(self):
+        """ prepare_for_use
+
+        Prepares the stream for use. This functions should always be
+        called after the stream initialization.
+
+        """
+        self._load_data()
+        del self.raw_data
+        self.restart()
+
+    def _load_data(self):
+
+        rows, cols = self.raw_data.shape
+        self.n_samples = rows
+        labels = self.raw_data.columns.values.tolist()
+
+        if (self.target_idx + self.n_targets) == cols or (self.target_idx + self.n_targets) == 0:
+            # Take everything to the right of target_idx
+            self.y = self.raw_data.iloc[:, self.target_idx:].as_matrix()
+            self.outputs_labels = self.raw_data.iloc[:, self.target_idx:].columns.values.tolist()
+        else:
+            # Take only n_targets columns to the right of target_idx, use the rest as features
+            self.y = self.raw_data.iloc[:, self.target_idx:self.target_idx + self.n_targets].as_matrix()
+            self.outputs_labels = labels[self.target_idx:self.target_idx + self.n_targets]
+
+        self.X = self.raw_data.drop(self.outputs_labels, axis=1).as_matrix()
+        self.features_labels = self.raw_data.drop(self.outputs_labels, axis=1).columns.values.tolist()
+
+        _, self.n_features = self.X.shape
+        if self.cat_features_idx:
+            if max(self.cat_features_idx) < self.n_features:
+                self.n_cat_features = len(self.cat_features_idx)
+            else:
+                raise IndexError('Categorical feature index in {} '
+                                 'exceeds n_features {}'.format(self.cat_features_idx, self.n_features))
+        self.n_num_features = self.n_features - self.n_cat_features
+
+        if self.y.dtype == np.integer:
+            self.task_type = self.CLASSIFICATION
+            self.n_classes = len(np.unique(self.y))
+        else:
+            self.task_type = self.REGRESSION
+
+    def restart(self):
+        """ restart
+
+        Restarts the stream's sample feeding, while keeping all of its
+        parameters.
+
+        It basically server the purpose of reinitializing the stream to
+        its initial state.
+
+        """
+        self.sample_idx = 0
+        self.current_sample_x = None
+        self.current_sample_y = None
+
+    def is_restartable(self):
+        return True
+
+    def next_sample(self, batch_size=1):
+        """ next_sample
+
+        If there is enough instances to supply at least batch_size samples, those
+        are returned. If there aren't a tuple of (None, None) is returned.
+
+        Parameters
+        ----------
+        batch_size: int
+            The number of instances to return.
+
+        Returns
+        -------
+        tuple or tuple list
+            Returns the next batch_size instances.
+            For general purposes the return can be treated as a numpy.ndarray.
+
+        """
+        self.sample_idx += batch_size
+        try:
+
+            self.current_sample_x = self.X[self.sample_idx - batch_size:self.sample_idx, :]
+            self.current_sample_y = self.y[self.sample_idx - batch_size:self.sample_idx, :]
+            if self.n_targets < 2:
+                self.current_sample_y = self.current_sample_y.flatten()
+
+        except IndexError:
+            self.current_sample_x = None
+            self.current_sample_y = None
+        return self.current_sample_x, self.current_sample_y
+
+    def has_more_samples(self):
+        return (self.n_samples - self.sample_idx) > 0
+
+    def n_remaining_samples(self):
+        return self.n_samples - self.sample_idx
+
+    def print_df(self):
+        print(self.X)
+        print(self.y)
+
+    def get_n_features(self):
+        return self.n_features
+
+    def get_n_cat_features(self):
+        return self.n_cat_features
+
+    def get_n_num_features(self):
+        return self.n_num_features
+
+    def get_n_targets(self):
+        return self.n_targets
+
+    def get_feature_names(self):
+        return self.features_labels
+
+    def get_target_names(self):
+        return self.outputs_labels
+
+    def last_sample(self):
+        return self.current_sample_x, self.current_sample_y
+
+    def get_name(self):
+        if self.task_type == self.CLASSIFICATION:
+            return "{} target(s), {} classes".format(self.n_targets, self.n_classes)
+        elif self.task_type == self.REGRESSION:
+            return "{} target(s)".format(self.n_targets)
+
+    def get_targets(self):
+        if self.task_type == 'classification':
+            if self.n_targets == 1:
+                return np.unique(self.y).tolist()
+            else:
+                return [np.unique(self.y[:, i]).tolist() for i in range(self.n_targets)]
+        elif self.task_type == self.REGRESSION:
+            return [float] * self.n_targets
+
+    def get_info(self):
+        return 'Dataset Stream:' + '  -  n_targets: ' + str(self.n_targets)
---
+++ tests/data/test_dataset_stream.py
@@ -0,0 +1,58 @@
+import os
+import numpy as np
+import pandas as pd
+from skmultiflow.data.dataset_stream import DatasetStream
+
+
+def test_dataset_stream(test_path, package_path):
+    test_file = os.path.join(package_path, 'src/skmultiflow/datasets/sea_stream.csv')
+    raw_data = pd.read_csv(test_file)
+    stream = DatasetStream(raw_data)
+    stream.prepare_for_use()
+
+    assert stream.n_remaining_samples() == 40000
+
+    expected_names = ['attrib1', 'attrib2', 'attrib3']
+    assert stream.get_feature_names() == expected_names
+
+    expected_targets = [0, 1]
+    assert stream.get_targets() == expected_targets
+
+    assert stream.get_target_names() == ['class']
+
+    assert stream.get_n_features() == 3
+
+    assert stream.get_n_cat_features() == 0
+
+    assert stream.get_n_num_features() == 3
+
+    assert stream.get_n_targets() == 1
+
+    assert stream.get_name() == '1 target(s), 2 classes'
+
+    assert stream.has_more_samples() is True
+
+    assert stream.is_restartable() is True
+
+    # Load test data corresponding to first 10 instances
+    test_file = os.path.join(test_path, 'sea_stream.npz')
+    data = np.load(test_file)
+    X_expected = data['X']
+    y_expected = data['y']
+
+    X, y = stream.next_sample()
+    assert np.alltrue(X[0] == X_expected[0])
+    assert np.alltrue(y[0] == y_expected[0])
+
+    X, y = stream.last_sample()
+    assert np.alltrue(X[0] == X_expected[0])
+    assert np.alltrue(y[0] == y_expected[0])
+
+    stream.restart()
+    X, y = stream.next_sample(10)
+    assert np.alltrue(X == X_expected)
+    assert np.alltrue(y == y_expected)
+
+    assert stream.get_n_targets() == np.array(y).ndim
+
+    assert stream.get_n_features() == X.shape[1]
