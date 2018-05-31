9393d09320a72b50eb812425f8145e5d0364bcaa
darkmyter <bader.d24@gmail.com>
2018-05-31 13:14:12 +0200

    Update of the data_stream Generator. Now X and  y can be passed separately or joined. Tests and documentation included.
    Former-commit-id: acc8752b8b8d304db3472f1cdeccefad5a95fb48

---
+++ tests/data/test_data_stream.py
@@ -0,0 +1,121 @@
+import os
+import numpy as np
+import pandas as pd
+from skmultiflow.data.data_stream import DataStream
+
+
+def test_data_stream(test_path, package_path):
+    test_file = os.path.join(package_path, 'src/skmultiflow/datasets/sea_stream.csv')
+    raw_data = pd.read_csv(test_file)
+    stream = DataStream(raw_data)
+
+    assert stream._flag == False
+
+    stream.prepare_for_use()
+
+    assert stream.n_remaining_samples() == 40000
+
+    expected_names = ['attrib1', 'attrib2', 'attrib3']
+    assert stream.feature_names == expected_names
+
+    expected_targets = [0, 1]
+    assert stream.target_values == expected_targets
+
+    assert stream.target_names == ['class']
+
+    assert stream.n_features == 3
+
+    assert stream.n_cat_features == 0
+
+    assert stream.n_num_features == 3
+
+    assert stream.n_targets == 1
+
+    assert stream.get_data_info() == '1 target(s), 2 target_values'
+
+    assert stream.has_more_samples() is True
+
+    assert stream.is_restartable() is True
+
+    # Load test data corresponding to first 10 instances
+    test_file = os.path.join(test_path, 'sea_stream.npz')
+    data = np.load(test_file)
+    X_expected = data['X']
+    y_expected = data['y']
+
+    X, y = stream.next_sample()
+    assert np.alltrue(X[0] == X_expected[0])
+    assert np.alltrue(y[0] == y_expected[0])
+
+    X, y = stream.last_sample()
+    assert np.alltrue(X[0] == X_expected[0])
+    assert np.alltrue(y[0] == y_expected[0])
+
+    stream.restart()
+    X, y = stream.next_sample(10)
+    assert np.alltrue(X == X_expected)
+    assert np.alltrue(y == y_expected)
+
+    assert stream.n_targets == np.array(y).ndim
+
+    assert stream.n_features == X.shape[1]
+
+
+def test_data_stream_X_y(test_path, package_path):
+    test_file = os.path.join(package_path, 'src/skmultiflow/datasets/sea_stream.csv')
+    raw_data = pd.read_csv(test_file)
+    y = raw_data.iloc[:, -1:]
+    X = raw_data.iloc[:, :-1]
+    stream = DataStream(X, y)
+
+    assert stream._flag == True
+
+    stream.prepare_for_use()
+
+    assert stream.n_remaining_samples() == 40000
+
+    expected_names = ['attrib1', 'attrib2', 'attrib3']
+    assert stream.feature_names == expected_names
+
+    expected_targets = [0, 1]
+    assert stream.target_values == expected_targets
+
+    assert stream.target_names == ['class']
+
+    assert stream.n_features == 3
+
+    assert stream.n_cat_features == 0
+
+    assert stream.n_num_features == 3
+
+    assert stream.n_targets == 1
+
+    assert stream.get_data_info() == '1 target(s), 2 target_values'
+
+    assert stream.has_more_samples() is True
+
+    assert stream.is_restartable() is True
+
+    # Load test data corresponding to first 10 instances
+    test_file = os.path.join(test_path, 'sea_stream.npz')
+    data = np.load(test_file)
+    X_expected = data['X']
+    y_expected = data['y']
+
+    X, y = stream.next_sample()
+    assert np.alltrue(X[0] == X_expected[0])
+    assert np.alltrue(y[0] == y_expected[0])
+
+    X, y = stream.last_sample()
+    assert np.alltrue(X[0] == X_expected[0])
+    assert np.alltrue(y[0] == y_expected[0])
+
+    stream.restart()
+    X, y = stream.next_sample(10)
+    assert np.alltrue(X == X_expected)
+    assert np.alltrue(y == y_expected)
+
+    assert stream.n_targets == np.array(y).ndim
+
+    assert stream.n_features == X.shape[1]
+
