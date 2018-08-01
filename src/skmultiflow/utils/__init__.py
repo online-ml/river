eaf9a206bafa28f951231194735af56b16367e84
krifi_amine <aminekrifi1994@gmail.com>
2018-08-01 15:42:24 +0200

    Modification for init.py: 1- added import fonctions from utilis and some data structures from data_structures Modification for data_structure.py: 1- added pep8 to code
    Former-commit-id: d03365d9a5b8897a31ce28001426dfea94121b84

--- src/skmultiflow/utils/__init__.py
+++ src/skmultiflow/utils/__init__.py
@@ -4,3 +4,10 @@ The :mod:`skmultiflow.utils` module contains contains utility methods for `sciki
 
 from .validation import check_random_state
 from .validation import check_weights
+from .utils import normalize_values_in_dict
+from .utils import get_dimensions
+from .data_structures import FastBuffer
+from .data_structures import FastComplexBuffer
+
+__all__ = ["check_random_state", "check_weights", "get_dimensions",
+           "normalize_values_in_dict", "FastBuffer", "FastComplexBuffer"]
