260a0ba42ff84b4bacf06b49256e3306eb8ddbda
krifi_amine <aminekrifi1994@gmail.com>
2018-08-24 13:01:51 +0200

    created  tests/meta: 1-added test for adaptive random forests 2-added test for batch incremental 3-added test for classifier chains 4-added test for regressor chains 5-added test for leverage bagging 6-added test for oza bagging 7-added test for oza bagging adwin 8-added test for multioutput learner

---
+++ tests/meta/test_adaptive_random_forests.py
@@ -0,0 +1,46 @@
+from skmultiflow.core.pipeline import Pipeline
+from skmultiflow.data import RandomTreeGenerator
+from skmultiflow.meta.adaptive_random_forests import AdaptiveRandomForest
+import numpy as np
+
+
+def test_adaptive_random_forests():
+    stream = RandomTreeGenerator(tree_random_state=112, sample_random_state=112)
+    stream.prepare_for_use()
+
+    classifier = AdaptiveRandomForest(random_state=112)
+
+    learner = Pipeline([('classifier', classifier)])
+
+    X, y = stream.next_sample(150)
+    learner.partial_fit(X, y)
+
+    cnt = 0
+    max_samples = 5000
+    predictions = []
+    true_labels = []
+    wait_samples = 100
+    correct_predictions = 0
+
+    while cnt < max_samples:
+        X, y = stream.next_sample()
+        # Test every n samples
+        if (cnt % wait_samples == 0) and (cnt != 0):
+            predictions.append(learner.predict(X)[0])
+            true_labels.append(y[0])
+            if np.array_equal(y[0], predictions[-1]):
+                correct_predictions += 1
+
+        learner.partial_fit(X, y)
+        cnt += 1
+
+    performance = correct_predictions / len(predictions)
+    expected_predictions = [1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1,
+                            0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0,
+                            1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0]
+    expected_correct_predictions = 31
+    expected_performance = 0.6326530612244898
+
+    assert np.alltrue(predictions == expected_predictions)
+    assert np.isclose(expected_performance, performance)
+    assert correct_predictions == expected_correct_predictions
---
+++ tests/meta/test_multi_output_learner.py
@@ -0,0 +1,98 @@
+from skmultiflow.meta.multi_output_learner import MultiOutputLearner
+from skmultiflow.core.pipeline import Pipeline
+from skmultiflow.data import MultilabelGenerator
+from skmultiflow.trees.hoeffding_tree import HoeffdingTree
+from skmultiflow.metrics.measure_collection import hamming_score
+import numpy as np
+
+
+def test_multi_output_learner():
+
+    stream = MultilabelGenerator(n_samples=10000, n_features=15, n_targets=3, n_labels=4, random_state=112)
+    stream.prepare_for_use()
+
+    classifier = MultiOutputLearner(base_estimator=HoeffdingTree())
+
+    learner = Pipeline([('classifier', classifier)])
+
+    X, y = stream.next_sample(150)
+    learner.partial_fit(X, y)
+
+    cnt = 0
+    max_samples = 5000
+    predictions = []
+    true_labels = []
+    wait_samples = 100
+    correct_predictions = 0
+
+    while cnt < max_samples:
+        X, y = stream.next_sample()
+        # Test every n samples
+        if (cnt % wait_samples == 0) and (cnt != 0):
+            predictions.append(learner.predict(X)[0])
+            true_labels.append(y[0])
+            if np.array_equal(y[0], predictions[-1]):
+                correct_predictions += 1
+
+        learner.partial_fit(X, y)
+        cnt += 1
+
+    perf = hamming_score(true_labels, predictions)
+    expected_predictions = [[1., 1., 1.],
+                            [1., 1., 1.],
+                            [1., 1., 1.],
+                            [1., 1., 1.],
+                            [1., 1., 1.],
+                            [0., 1., 1.],
+                            [1., 0., 1.],
+                            [1., 0., 1.],
+                            [1., 1., 1.],
+                            [0., 0., 1.],
+                            [0., 1., 1.],
+                            [0., 1., 1.],
+                            [1., 1., 1.],
+                            [0., 1., 1.],
+                            [1., 1., 0.],
+                            [1., 1., 1.],
+                            [0., 1., 1.],
+                            [1., 0., 0.],
+                            [1., 0., 1.],
+                            [1., 1., 1.],
+                            [1., 0., 1.],
+                            [1., 1., 1.],
+                            [1., 1., 1.],
+                            [1., 1., 1.],
+                            [1., 1., 1.],
+                            [1., 1., 1.],
+                            [1., 1., 1.],
+                            [1., 0., 0.],
+                            [0., 1., 1.],
+                            [1., 1., 0.],
+                            [1., 1., 1.],
+                            [0., 1., 1.],
+                            [1., 1., 1.],
+                            [0., 1., 1.],
+                            [1., 0., 1.],
+                            [1., 0., 1.],
+                            [0., 0., 1.],
+                            [0., 1., 1.],
+                            [1., 1., 0.],
+                            [0., 1., 1.],
+                            [1., 1., 1.],
+                            [1., 1., 1.],
+                            [1., 0., 1.],
+                            [1., 1., 1.],
+                            [1., 1., 1.],
+                            [1., 0., 1.],
+                            [1., 1., 1.],
+                            [1., 1., 1.],
+                            [0., 1., 1.]]
+    expected_correct_predictions = 32
+
+    expected_performance = 0.8503401360544217
+
+    assert np.alltrue(np.array_equal(predictions, expected_predictions))
+    assert np.isclose(expected_performance, perf)
+    assert correct_predictions == expected_correct_predictions
+
+
