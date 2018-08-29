ecd3bde39d2f05ad2bc63fa37e8d1e6e62553356
smastelini <saulomastelini@gmail.com>
2018-08-29 16:57:50 -0300

    Added IntraClusterVarianceReductionSplitCriterion for MTR tasks
    Former-commit-id: 6ea0008eeb0e076ec69b6f8eeff9d00fcac5adcb

---
+++ src/skmultiflow/trees/intra_cluster_variance_reduction_split_criterion.py
@@ -0,0 +1,21 @@
+from skmultiflow.trees.variance_reduction_split_criterion \
+    import VarianceReductionSplitCriterion
+import numpy as np
+
+
+# This class extends VarianceReductionSplitCriterion since it just computes
+# the SD differently than its Ancestor (considering multiple targets)
+class IntraClusterVarianceReductionSplitCriterion(VarianceReductionSplitCriterion):
+
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def compute_SD(dist):
+        # TODO Also consider passing different weights for the targets
+
+        N = int(dist[0])
+        sum = dist[1]
+        sum_sq = dist[2]
+
+        return np.sqrt((np.sum(sum_sq) - (np.sum(sum) * np.sum(sum))/N)/N)
