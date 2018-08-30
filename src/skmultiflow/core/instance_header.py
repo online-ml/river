ab61f0c1a16ddafdfb58e236133bc288dc32ed36
smastelini <saulomastelini@gmail.com>
2018-08-30 10:52:02 -0300

    RegressionHoeffdingTree observed class distribution indexing bug fix
    Former-commit-id: 30d91e1001864fc5621ba08d65ac1800fb7e87ae

--- src/skmultiflow/core/instance_header.py
+++ src/skmultiflow/core/instance_header.py
@@ -3,38 +3,38 @@ from skmultiflow.core.base_object import BaseObject
 
 class InstanceHeader(BaseObject):
     """ InstanceHeader
-    
-    Stores the header from an instance, simply keeps feature and 
+
+    Stores the header from an instance, simply keeps feature and
     label's names
-    
+
     Parameters
     ----------
     header: list, optional
         The entries of the header.
-        
+
     """
 
-    def __init__(self, header = None):
+    def __init__(self, header=None):
         super().__init__()
         self.header = header
         pass
 
-    def get_header_label_at(self, header_index = -1):
+    def get_header_label_at(self, header_index=-1):
         """ get_header_label_at
-        
+
         Gets the header label at index header_index.
-        
+
         Parameters
         ----------
         header_index: int
             An index from the instance header.
-        
+
         Returns
         -------
         string
             The index label.
-        
-         
+
+
         """
         if self.header is not None:
             return self.header[header_index] if ((header_index > -1) and (header_index < len(self.header))) else None
