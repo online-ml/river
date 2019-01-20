a31d1a1a9ed42ef88a575adc30b987b6f87127b4
garawalid <gwalid94@gmail.com>
2019-01-20 02:12:36 +0100

    Hello world examples for Docker.

---
+++ docker/examples/src/hoeffding_tree.py
@@ -0,0 +1,19 @@
+from skmultiflow.data import WaveformGenerator
+
+from skmultiflow.trees.hoeffding_tree import HoeffdingTree
+from skmultiflow.evaluation.evaluate_prequential import EvaluatePrequential
+
+# 1. Create a stream
+stream = WaveformGenerator()
+stream.prepare_for_use()
+
+# 2. Instantiate the HoeffdingTree classifier
+ht = HoeffdingTree()
+
+# 3. Setup the evaluator
+evaluator = EvaluatePrequential(show_plot=False,
+                                pretrain_size=200,
+                                max_samples=20000)
+
+# 4. Run evaluation
+evaluator.evaluate(stream=stream, model=ht)
