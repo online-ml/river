6eb918cf6441d6a76bd52a52e5f57575ff6cb408
Max Halford <maxhalford25@gmail.com>
2019-01-25 14:23:04 +0100

    Add conditional surprise import

--- creme/reco/base.py
+++ creme/reco/base.py
@@ -1,28 +1,46 @@
 import abc
 
-import surprise
+try:
+    import surprise
+    SURPRISE_INSTALLED = True
+except ImportError:
+    SURPRISE_INSTALLED = False
 
 
-class Recommender(abc.ABC, surprise.AlgoBase):
+if SURPRISE_INSTALLED:
 
-    @abc.abstractmethod
-    def fit_one(self, row_id, col_id, value):
-        pass
+    class Recommender(abc.ABC, surprise.AlgoBase):
 
-    @abc.abstractmethod
-    def predict_one(self, row_id, col_id):
-        pass
+        @abc.abstractmethod
+        def fit_one(self, row_id, col_id, value):
+            pass
 
-    def fit(self, trainset):
-        """Fits to an entire training set.
+        @abc.abstractmethod
+        def predict_one(self, row_id, col_id):
+            pass
 
-        This method is compatible with the API from the `surprise` library.
+        def fit(self, trainset):
+            """Fits to an entire training set.
 
-        """
-        surprise.AlgoBase.fit(self, trainset)
-        for triplet in trainset.all_ratings():
-            self.fit_one(*triplet)
-        return self
+            This method is compatible with the API from the `surprise` library.
 
-    def estimate(self, *_):
-        return self.predict_one(*_)
+            """
+            surprise.AlgoBase.fit(self, trainset)
+            for triplet in trainset.all_ratings():
+                self.fit_one(*triplet)
+            return self
+
+        def estimate(self, *_):
+            return self.predict_one(*_)
+
+else:
+
+    class Recommender(abc.ABC, surprise.AlgoBase):
+
+        @abc.abstractmethod
+        def fit_one(self, row_id, col_id, value):
+            pass
+
+        @abc.abstractmethod
+        def predict_one(self, row_id, col_id):
+            pass
