edba927f607d8a8b08a90c7da4da5ad75cd138be
Max Halford <maxhalford25@gmail.com>
2019-01-30 14:18:29 +0100

    Fix math equations styling

--- creme/optim/losses.py
+++ creme/optim/losses.py
@@ -2,30 +2,32 @@ import abc
 import math
 
 
-__all__ = ['SquaredLoss', 'LogLoss']
+__all__ = ['LogLoss', 'SquaredLoss']
 
 
 class Loss(abc.ABC):
 
     @abc.abstractmethod
     def __call__(self, y_true, y_pred) -> float:
+        """Returns the loss."""
         pass
 
     @abc.abstractmethod
-    def gradient(self, y_true, y_pred) -> dict:
+    def gradient(self, y_true, y_pred) -> float:
+        """Returns the gradient."""
         pass
 
 
 class SquaredLoss(Loss):
-    """Computes the squared loss, also known as the mean squared error or L2 loss.
+    """Computes the squared loss, also known as the L2 loss.
 
     Mathematically, it is defined as
 
-    $$L = \\frac{1}{n} \\sum_i^n (p_i - y_i)^2$$
+    .. math:: L = (p_i - y_i)^2
 
     It's gradient w.r.t. to $p_i$ is
 
-    $$\\frac{\\partial L}{\\partial p_i} = p_i$$
+    .. math:: \\frac{\\partial L}{\\partial p_i} = 2(p_i - y_i)
 
     """
 
@@ -37,13 +39,20 @@ class SquaredLoss(Loss):
 
 
 class LogLoss(Loss):
+    """Computes the logarithmic loss.
 
-    @staticmethod
-    def clip_proba(p):
-        return max(min(p, 1 - 1e-15), 1e-15)
+    Mathematically, it is defined as
+
+    .. math:: L = -y_i log(p_i) + (1-y_i) log(1-p_i)
+
+    It's gradient w.r.t. to $p_i$ is
+
+    .. math:: \\frac{\\partial L}{\\partial y_i} = sign(p_i - y_i)
+
+    """
 
     def __call__(self, y_true, y_pred):
-        y_pred = self.clip_proba(y_pred)
+        y_pred = max(min(y_pred, 1 - 1e-15), 1e-15)
         return -(y_true * math.log(y_pred) + (1 - y_true) * math.log(1 - y_pred))
 
     def gradient(self, y_true, y_pred):
--- docs/generated/creme.model_selection.online_score.rst
+++
@@ -1,6 +0,0 @@
-creme.model\_selection.online\_score
-====================================
-
-.. currentmodule:: creme.model_selection
-
-.. autofunction:: online_score
