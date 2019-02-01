0b40bf218cb43a01e5b2438e5c979f64449c8ece
raphaelsty <raphael.sourty@gmail.com>
2019-02-01 21:57:28 +0100

    Add CentralMoments, Kurtosis and Skew classes - new features

--- creme/stats.py
+++ creme/stats.py
@@ -228,3 +228,176 @@ class Sum(RunningStatistic):
     
     def get(self):
         return self.sum
+
+class CentralMoments(RunningStatistic):
+    """Computes central moments using Welford's algorithm.
+    Attributes:
+        
+        old_count (stats.count).
+        new_count (stats.count).
+        delta (float): Mean of differences.
+        sum_delta (float): Mean of sum of differences.
+        M1 (float): sums of powers of differences from the mean order 1.
+        M2 (float): sums of powers of differences from the mean order 2.
+        M3 (float): sums of powers of differences from the mean order 3.
+        M4 (float): sums of powers of differences from the mean order 4.
+    
+    References:
+        - `Welford's online algorithm <https://www.wikiwand.com/en/Algorithms_for_calculating_variance#/Welford's_Online_algorithm>`_
+    """
+    def __init__(self):
+        super().__init__()
+        self.old_count = Count()
+        self.new_count = Count()
+        
+        self.delta     = 0
+        self.sum_delta = 0
+        
+        self.M1 = 0
+        self.M2 = 0
+        self.M3 = 0
+        self.M4 = 0
+    
+    def _update_delta(self, x):
+        self.delta = (x - self.sum_delta) / self.new_count.get()
+        return self
+        
+    def _update_sum_delta(self):
+        self.sum_delta += self.delta
+        return self
+
+    def _update_m1(self, x):
+        self.M1 = (x - self.sum_delta) * self.delta * self.old_count.get()
+        return self
+
+    def _update_m2(self):
+        self.M2 += self.M1
+        return self
+
+    def _update_m3(self):
+        self.M3 += (self.M1 * self.delta * (self.new_count.get() - 2) - 3 * 
+                    self.delta * self.M2)
+        return self
+
+    def _update_m4(self):
+        delta_square = self.delta**2
+        self.M4 += (self.M1 * delta_square *
+                   (self.new_count.get()**2 - 3 * self.new_count.get() + 3) +
+                   6 * delta_square * self.M2 - 4 * self.delta * self.M3)
+        return self
+
+    def _update_count(self):
+        if self.old_count.get() < self.new_count.get():
+            self.old_count.update()
+            self.new_count.update()
+        elif self.old_count.get() == self.new_count.get():
+            # Initialize new_count
+            self.new_count.update()
+        return self
+
+class Kurtosis(CentralMoments):
+    """Computes a running kurtosis using Welford's algorithm.
+    
+    Attributes:
+        central_moments (stats.CentralMoments)
+        
+    References:
+        - `Welford's online algorithm <https://www.wikiwand.com/en/Algorithms_for_calculating_variance#/Welford's_Online_algorithm>`_
+    
+    Example:
+        >>> import creme
+        >>> import pprint
+        >>> import scipy.stats
+        >>> import numpy as np
+
+        >>> np.random.seed(42)
+        >>> array = np.random.normal(loc = 0, scale = 1, size = 10)
+
+        >>> kurtosis = creme.stats.Kurtosis()
+        >>> pprint.pprint([kurtosis.update(v).get() for v in array])
+        [-3,
+         -2.0,
+         -1.5,
+         -1.011599627723906,
+         -0.9615800585356089,
+         -0.6989395431537853,
+         -1.4252699121794408,
+         -1.311437071070812,
+         -1.246289111322894,
+         -1.082283689864171]
+
+        >>> pprint.pprint(scipy.stats.kurtosis(array))
+        -1.0822836898641714
+    """
+    def __init__(self):
+        super().__init__()
+    
+    @property
+    def name(self):
+        return 'kurtosis'
+
+    def update(self, x):
+        self._update_count()
+        self._update_delta(x)
+        self._update_m1(x)
+        self._update_sum_delta()
+        self._update_m4()
+        self._update_m3()
+        self._update_m2()
+        return self
+
+    def get(self):
+        return (self.new_count.get() * self.M4) / self.M2**2 - 3 if self.M2 != 0 else -3
+
+class Skew(CentralMoments):
+    """Computes a running skew using Welford's algorithm.
+    
+    Attributes:
+        central_moments (stats.CentralMoments)
+    
+    References:
+        - `Welford's online algorithm <https://www.wikiwand.com/en/Algorithms_for_calculating_variance#/Welford's_Online_algorithm>`_
+    
+    Example:
+        >>> import creme
+        >>> import pprint
+        >>> import scipy.stats
+        >>> import numpy as np
+
+        >>> np.random.seed(42)
+        >>> array = np.random.normal(loc = 0, scale = 1, size = 10)
+
+        >>> skew = creme.stats.Skew()
+        >>> pprint.pprint([skew.update(v).get() for v in array])
+        [0,
+         0.0,
+         -0.6043053732501439,
+         0.2960327239981376,
+         0.5234724473423674,
+         0.7712778043924866,
+         0.39022088752624845,
+         0.278892645224261,
+         0.37425953513864063,
+         0.3476878073823696]
+
+        >>> pprint.pprint(scipy.stats.skew(array))
+        0.34768780738236926
+    """
+    def __init__(self):
+        super().__init__()
+    
+    @property
+    def name(self):
+        return 'skwew'
+
+    def update(self, x):
+        self._update_count()
+        self._update_delta(x)
+        self._update_m1(x)
+        self._update_sum_delta()
+        self._update_m3()
+        self._update_m2()
+        return self
+    
+    def get(self):
+        return (math.sqrt(self.new_count.get()) * self.M3) / self.M2**(3/2) if self.M2 != 0 else 0
