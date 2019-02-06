27b04fbac37f9e704ac7e95779657224a0bf5b36
Max Halford <maxhalford25@gmail.com>
2019-02-06 16:56:33 +0100

    Add TargetEncoder

--- creme/stats/mean.py
+++ creme/stats/mean.py
@@ -25,35 +25,3 @@ class Mean(base.RunningStatistic):
 
     def get(self):
         return self.mu
-
-
-class SmoothMean(Mean):
-    """Computes a running mean using additive smoothing.
-
-    Parameters:
-        prior (float): Prior mean.
-        prior_weight (float): Strengh of the prior mean.
-
-    Attributes:
-        count (stats.Count)
-        mu (float): The current estimated mean.
-
-    References:
-
-    - `Additive smoothing <https://www.wikiwand.com/en/Additive_smoothing>`_
-
-    """
-
-    def __init__(self, prior, prior_weight):
-        super().__init__()
-        self.prior = prior
-        self.prior_weight = prior_weight
-
-    @property
-    def name(self):
-        return 'smooth_mean'
-
-    def get(self):
-        numerator = self.mu * self.count.get() + self.prior * self.prior_weight
-        denominator = self.count.get() + self.prior_weight
-        return numerator / denominator
