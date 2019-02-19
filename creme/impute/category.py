841b4b8b1b2cd4cd6446756b135f8222fec1556d
raphaelsty <raphael.sourty@gmail.com>
2019-02-19 14:32:08 +0100

    Fill missing values with a constant

--- creme/impute/category.py
+++ creme/impute/category.py
@@ -2,6 +2,8 @@ import collections
 import copy
 
 from . import imputer
+from . import _constant
+
 from .. import stats
 
 
@@ -13,6 +15,7 @@ class CategoryImputer(imputer.Imputer):
         by (str): Name of the field to impute with aggregatation.
         how (str): Method to fill missing values.
         k (int): Number of modalities in the target variable.
+        constant_value (str): Constant to replace missing values, when using strategy='constant'.
 
     >>> import creme
 
@@ -40,6 +43,24 @@ class CategoryImputer(imputer.Imputer):
     {'x': 'rainy'}
     {'x': 'rainy'}
     {'x': 'rainy'}
+
+    >>> imputer_top_k = creme.impute.CategoryImputer(
+    ...     on='x',
+    ...     how='constant',
+    ...     constant_value='unknown',
+    ... )
+
+    >>> for x in X:
+    ...     print(imputer_top_k.fit_one(x))
+    {'x': 'sunny'}
+    {'x': 'rainy'}
+    {'x': 'humidity'}
+    {'x': 'sunny'}
+    {'x': 'rainy'}
+    {'x': 'rainy'}
+    {'x': 'unknown'}
+    {'x': 'unknown'}
+    {'x': 'unknown'}
 
     >>> imputer_by_town = creme.impute.CategoryImputer(
     ...    on='weather',
@@ -74,12 +95,20 @@ class CategoryImputer(imputer.Imputer):
     {'town': 'Pekin', 'weather': 'sunny'}
     """
 
-    def __init__(self, on, by=None, k=25, exact=False):
+    def __init__(self, on, how='mode', by=None, k=25, exact=False, constant_value=None):
 
         super().__init__()
         self.on = on
         self.by = by
 
+        allowed_imputers = {
+            'mode': stats.Mode(),
+            'constant': _constant.Constant(constant_value=constant_value),
+        }
+
+        if how not in allowed_imputers:
+            raise ValueError(
+                f'Can only use one of: {allowed_imputers.keys()}')
+
         self.imputers = collections.defaultdict(
-            lambda: copy.deepcopy(stats.Mode(k=k, exact=exact))
-        )
+            lambda: copy.deepcopy(allowed_imputers[how]))
