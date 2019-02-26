cc32caa5ede84ddaf986d59e3ce58985cde8fbe0
Max Halford <maxhalford25@gmail.com>
2019-02-26 16:39:41 +0100

    Use collections.Counter instead of a custom implementation

--- creme/stats/entropy.py
+++ creme/stats/entropy.py
@@ -1,5 +1,5 @@
+import collections
 import math
-from collections import defaultdict
 
 from . import base
 from . import count
@@ -12,8 +12,8 @@ class Entropy(base.RunningStatistic):
     Attributes:
         entropy (float) : The running entropy.
         alpha (int) : Fading factor
-    
-    Example: 
+
+    Example:
 
     ::
 
@@ -22,7 +22,7 @@ class Entropy(base.RunningStatistic):
         >>> import numpy as np
         >>> from scipy.stats import entropy
         >>> from creme import stats
-        
+
         >>> def entropy_list(labels, base=None):
         ...   value,counts = np.unique(labels, return_counts=True)
         ...   return entropy(counts, base=base)
@@ -30,10 +30,10 @@ class Entropy(base.RunningStatistic):
         >>> SEED = 42 * 1337
         >>> random.seed(SEED)
 
-        >>> entro = stats.Entropy(alpha=1) 
+        >>> entro = stats.Entropy(alpha=1)
 
         >>> list_animal = []
-        >>> for animal, num_val in zip(['cat', 'dog', 'bird'],[301, 401, 601]): 
+        >>> for animal, num_val in zip(['cat', 'dog', 'bird'],[301, 401, 601]):
         ...     list_animal += [animal for i in range(num_val)]
         >>> random.shuffle(list_animal)
 
@@ -44,7 +44,7 @@ class Entropy(base.RunningStatistic):
         1.058093
         >>> print(f'{entropy_list(list_animal):.6f}')
         1.058093
-       
+
 
     References :
     1. `Updating Formulas and Algorithms for Computing Entropy and Gini Index from Time-Changing Data Streams <https://arxiv.org/pdf/1403.6348.pdf>`_
@@ -56,34 +56,34 @@ class Entropy(base.RunningStatistic):
         if 0 < alpha <= 1:
             self.alpha = alpha
         else:
-            raise ValueError('alpha must be between 0  excluded and 1')
+            raise ValueError('alpha must be between 0 excluded and 1')
         self.eps = eps
         self.entropy = 0
-        self.count = count.Count()
-        self.count_category = categorical_count.CategoricalCount()
+        self.n = 0
+        self.counter = collections.Counter()
 
     @property
     def name(self):
         return 'entropy'
 
     def update(self, x):
-        
-        if x not in self.count_category.get():
-            self.count_category.cat_count[x] = 0
 
-        updated_entropy = ((self.count.get() + self.eps) / (self.count.get() + 1)) * ((self.alpha *
-                                                                         self.entropy) - math.log(((self.count.get() + self.eps) / (self.count.get() + 1))))
-        
-        updated_entropy -= ((self.count_category.get()[x] + 1) / (self.count.get() + 1)) * math.log(
-            ((self.count_category.get()[x] + 1) / (self.count.get() + 1)))
-        
-        updated_entropy += ((self.count_category.get()[x] + self.eps) / (self.count.get() + 1)) * math.log(
-            ((self.count_category.get()[x] + self.eps) / (self.count.get() + 1)))
-       
+        cx = self.counter.get(x, 0)
+        n = self.n
+
+        updated_entropy = ((n + self.eps) / (n + 1)) * ((self.alpha * self.entropy) - math.log(((n + self.eps) / (n + 1))))
+
+        updated_entropy -= ((cx + 1) / (n + 1)) * math.log(
+            ((cx + 1) / (n + 1)))
+
+        updated_entropy += ((cx + self.eps) / (n + 1)) * math.log(
+            ((cx + self.eps) / (n + 1)))
+
         self.entropy = updated_entropy
 
-        self.count.update()
-        self.count_category.update(x)
+        self.n += 1
+        self.counter.update([x])
+
         return self
 
     def get(self):
