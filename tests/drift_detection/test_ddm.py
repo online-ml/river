ec93d3aaa1779ba5681a605062c9ff6e6a31ddc4
Jacob Montiel <jacob.montiel@gmail.com>
2019-03-12 14:20:25 +0100

    Update DDM drift detector implementation - Fix minor bug where drift was calculated for instances before reaching the min number of instances - Update tests - Improve documentation
    Former-commit-id: 8bfbc083bcd2b70be6f1df48ed138255b2e5740b

--- tests/drift_detection/test_ddm.py
+++ tests/drift_detection/test_ddm.py
@@ -5,17 +5,17 @@ from skmultiflow.drift_detection import DDM
 
 def test_ddm(test_path):
     """
-    EDDM drift detection test.
+    DDM drift detection test.
     The first half of the stream contains a sequence corresponding to a normal distribution of integers from 0 to 1.
     From index 999 to 1999 the sequence is a normal distribution of integers from 0 to 7.
     """
     ddm = DDM()
     test_file = os.path.join(test_path, 'drift_stream.npy')
     data_stream = np.load(test_file)
-    expected_indices = [29]
+    expected_indices = [1009]
     detected_indices = []
 
-    for i in range(1000):   # Only use the first half of stream (input is 0 or 1)
+    for i in range(data_stream.size):
         ddm.add_element(data_stream[i])
         if ddm.detected_change():
             detected_indices.append(i)
