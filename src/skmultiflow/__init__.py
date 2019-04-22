c908bf1880be5ff213197cbdf8c67588f6c8f9ad
Jacob Montiel <jacob.montiel@gmail.com>
2019-04-23 01:08:21 +0200

    Add _version.py to make version information available within the package.
    Former-commit-id: 1682df74c3fb867d8dd548d2b6472a80859847e1

--- src/skmultiflow/__init__.py
+++ src/skmultiflow/__init__.py
@@ -14,5 +14,8 @@ from . import trees
 from . import utils
 from . import visualization
 
-__all__ = ['bayes', 'core', 'data', 'drift_detection', 'evaluation', 'lazy',
-           'meta', 'metrics', 'neural_networks', 'trees', 'utils', 'visualization']
+from ._version import __version__
+from .utils._show_versions import show_versions
+
+__all__ = ['__version__', 'bayes', 'core', 'data', 'drift_detection', 'evaluation', 'lazy',
+           'meta', 'metrics', 'neural_networks', 'trees', 'utils', 'visualization', 'show_versions']
