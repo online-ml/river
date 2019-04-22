9628370e43e286b015f29bcca40f6f6f1b3faf12
Jacob Montiel <jacob.montiel@gmail.com>
2019-04-23 01:11:35 +0200

    Add BaseEstimator and BaseStreamEstimator classes - BaseEstimator is based on sklearn's and is intended to improve compatibility between the frameworks - BaseStreamEstimator provides additional functionality in the context of skmultiflow
    Former-commit-id: 8a545145d6a50e56d31ba0b6a697e84d07ffef3c

--- src/skmultiflow/utils/utils.py
+++ src/skmultiflow/utils/utils.py
@@ -1,7 +1,10 @@
-import numpy as np
 import math
 import sys
+import numbers
+
 from collections import deque
+
+import numpy as np
 
 
 def get_dimensions(X):
@@ -143,3 +146,35 @@ def calculate_object_size(obj, unit='byte'):
         final_size = byte_size
 
     return final_size
+
+
+def is_scalar_nan(x):
+    """Tests if x is NaN
+
+    This function is meant to overcome the issue that np.isnan does not allow
+    non-numerical types as input, and that np.nan is not np.float('nan').
+
+    Parameters
+    ----------
+    x : any type
+
+    Returns
+    -------
+    boolean
+
+    Examples
+    --------
+    >>> is_scalar_nan(np.nan)
+    True
+    >>> is_scalar_nan(float("nan"))
+    True
+    >>> is_scalar_nan(None)
+    False
+    >>> is_scalar_nan("")
+    False
+    >>> is_scalar_nan([np.nan])
+    False
+    """
+    # convert from numpy.bool_ to python bool to ensure that testing
+    # is_scalar_nan(x) is True does not fail.
+    return bool(isinstance(x, numbers.Real) and np.isnan(x))
