ae99f373773c45cf704801b71af86d3d50fe1b1a
Jacob Montiel <jacob.montiel@gmail.com>
2019-04-24 02:38:34 +0200

    Make new base classes available at the core module level

--- src/skmultiflow/core/__init__.py
+++ src/skmultiflow/core/__init__.py
@@ -3,5 +3,9 @@ The :mod:`skmultiflow.core` module covers core methods of `scikit-multiflow`.
 """
 
 from .pipeline import Pipeline
+from .base import BaseStreamEstimator
+from .base import ClassifierMixin
+from .base import RegressorMixin
+from .base import MetaEstimatorMixin
 
-__all__ = ["Pipeline"]
+__all__ = ["BaseStreamEstimator", "ClassifierMixin", "RegressorMixin", "MetaEstimatorMixin", "Pipeline"]
