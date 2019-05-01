6d9d3d97ff8b6370d440127e3a3d64a281c4aae3
Jacob Montiel <jacob.montiel@gmail.com>
2019-05-01 03:12:47 +0200

    Minor documentation update

--- src/skmultiflow/lazy/knn.py
+++ src/skmultiflow/lazy/knn.py
@@ -5,7 +5,7 @@ from skmultiflow.utils.utils import *
 
 
 class KNN(BaseStreamEstimator, ClassifierMixin):
-    """ K-Nearest Neighbors Classifier
+    """ K-Nearest Neighbors classifier
     
     This is a non-parametric classification method. The output of this
     algorithm are the n_neighbors closest training examples to the query sample
