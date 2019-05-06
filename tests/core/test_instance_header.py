4c2fdb17f879f0d81be4abbe8d330ffaeed7d465
Jacob Montiel <jacob.montiel@gmail.com>
2019-05-06 20:37:36 +0200

    Replace BaseObject class with object class - This change is intended to streamline the implementation of some aux classes - This is in preparation for a new implementation of BaseObject - Update tests

--- tests/core/test_instance_header.py
+++ tests/core/test_instance_header.py
@@ -1,12 +1,10 @@
 from skmultiflow.core.instance_header import InstanceHeader
 
 
-def test_instance_heaer():
+def test_instance_header():
     header = InstanceHeader(header=['foo', 'bar', 'target'])
 
     assert header.get_info() == "InstanceHeader: header: ['foo', 'bar', 'target']"
-
-    assert header.get_class_type() == 'instance'
 
     assert header.get_header_label_at(0) == 'foo'
 
