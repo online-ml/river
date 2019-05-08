b4df186550e5b8ecf9897de035af4258dc977762
Jacob Montiel <jacob.montiel@gmail.com>
2019-05-08 02:17:07 +0200

    Pipeline: Change base class from BaseObject to BaseStreamEstimator - Update tests
    Former-commit-id: dabedd01ee223ad8e11368813885c44fe64f04dc

--- src/skmultiflow/core/__init__.py
+++ src/skmultiflow/core/__init__.py
@@ -2,12 +2,12 @@
 The :mod:`skmultiflow.core` module covers core methods of `scikit-multiflow`.
 """
 
-from .pipeline import Pipeline
 from .base import BaseStreamEstimator
 from .base import ClassifierMixin
 from .base import RegressorMixin
 from .base import MetaEstimatorMixin
 from .base import MultiOutputMixin
+from .pipeline import Pipeline
 
 __all__ = ["BaseStreamEstimator", "ClassifierMixin", "RegressorMixin", "MetaEstimatorMixin", "MultiOutputMixin",
            "Pipeline"]
