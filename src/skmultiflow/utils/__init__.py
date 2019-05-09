83ce9029c9cc42f43a64036b792a8baf0a501dd1
Jacob Montiel <jacob.montiel@gmail.com>
2019-05-10 01:03:37 +0200

    Update utilities tests
    Former-commit-id: 41861749f6a0200f2bca73d3c4f4519840b5035e

--- src/skmultiflow/utils/__init__.py
+++ src/skmultiflow/utils/__init__.py
@@ -9,6 +9,7 @@ from .utils import get_dimensions
 from .utils import is_scalar_nan
 from .data_structures import FastBuffer
 from .data_structures import FastComplexBuffer
+from ._show_versions import show_versions
 
 __all__ = ["check_random_state", "check_weights", "get_dimensions", 'is_scalar_nan',
-           "normalize_values_in_dict", "FastBuffer", "FastComplexBuffer"]
+           "normalize_values_in_dict", "FastBuffer", "FastComplexBuffer", "show_versions"]
