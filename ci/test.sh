b603fc4126cfd100f431943d3dd4f7caa9b83e5e
Max Halford <maxhalford25@gmail.com>
2019-05-24 23:53:14 +0200

    Add cache for miniconda and pip

--- ci/test.sh
+++ ci/test.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 # Install and update Miniconda
-if [ $TRAVIS_OS_NAME = "osx" ]
-  then
-    fname=Miniconda3-latest-MacOSX-x86_64.sh
-  else
-    fname=Miniconda3-latest-Linux-x86_64.sh
+if [[ ! -f download/miniconda.sh ]]
+  if [ $TRAVIS_OS_NAME = "osx" ]
+    then
+      fname=Miniconda3-latest-MacOSX-x86_64.sh
+    else
+      fname=Miniconda3-latest-Linux-x86_64.sh
+    fi
+  wget https://repo.continuum.io/miniconda/$fname -O download/miniconda.sh
   fi
-wget https://repo.continuum.io/miniconda/$fname \
-    -O miniconda.sh
 MINICONDA_PATH=$HOME/miniconda
-chmod +x miniconda.sh && ./miniconda.sh -b -p $MINICONDA_PATH
+chmod +x download/miniconda.sh && ./download/miniconda.sh -b -p $MINICONDA_PATH
 export PATH=$MINICONDA_PATH/bin:$PATH
 conda update --yes conda
 
