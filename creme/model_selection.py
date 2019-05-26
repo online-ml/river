0913561b9d9eaa1886e2ab249856057cdbbdd18d
Max Halford <maxhalford25@gmail.com>
2019-05-26 20:21:02 +0200

    WIP plotting

--- creme/model_selection.py
+++ creme/model_selection.py
@@ -1,6 +1,8 @@
 """
 Utilities for measuring the performance of online learning algorithms.
 """
+import math
+
 from . import base
 from . import stream
 
@@ -8,13 +10,14 @@ from . import stream
 __all__ = ['online_score', 'online_qa_score']
 
 
-def online_score(X_y, model, metric):
+def online_score(X_y, model, metric, print_every=math.inf):
     """Computes the online score of a model given a stream of data and a metric.
 
     Parameters:
         X_y (generator): A stream of (features, target) tuples.
         model (estimator)
         metric (callable)
+        print_every (int): Iteration number at which to print the current metric.
 
     Returns:
         metric
@@ -39,15 +42,18 @@ def online_score(X_y, model, metric):
         metric.update(y, y_pred)
 
     # Train the model and use the out-of-fold predictions to update the metric
-    for x, y in X_y:
+    for i, (x, y) in enumerate(X_y):
         y_pred = pred_func(x)
         model = model.fit_one(x, y)
         metric.update(y, y_pred)
 
+        if not (i + 1) % print_every:
+            print(f'[{i+1}] {metric}')
+
     return metric
 
 
-def online_qa_score(X_y, model, metric, on, lag):
+def online_qa_score(X_y, model, metric, on, lag, print_every=math.inf):
     """A variant of online scoring where the targets are revealed with a lag.
 
     ``X_y`` is converted into a question and answer where the model is asked to predict an
@@ -62,6 +68,7 @@ def online_qa_score(X_y, model, metric, on, lag):
         lag (datetime.timedelta or int or float): Amount to wait before revealing the target
             associated with each observation. This value is expected to be able to sum with the
             `on` attribute.
+        print_every (int): Iteration number at which to print the current metric.
 
     Returns:
         metric
@@ -79,16 +86,19 @@ def online_qa_score(X_y, model, metric, on, lag):
         pred_func = model.predict_proba_one
 
     # Train the model and use the out-of-fold predictions to update the metric
-    for x, y in stream.simulate_qa(X_y, on=on, lag=lag):
+    for i, (is_question, x, y) in enumerate(stream.simulate_qa(X_y, on=on, lag=lag)):
 
         # If y is None then this is a question
-        if y is None:
+        if is_question:
             y_pred = pred_func(x)
             if y_pred != {} and y_pred is not None:
                 metric.update(y, y_pred)
-            continue
 
         # If not this is an answer
-        model = model.fit_one(x, y)
+        else:
+            model = model.fit_one(x, y)
+
+        if not (i + 1) % print_every:
+            print(f'[{i+1}] {metric}')
 
     return metric
