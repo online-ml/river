8835c738f60744bb71ddc6ad4d86006fcf2701d5
Max Halford <maxhalford25@gmail.com>
2019-06-07 12:00:17 +0200

    Add Normalizer
    Former-commit-id: 7c67836620a2195cd7b49f3d16ab03dac9cbf81b

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 - `Jaccard` and `RollingConfusionMatrix` to `metrics`
 - `ClassifierChain` and `RegressorChain` to `multioutput`
+- `Normalizer` to `preprocessing`
 
 ### Modified
 
--- creme/preprocessing/__init__.py
+++ creme/preprocessing/__init__.py
@@ -4,13 +4,15 @@ Module for preparing data to make it compatible with certain machine learning al
 from .feature_hasher import FeatureHasher
 from .one_hot import OneHotEncoder
 from .poly import PolynomialExtender
-from .scale import StandardScaler
 from .scale import MinMaxScaler
+from .scale import Normalizer
+from .scale import StandardScaler
 
 
 __all__ = [
     'FeatureHasher',
     'MinMaxScaler',
+    'Normalizer',
     'OneHotEncoder',
     'PolynomialExtender',
     'StandardScaler'
