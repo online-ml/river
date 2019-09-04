63c8d946c7309b9ba402c89531d4dc089cdcc741
AdilZouitine <adilzouitinegm@gmail.com>
2019-09-04 13:46:52 +0200

    add docstring

--- creme/stats/iqr.py
+++ creme/stats/iqr.py
@@ -3,6 +3,39 @@ from . import quantile
 
 
 class IQR(base.Univariate):
+    """Computes the interquartile range.
+
+    Parameters:
+        q_inf (float): Desired inferior quantile, must be between 0 and 1. Defaults to `0.25`.
+        q_sup (float): Desired superior quantile, must be between 0 and 1. Defaults to `0.75`.
+
+    Example:
+
+        ::
+
+            >>> from creme import stats
+            >>> iqr = stats.IQR(
+            ...     q_inf=0.25,
+            ...     q_sup=0.75
+            ... )
+
+            >>> for i in range(0, 1001):
+            ...     iqr = iqr.update(i)
+            ...     if i % 100 == 0:
+            ...         print(iqr.get())
+            0
+            50
+            100
+            150
+            200
+            250
+            300
+            350
+            400
+            450
+            500
+    """
+
     def __init__(self, q_inf=0.25, q_sup=0.75):
         if q_inf >= q_sup:
             raise ValueError('q_inf must be strictly less than q_sup')
@@ -19,7 +52,41 @@ class IQR(base.Univariate):
 
 
 class RollingIQR(base.RollingUnivariate):
-    
+    """Computes the rolling interquartile range.
+
+    Parameters:
+        window_size (int): Size of the window.
+        q_inf (float): Desired inferior quantile, must be between 0 and 1. Defaults to `0.25`.
+        q_sup (float): Desired superior quantile, must be between 0 and 1. Defaults to `0.75`.
+
+    Example:
+
+        ::
+
+            >>> from creme import stats
+            >>> rolling_iqr = stats.RollingIQR(
+            ...     q_inf=0.25,
+            ...     q_sup=0.75,
+            ...     window_size=100
+            ... )
+
+            >>> for i in range(0, 1001):
+            ...     rolling_iqr = iqr.update(i)
+            ...     if i % 100 == 0:
+            ...         print(rolling_iqr.get())
+            0
+            50
+            50
+            50
+            50
+            50
+            50
+            50
+            50
+            50
+            50
+    """
+
     def __init__(self, window_size, q_inf=0.25, q_sup=0.75):
         if q_inf >= q_sup:
             raise ValueError('q_inf must be strictly less than q_sup')
