56bb8bf2e3046d3a810ea5e97e5a4bacfaaa01da
Max Halford <maxhalford25@gmail.com>
2019-09-09 09:21:18 +0200

    WIP

--- creme/tree/splitting.py
+++ creme/tree/splitting.py
@@ -21,11 +21,11 @@ LT = Op('<', operator.lt)
 EQ = Op('=', operator.eq)
 
 
-class Split(collections.namedtuple('Split', 'on how at impurity')):
+class Split(collections.namedtuple('Split', 'on how at')):
     """A data class for storing split details."""
 
     def __str__(self):
-        return f'{self.on} {self.how} {self.at} ({self.impurity:.5f})'
+        return f'{self.on} {self.how} {self.at}'
 
     def __call__(self, x):
         return self.how(x[self.on], self.at)
@@ -57,12 +57,11 @@ class HistSplitEnum(SplitEnum, collections.defaultdict):
         self.P_x.update(x)
         return self
 
-    def enumerate_splits(self, target_dist, criterion):
+    def enumerate_splits(self, target_dist):
 
         for b in self.P_x[:-1]:
 
             x = b.right
-            p_x = self.P_x.cdf(x)  # P(x < t)
 
             l_dist = {}
             r_dist = {}
@@ -70,11 +69,13 @@ class HistSplitEnum(SplitEnum, collections.defaultdict):
             for y in target_dist:
                 p_xy = self.P_xy[y].cdf(x)  # P(x < t | y)
                 p_y = target_dist.pmf(y)  # P(y)
-                l_dist[y] = p_xy * p_y / p_x  # P(y | x < t)
-                r_dist[y] = (1 - p_xy) * p_y / (1 - p_x)  # P(y | x >= t)
+                l_dist[y] = target_dist.n_samples * p_xy * p_y  # P(y | x < t)
+                r_dist[y] = target_dist.n_samples * (1 - p_xy) * p_y  # P(y | x >= t)
 
-            impurity = p_x * criterion(l_dist) + (1 - p_x) * criterion(r_dist)
-            yield Split(on=self.feature_name, how=LT, at=x, impurity=impurity)
+            l_dist = proba.Multinomial(l_dist)
+            r_dist = proba.Multinomial(r_dist)
+
+            yield Split(on=self.feature_name, how=LT, at=x), l_dist, r_dist
 
 
 class CategoricalSplitEnum(SplitEnum):
@@ -119,61 +120,3 @@ class CategoricalSplitEnum(SplitEnum):
     def enumerate_splits(self):
         for cat in self.categories:
             yield cat, EQ
-
-
-class GaussianSplitEnum(SplitEnum):
-    """
-
-    Example:
-
-        >>> import collections
-        >>> import random
-
-        >>> sf = GaussianSplitEnum(n=10)
-        >>> class_dist = collections.Counter()
-
-        >>> random.seed(42)
-        >>> X = (
-        ...     [random.gauss(3, 2) for _ in range(60)] +
-        ...     [random.gauss(7, 2) for _ in range(40)]
-        ... )
-        >>> Y = [False] * 60 + [True] * 40
-
-        >>> for x, y in zip(X, Y):
-        ...     sf = sf.update(x, y)
-        ...     class_dist.update([y])
-
-    """
-
-    def __init__(self, feature_name, n):
-        super().__init__(feature_name)
-        self.P_xy = collections.defaultdict(proba.Gaussian)
-        self.n = n
-        self.min = math.inf
-        self.max = -math.inf
-        self.P_x = proba.Gaussian()
-
-    def update(self, x, y):
-        self.min = min(x, self.min)
-        self.max = max(x, self.max)
-        self.P_x.update(x)
-        self.P_xy[y].update(x)
-        return self
-
-    def p_feature_given_class(self, x, y):
-        return self.P_xy[y].cdf(x)
-
-    def enumerate_splits(self):
-        a = max(self.min, min(d.mu - 2. * d.sigma for d in self.P_xy.values()))
-        b = min(self.max, max(d.mu + 2. * d.sigma for d in self.P_xy.values()))
-
-        #a = max(self.min, self.P_x.mu - 2. * self.P_x.sigma)
-        #b = min(self.max, self.P_x.mu + 2. * self.P_x.sigma)
-
-        step = (b - a) / (self.n - 1)
-
-        if step == 0:
-            return
-
-        for i in range(self.n):
-            yield a + i * step, LT
