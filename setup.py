5d352235393e2b718b8d38a52a53c8b4533dbce7
Max Halford <maxhalford25@gmail.com>
2019-10-17 20:04:31 +0200

    Fix
    Former-commit-id: c78b447c036ac5015de96e54382b498561308378

--- setup.py
+++ setup.py
@@ -147,8 +147,7 @@ setup(
     cmdclass={
         'upload': UploadCommand,
     },
-    ext_modules=cythonize(
-        [Extension('*', sources=['**/*.pyx'], libraries=['m'])],
-        compiler_directives={'language_level': '3'}
-    )
+    ext_modules=cythonize([
+        Extension('*', sources=['**/*.pyx'], libraries=['m'])
+    ])
 )
