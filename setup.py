459e326ec7db94d94b5edd31fc9e7e4d54144e31
Max Halford <maxhalford25@gmail.com>
2019-10-23 14:45:14 +0200

    Bump version and upload wheels manually
    Former-commit-id: 711ad8c24ba2c753c4014e7890b632d18619bc80

--- setup.py
+++ setup.py
@@ -73,43 +73,6 @@ else:
     about['__version__'] = VERSION
 
 
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPi via Twine…')
-        os.system('twine upload dist/*')
-
-        #self.status('Pushing git tags…')
-        #os.system('git tag v{0}'.format(about['__version__']))
-        #os.system('git push --tags')
-
-        sys.exit()
-
-
 # Where the magic happens:
 setup(
     name=NAME,
@@ -143,9 +106,5 @@ setup(
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
-    # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
     ext_modules=cythonize([Extension('*', sources=['**/*.pyx'], libraries=['m'])])
 )
