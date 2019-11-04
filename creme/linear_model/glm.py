118b45b2dbb4de82f57e399ffb3ca4648226561c
Max Halford <maxhalford25@gmail.com>
2019-11-04 16:14:16 +0100

    Add clip_gradient parameter

--- creme/linear_model/glm.py
+++ creme/linear_model/glm.py
@@ -17,17 +17,16 @@ class GLM:
         intercept_lr (optim.schedulers.Scheduler or float): Learning rate scheduler used for
             updating the intercept. If a `float` is passed, then an instance of
             `optim.schedulers.Constant` will be used. Setting this to 0 implies that the intercept
-            will be not be updated.
-        Setting this to 0
-            means that no intercept will be used.
-        l2 (float): Amount of L2 regularization used to push weights towards .
+            will be not be updated. Setting this to 0 means that no intercept will be used.
+        l2 (float): Amount of L2 regularization used to push weights towards 0.
+        clip_gradient (float): Clips the absolute value of each gradient value.
 
     Attributes:
         weights (collections.defaultdict)
 
     """
 
-    def __init__(self, optimizer, loss, l2, intercept, intercept_lr):
+    def __init__(self, optimizer, loss, l2, intercept, intercept_lr, clip_gradient):
         self.optimizer = optimizer
         self.loss = loss
         self.l2 = l2
@@ -37,6 +36,7 @@ class GLM:
             if isinstance(intercept_lr, numbers.Number) else
             intercept_lr
         )
+        self.clip_gradient = clip_gradient
         self.weights = collections.defaultdict(float)
 
     def _raw_dot(self, x):
@@ -50,12 +50,13 @@ class GLM:
         # Obtain the gradient of the loss with respect to the raw output
         g_loss = self.loss.gradient(y_true=y, y_pred=self._raw_dot(x))
 
-        # Clip the gradient of the loss to avoid numerical instabilities
-        g_loss = utils.math.clamp(g_loss, -1e12, 1e12)
-
         # Calculate the gradient
         gradient = {
-            i: xi * g_loss + 2. * self.l2 * self.weights.get(i, 0)
+            i: utils.math.clamp(
+                x=xi * g_loss + 2. * self.l2 * self.weights.get(i, 0),
+                minimum=-self.clip_gradient,
+                maximum=self.clip_gradient
+            )
             for i, xi in x.items()
         }
 
@@ -81,7 +82,7 @@ class LinearRegression(GLM, base.Regressor):
             updating the intercept. If a `float` is passed, then an instance of
             `optim.schedulers.Constant` will be used. Setting this to 0 implies that the intercept
             will be not be updated.
-        l2 (float): Amount of L2 regularization used to push weights towards .
+        l2 (float): Amount of L2 regularization used to push weights towards 0.
 
     Attributes:
         weights (collections.defaultdict): The current weights assigned to the features.
@@ -120,7 +121,8 @@ class LinearRegression(GLM, base.Regressor):
 
     """
 
-    def __init__(self, optimizer=None, loss=None, l2=.0001, intercept=0., intercept_lr=.01):
+    def __init__(self, optimizer=None, loss=None, l2=.0001, intercept=0., intercept_lr=.01,
+                 clip_gradient=1e12):
         super().__init__(
             optimizer=(
                 optim.SGD(optim.schedulers.InverseScaling(.01, .25))
@@ -130,7 +132,8 @@ class LinearRegression(GLM, base.Regressor):
             loss=optim.losses.Squared() if loss is None else loss,
             intercept=intercept,
             intercept_lr=intercept_lr,
-            l2=l2
+            l2=l2,
+            clip_gradient=clip_gradient
         )
 
     def predict_one(self, x):
@@ -182,13 +185,15 @@ class LogisticRegression(GLM, base.BinaryClassifier):
 
     """
 
-    def __init__(self, optimizer=None, loss=None, l2=.0001, intercept=0., intercept_lr=.01):
+    def __init__(self, optimizer=None, loss=None, l2=.0001, intercept=0., intercept_lr=.01,
+                 clip_gradient=1e12):
         super().__init__(
             optimizer=optim.SGD(.01) if optimizer is None else optimizer,
             loss=optim.losses.Log() if loss is None else loss,
             intercept=intercept,
             intercept_lr=intercept_lr,
-            l2=l2
+            l2=l2,
+            clip_gradient=clip_gradient
         )
 
     def predict_proba_one(self, x):
