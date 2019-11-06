e162fe5a2a465e6b2cbd6673f40d0bc5fb42e98b
Max Halford <maxhalford25@gmail.com>
2019-11-06 09:52:43 +0100

    Fix SMAPE

--- creme/metrics/smape.py
+++ creme/metrics/smape.py
@@ -15,23 +15,21 @@ class SMAPE(stats.Mean, base.RegressionMetric):
 
             >>> from creme import metrics
 
-            >>> y_true = [100, 100]
-            >>> y_pred = [110, 90]
+            >>> y_true = [0.07533, 0.07533, 0.07533, 0.07533, 0.07533, 0.07533, 0.0672, 0.0672]
+            >>> y_pred = [0.102, 0.107, 0.047, 0.1, 0.032, 0.047, 0.108, 0.089]
 
             >>> metric = metrics.SMAPE()
             >>> for y_t, y_p in zip(y_true, y_pred):
-            ...     print(metric.update(y_t, y_p))
-            SMAPE: 4.761905
-            SMAPE: 5.012531
+            ...     metric = metric.update(y_t, y_p)
 
             >>> metric
-            SMAPE: 5.012531
+            SMAPE: 42.603066
 
     """
 
     def update(self, y_true, y_pred, sample_weight=1.):
         return super().update(
-            x=abs(y_true - y_pred) / (abs(y_true) + abs(y_pred)),
+            x=2. * abs(y_true - y_pred) / (abs(y_true) + abs(y_pred)),
             w=sample_weight
         )
 
