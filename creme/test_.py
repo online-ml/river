33601e56557ac5e1be7e09c4a702e28d8b749d11
Max Halford <maxhalford25@gmail.com>
2019-11-12 22:58:43 +0100

    Update checks
    Former-commit-id: 3881ffc26b55cb0d458297b1cd6ded2eaa110d15

--- creme/test_.py
+++ creme/test_.py
@@ -4,16 +4,13 @@ General tests for all estimators.
 import copy
 import importlib
 import inspect
-import itertools
 
 import pytest
 
-import creme
 from creme import base
 from creme import dummy
 from creme import cluster
 from creme import compose
-from creme import datasets
 from creme import ensemble
 from creme import feature_extraction
 from creme import feature_selection
@@ -21,17 +18,14 @@ from creme import linear_model
 from creme import multiclass
 from creme import multioutput
 from creme import naive_bayes
-from creme import optim
 from creme import preprocessing
 from creme import stats
-from creme import stream
 from creme import time_series
 from creme import tree
 from creme import utils
 from creme.compat.sklearn import CremeBaseWrapper
 from creme.compat.sklearn import SKLBaseWrapper
 
-from sklearn import datasets
 
 def get_all_estimators():
 
@@ -136,56 +130,3 @@ def get_all_estimators():
 )
 def test_check_estimator(estimator):
     utils.estimator_checks.check_estimator(estimator)
-
-
-@pytest.mark.parametrize(
-    'optimizer, initializer',
-    itertools.product([
-        pytest.param(copy.deepcopy(optimizer), id=str(optimizer))
-            for optimizer in [
-                optim.AdaBound(),
-                optim.AdaDelta(),
-                optim.AdaGrad(),
-                optim.AdaMax(),
-                optim.Adam(),
-                optim.Momentum(),
-                optim.NesterovMomentum(),
-                optim.RMSProp(),
-                optim.SGD()
-            ]
-        ],
-        [
-            pytest.param(copy.deepcopy(initializer), id=str(initializer))
-            for initializer in [
-                optim.initializers.Zeros(),
-                optim.initializers.Constant(value=3.14),
-                optim.initializers.Normal(mu=0, sigma=1, random_state=42)
-            ]
-        ]
-    )
-)
-def test_check_gradient(optimizer, initializer):
-    # Test gradient finite difference with linear regression
-    X_y = stream.iter_sklearn_dataset(
-        dataset=datasets.load_boston(),
-        shuffle=True,
-        random_state=42
-    )
-
-    scaler = preprocessing.StandardScaler()
-    model = linear_model.LinearRegression(initializer=initializer)
-
-    for x, y in X_y:
-        x = scaler.fit_one(x).transform_one(x)
-        utils.estimator_checks.check_gradient_finite_difference(model, x, y, delta = 1e-15)
-        model.fit_one(x, y)
-
-    # Test gradient finite difference with logistic regression
-    X_y = creme.datasets.fetch_electricity()
-    scaler = preprocessing.StandardScaler()
-    model = linear_model.LogisticRegression(initializer=initializer)
-
-    for x, y in X_y:
-        x = scaler.fit_one(x).transform_one(x)
-        utils.estimator_checks.check_gradient_finite_difference(model, x, y, delta = 1e-15)
-        model.fit_one(x, y)
