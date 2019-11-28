74a5ab87a3164bc9e016aa202ef132a85b180900
Vaysse <robinvaysse@hotmail.fr>
2019-11-28 09:39:25 +0100

    Add correct bias option to AMSGrad

--- creme/optim/amsgrad.py
+++ creme/optim/amsgrad.py
@@ -41,21 +41,31 @@ class AMSGrad(base.Optimizer):
 
     """
 
-    def __init__(self, lr=0.1, beta_1=0.9, beta_2=0.999, eps=1e-8):
+    def __init__(self, lr=0.1, beta_1=0.9, beta_2=0.999, eps=1e-8, correct_bias=True):
         super().__init__(lr)
         self.beta_1 = beta_1
         self.beta_2 = beta_2
         self.eps = eps
+        self.correct_bias = correct_bias
         self.m = collections.defaultdict(float)
         self.v = collections.defaultdict(float)
         self.v_hat = collections.defaultdict(float)
 
     def _update_after_pred(self, w, g):
 
+        if self.correct_bias:
+            # Correct bias for `v`
+            learning_rate = self.learning_rate * (1 - self.beta_2 ** (self.n_iterations + 1))**0.5
+            # Correct bias for `m`
+            learning_rate /= 1 - self.beta_1 ** (self.n_iterations + 1)
+        else:
+            learning_rate = self.learning_rate
+
         for i, gi in g.items():
             self.m[i] = self.beta_1 * self.m[i] + (1 - self.beta_1) * gi
             self.v[i] = self.beta_2 * self.v[i] + (1 - self.beta_2) * gi ** 2
             self.v_hat[i] = max(self.v_hat[i], self.v[i])
-            w[i] -= self.learning_rate * self.m[i] / (self.v_hat[i] ** 0.5 + self.eps)
+
+            w[i] -= learning_rate * self.m[i] / (self.v_hat[i] ** 0.5 + self.eps)
 
         return w
