Fix bug in normalization calculation and perceptron leafs in HoeffdingTreeRegressor
- Rename sd_squared in the more explicit var
- Rename sd in std (R convention -> NumPy convention)
- Protect normalization computation against division by zero
- Add a private method to calculate standard deviation incrementally
- Fix training implementation of perceptron leaf nodes
- Update calculation when using perceptron leafs in HTR

src/skmultiflow/trees/nodes/inactive_learning_node_perceptron.py
@@ -1,5 +1,10 @@
+from copy import deepcopy
+
 import numpy as np
+
+from skmultiflow.utils import check_random_state
 from skmultiflow.trees.nodes import InactiveLearningNode
+from skmultiflow.trees.nodes.active_learning_node_perceptron import compute_sd
 
 
 class InactiveLearningNodePerceptron(InactiveLearningNode):
@@ -9,22 +14,28 @@ class InactiveLearningNodePerceptron(InactiveLearningNode):
     Parameters
     ----------
     initial_class_observations: dict
-        In regression tasks this dictionary carries the sufficient to perform
-        online variance calculation. They refer to the number of observations
-        (key '0'), the sum of the target values (key '1'), and the sum of the
-        squared target values (key '2').
-    perceptron_weight: np.ndarray(n_features) or None, optional (default=None)
-        (default=None)
-        The weights for the linear models. If
-        not passed, uniform values in the range [-1, 1] are used.
+        In regression tasks this dictionary carries the sufficient statistics
+        to perform online variance calculation. They refer to the number of
+        observations (key '0'), the sum of the target values (key '1'), and
+        the sum of the squared target values (key '2').
+    perceptron_node: ActiveLearningNodePerceptron (default=None)
+        A node containing statistics about observed data.
+    random_state: int, RandomState instance or None, optional (default=None)
+        If int, random_state is the seed used by the random number generator;
+        If RandomState instance, random_state is the random number generator;
+        If None, the random number generator is the RandomState instance used
+        by `np.random`.
     """
-    def __init__(self, initial_class_observations, perceptron_weight=None):
+    def __init__(self, initial_class_observations, perceptron_node=None, random_state=None):
         """ InactiveLearningNodePerceptron class constructor."""
         super().__init__(initial_class_observations)
-        if perceptron_weight is None:
-            self.perceptron_weight = []
+        self.random_state = check_random_state(random_state)
+        self.samples_seen = 0
+        if perceptron_node is None:
+            self.perceptron_weight = None
         else:
-            self.perceptron_weight = perceptron_weight
+            self.perceptron_weight = deepcopy(perceptron_node.perceptron_weight)
+            self.samples_seen = perceptron_node.samples_seen
 
     def learn_from_instance(self, X, y, weight, rht):
         """Update the node with the provided instance.
@@ -42,26 +53,27 @@ class InactiveLearningNodePerceptron(InactiveLearningNode):
 
         """
         if self.perceptron_weight is None:
-            self.perceptron_weight = np.random.uniform(-1, 1, len(X)+1)
+            self.perceptron_weight = self.random_state.uniform(-1, 1, len(X) + 1)
 
         try:
             self._observed_class_distribution[0] += weight
+            self._observed_class_distribution[1] += y * weight
+            self._observed_class_distribution[2] += y * y * weight
         except KeyError:
             self._observed_class_distribution[0] = weight
+            self._observed_class_distribution[1] = y * weight
+            self._observed_class_distribution[2] = y * y * weight
+
+        # Update perceptron
+        self.samples_seen = self._observed_class_distribution[0]
 
         if rht.learning_ratio_const:
             learning_ratio = rht.learning_ratio_perceptron
         else:
-            learning_ratio = rht.learning_ratio_perceptron / 1 + \
-                self._observed_class_distribution[0] * rht.learning_ratio_decay
+            learning_ratio = rht.learning_ratio_perceptron / \
+                             (1 + self.samples_seen * rht.learning_ratio_decay)
 
-        try:
-            self._observed_class_distribution[1] += y * weight
-            self._observed_class_distribution[2] += y * y * weight
-        except KeyError:
-            self._observed_class_distribution[1] = y * weight
-            self._observed_class_distribution[2] = y * y * weight
-
+        # Loop for compatibility with bagging methods
         for i in range(int(weight)):
             self.update_weights(X, y, learning_ratio, rht)
 
@@ -80,11 +92,7 @@ class InactiveLearningNodePerceptron(InactiveLearningNode):
             Regression Hoeffding Tree to update.
         """
         normalized_sample = ht.normalize_sample(X)
-        normalized_pred = self.predict(normalized_sample)
+        normalized_pred = np.dot(self.perceptron_weight, normalized_sample)
         normalized_target_value = ht.normalize_target_value(y)
-        self.perceptron_weight += learning_ratio * \
-            np.multiply((normalized_pred - normalized_target_value),
-                        normalized_sample)
-
-    def predict(self, X):
-        return np.dot(self.perceptron_weight, X[0])
+        delta = normalized_target_value - normalized_pred
+        self.perceptron_weight = self.perceptron_weight + learning_ratio * delta * normalized_sample