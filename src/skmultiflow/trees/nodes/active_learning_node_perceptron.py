99e8ed2a71af827267eea888a2e2c55ff7f36e49
Jacob <jacob.montiel@gmail.com>
2020-01-12 14:18:24 +1300

    Update active_learning_node_perceptron.py
    Add non-negative check in standard deviation calculation to avoid NaN values.
    
    Former-commit-id: e4743532ccac12422ad1eb5cd272d251c1aba89f

--- src/skmultiflow/trees/nodes/active_learning_node_perceptron.py
+++ src/skmultiflow/trees/nodes/active_learning_node_perceptron.py
@@ -120,6 +120,7 @@ class ActiveLearningNodePerceptron(ActiveLearningNodeForRegression):
 
 def compute_sd(square_val: float, val: float, size: float):
     if size > 1:
-        return np.sqrt((square_val - ((val * val) / size)) / size)
-    else:
-        return 0.0
+        a = square_val - ((val * val) / size)
+        if a > 0:
+            return np.sqrt(a / size)
+    return 0.0
