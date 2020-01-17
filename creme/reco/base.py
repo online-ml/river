fb4a671cfbf36194beea464dbfd5e0fa52cae03f
gbolmier <geoffrey.bolmier@gmail.com>
2020-01-17 12:55:41 +0100

    Update Recommender estimator
    Add base.Estimator inheritance and support for dict entries

--- creme/reco/base.py
+++ creme/reco/base.py
@@ -1,12 +1,40 @@
 import abc
 
+from .. import base
 
-class Recommender(abc.ABC):
+
+class Recommender(base.Estimator):
+    """A recommender."""
+
+    def fit_one(self, x, y):
+        return self._fit_one(x['user'], x['item'], y)
+
+    def predict_one(self, x):
+        return self._predict_one(x['user'], x['item'])
 
     @abc.abstractmethod
-    def fit_one(self, row_id, col_id, value):
-        pass
+    def _fit_one(self, user, item, y):
+        """Fits a ``user``-``item`` pair and a real-valued target ``y``.
+
+        Parameters:
+            user (int or str)
+            item (int or str)
+            y (float)
+
+        Returns:
+            self: object
+
+        """
 
     @abc.abstractmethod
-    def predict_one(self, row_id, col_id):
-        pass
+    def _predict_one(self, user, item):
+        """Predicts the target value of a set of features ``x``.
+
+        Parameters:
+            user (int or str)
+            item (int or str)
+
+        Returns:
+            float: The prediction.
+
+        """
