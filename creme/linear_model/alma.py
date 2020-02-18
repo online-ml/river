f53a939db9b64f26e9b3cbcca74b006d98fdcf5a
Max Halford <maxhalford25@gmail.com>
2020-02-18 15:24:48 +0100

    Fix tests
    Former-commit-id: 904ef15b3185772e597f22848e36eb1b52e92622

--- creme/linear_model/alma.py
+++ creme/linear_model/alma.py
@@ -22,9 +22,9 @@ class ALMAClassifier(base.BinaryClassifier):
         ::
 
             >>> from creme import datasets
+            >>> from creme import linear_model
             >>> from creme import metrics
             >>> from creme import model_selection
-            >>> from creme import optim
             >>> from creme import preprocessing
 
             >>> X_y = datasets.Phishing()
