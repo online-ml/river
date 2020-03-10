1e576f58ce071108f44f273dc22794804e4fdf8b
Max Halford <maxhalford25@gmail.com>
2020-03-10 17:30:34 +0100

    Add ImageSegments dataset
    Former-commit-id: 3bb37987c22f411b119ddd7f4c94d73b553d0abc

--- creme/ensemble/bagging.py
+++ creme/ensemble/bagging.py
@@ -2,7 +2,7 @@ import collections
 import copy
 import statistics
 
-from sklearn import utils
+import numpy as np
 
 from .. import base
 
@@ -17,7 +17,7 @@ class BaseBagging(base.Wrapper, base.Ensemble):
         self.n_models = n_models
         self.model = model
         self.seed = seed
-        self._rng = utils.check_random_state(seed)
+        self._rng = np.random.RandomState(seed)
 
     @property
     def _model(self):
