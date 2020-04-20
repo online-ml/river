604a3748cda029e15346e07f75e2c07db93cbdc2
Joshua Sant'Anna <45068597+dataJSA@users.noreply.github.com>
2020-04-20 21:18:05 +0200

    Add adpredictor module

---
+++ creme/linear_model/adpredictor.py
@@ -0,0 +1,27 @@
+import collections
+import math 
+import numbers
+
+import numpy as np 
+from .. import base 
+from .. import optim
+from .. import proba
+
+
+
+__all__ = [
+    'AdPredictor'
+]
+
+
+class BOPR:
+    """Bayesian Online Linear Regression Model.
+    """
+    pass
+
+
+class AdPredictor(BOPR, base.Regressor):
+    """AdPredictor.
+    """
+
+    
