b3d879cc939e6175f4e0cd4bc1c3ddf1fd20610b
Max Halford <maxhalford25@gmail.com>
2020-06-29 09:57:32 +0200

    Add default .pylintrc file
    Former-commit-id: d2103053df594450ebc05ca9f9f05d3addecb4be

--- creme/compat/__init__.py
+++ creme/compat/__init__.py
@@ -1,9 +1,9 @@
 """Compatibility tools.
 
-This module contains adapters for making ``creme`` estimators compatible with other libraries, and
+This module contains adapters for making `creme` estimators compatible with other libraries, and
 vice-versa whenever possible. The relevant adapters will only be usable if you have installed the
-necessary library. For instance, to use the ``compat.convert_sklearn_to_creme`` function, you have
-to install scikit-learn.
+necessary library. For instance, you have to install scikit-learn in order to use the
+`compat.convert_sklearn_to_creme` function.
 
 """
 import typing
@@ -11,14 +11,14 @@ import typing
 __all__: typing.List[str] = []
 
 try:
-    from .sklearn import convert_creme_to_sklearn
-    from .sklearn import convert_sklearn_to_creme
-    from .sklearn import SKL2CremeClassifier
-    from .sklearn import SKL2CremeRegressor
-    from .sklearn import Creme2SKLRegressor
-    from .sklearn import Creme2SKLClassifier
-    from .sklearn import Creme2SKLClusterer
-    from .sklearn import Creme2SKLTransformer
+    from .creme_to_sklearn import convert_creme_to_sklearn
+    from .creme_to_sklearn import Creme2SKLRegressor
+    from .creme_to_sklearn import Creme2SKLClassifier
+    from .creme_to_sklearn import Creme2SKLClusterer
+    from .creme_to_sklearn import Creme2SKLTransformer
+    from .sklearn_to_creme import convert_sklearn_to_creme
+    from .sklearn_to_creme import SKL2CremeClassifier
+    from .sklearn_to_creme import SKL2CremeRegressor
 
     __all__ += [
         'convert_creme_to_sklearn',
--- creme/compat/test_sklearn.py
+++ creme/compat/test_sklearn.py
@@ -6,7 +6,6 @@ from creme import cluster
 from creme import compat
 from creme import linear_model
 from creme import preprocessing
-from sklearn import linear_model as sk_linear_model
 
 
 @pytest.mark.parametrize('estimator', [
@@ -17,6 +16,6 @@ from sklearn import linear_model as sk_linear_model
         cluster.KMeans(seed=42)
     ]
 ])
-def test_sklearn_check_estimator(estimator: base.Estimator):
+def test_creme_to_sklearn_check_estimator(estimator: base.Estimator):
     skl_estimator = compat.convert_creme_to_sklearn(estimator)
     estimator_checks.check_estimator(skl_estimator)
