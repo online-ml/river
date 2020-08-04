d1dea0475993955251afa2639f606e7f398e20f5
smastelini <saulomastelini@gmail.com>
2020-08-04 11:13:48 -0300

    add data conversion utilities
    Former-commit-id: a9afe524051566ee6a3157e9cf252cca7aed4436

--- creme/utils/__init__.py
+++ creme/utils/__init__.py
@@ -10,13 +10,18 @@ from .skyline import Skyline
 from .vectordict import VectorDict
 from .window import Window
 from .window import SortedWindow
+# Data convertion utilities: to be removed in the future
+from .data_conversion import dict2numpy
+from .data_conversion import numpy2dict
 
 
 __all__ = [
-    'check_estimator'
+    'check_estimator',
+    'dict2numpy',
     'expand_param_grid',
     'inspect',
     'Histogram',
+    'numpy2dict',
     'SDFT',
     'Skyline',
     'SortedWindow',
