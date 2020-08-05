e53db754ab4cec8695d7550313fda3f3dc5d136d
smastelini <saulomastelini@gmail.com>
2020-08-05 18:41:57 -0300

    update creme/neighbors/__init__.py

--- creme/neighbors/__init__.py
+++ creme/neighbors/__init__.py
@@ -1,9 +1,13 @@
 """Neighbors-based learning."""
 from .knn import KNeighborsClassifier
 from .knn import KNeighborsRegressor
+from .knn_classifier import KNNClassifier
+from .knn_regressor import KNNRegressor
 
 
 __all__ = [
     'KNeighborsClassifier',
-    'KNeighborsRegressor'
+    'KNeighborsRegressor',
+    'KNNClassifier',
+    'KNNRegressor'
 ]
