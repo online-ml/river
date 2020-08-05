cea0079253607fa0b7e9eaeb02a2b360e36e629a
smastelini <saulomastelini@gmail.com>
2020-08-05 18:39:47 -0300

    make sure utilities can be imported
    Former-commit-id: e4ba84b2698f34b151132a2de1f6dc53b7e2c3e7

--- creme/utils/__init__.py
+++ creme/utils/__init__.py
@@ -2,6 +2,7 @@
 from . import inspect
 from . import math
 from . import pretty
+from . import skmultiflow_utils
 from .estimator_checks import check_estimator
 from .histogram import Histogram
 from .param_grid import expand_param_grid
@@ -20,6 +21,7 @@ __all__ = [
     'pretty',
     'Histogram',
     'SDFT',
+    'skmultiflow_utils',
     'Skyline',
     'SortedWindow',
     'VectorDict',
