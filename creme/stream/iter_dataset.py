d6cad950a522a0798b99174bce5f5bd1652ab051
Max Halford <maxhalford25@gmail.com>
2020-09-15 12:14:57 +0200

    Add iter_dataset

---
+++ creme/stream/iter_dataset.py
@@ -0,0 +1,30 @@
+import inspect
+import importlib
+
+
+__all__ = [
+    'available_datasets',
+    'iter_dataset'
+]
+
+
+def _available_datasets():
+    return dict(inspect.getmembers(importlib.import_module('creme.datasets'), inspect.isclass))
+
+
+def available_datasets():
+    """Return the list of available datasets.
+
+    Each element in the returned list can be passed to `stream.iter_dataset` to iterate over it.
+
+    """
+    return list(_available_datasets().keys())
+
+
+def iter_dataset(name: str, **kwargs):
+    """Iterate over a dataset.
+
+    You can check the list of available datasets by calling `stream.available_datasets`.
+
+    """
+    return _available_datasets()[name](**kwargs)
