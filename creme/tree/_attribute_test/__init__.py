6e7b2f6928b6fd27f87941d38b14e0bd34629a3a
smastelini <saulomastelini@gmail.com>
2020-10-06 12:09:52 -0300

    Convert ARFHoeffdingTreeRegressor and beautify code

--- creme/tree/_attribute_test/__init__.py
+++ creme/tree/_attribute_test/__init__.py
@@ -4,5 +4,10 @@ from .nominal_attribute_binary_test import NominalAttributeBinaryTest
 from .nominal_attribute_multiway_test import NominalAttributeMultiwayTest
 from .numeric_attribute_binary_test import NumericAttributeBinaryTest
 
-__all__ = ["AttributeSplitSuggestion", "InstanceConditionalTest", "NominalAttributeBinaryTest",
-           "NominalAttributeMultiwayTest", "NumericAttributeBinaryTest"]
+__all__ = [
+    'AttributeSplitSuggestion',
+    'InstanceConditionalTest',
+    'NominalAttributeBinaryTest',
+    'NominalAttributeMultiwayTest',
+    'NumericAttributeBinaryTest'
+]
--- creme/tree/arf_hoeffding_tree_regressor.py
+++ creme/tree/arf_hoeffding_tree_regressor.py
@@ -1,10 +1,17 @@
-from skmultiflow.trees import HoeffdingTreeRegressor
-from skmultiflow.utils import check_random_state
+from copy import deepcopy
+
+from creme.tree import HoeffdingTreeRegressor
+from creme.utils.skmultiflow_utils import check_random_state
+
+from creme import base
+from creme import linear_model
 
 from ._nodes import RandomActiveLearningNodeMean
 from ._nodes import InactiveLearningNodeMean
-from ._nodes import RandomActiveLearningNodePerceptron
-from ._nodes import InactiveLearningNodePerceptron
+from ._nodes import RandomActiveLearningNodeModel
+from ._nodes import InactiveLearningNodeModel
+from ._nodes import RandomActiveLearningNodeAdaptive
+from ._nodes import InactiveLearningNodeAdaptive
 
 
 class ARFHoeffdingTreeRegressor(HoeffdingTreeRegressor):
@@ -12,129 +19,126 @@ class ARFHoeffdingTreeRegressor(HoeffdingTreeRegressor):
 
     Parameters
     ----------
-    max_byte_size: int (default=33554432)
-        Maximum memory consumed by the tree.
-    memory_estimate_period: int (default=2000000)
-        Number of instances between memory consumption checks.
-    grace_period: int (default=50)
+    grace_period
         Number of instances a leaf should observe between split attempts.
-    split_confidence: float (default=0.01)
+    split_confidence
         Allowed error in split decision, a value closer to 0 takes longer to decide.
-    tie_threshold: float (default=0.05)
+    tie_threshold
         Threshold below which a split will be forced to break ties.
-    binary_split: boolean (default=False)
+    binary_split:
         If True, only allow binary splits.
-    stop_mem_management: boolean (default=False)
-        If True, stop growing as soon as memory limit is hit.
-    remove_poor_atts: boolean (default=False)
-        If True, disable poor attributes.
-    no_preprune: boolean (default=False)
-        If True, disable pre-pruning.
-    leaf_prediction: string (default='perceptron')
+    leaf_prediction
         | Prediction mechanism used at leafs.
         | 'mean' - Target mean
-        | 'perceptron' - Perceptron
-    nominal_attributes: list, optional (default: None)
-        List of Nominal attributes. If empty, then assume that all attributes are numerical.
-    learning_ratio_perceptron: float (default: 0.02)
-        The learning rate of the perceptron.
-    learning_ratio_decay: float (default: 0.001)
-        Decay multiplier for the learning rate of the perceptron
-    learning_ratio_const: boolean (default: True)
-        If False the learning ratio will decay with the number of examples seen
-    max_features: int (default: 2)
+        | 'model' - Uses the model defined in `leaf_model`
+        | 'adaptive' - Chooses between 'mean' and 'model' dynamically
+    leaf_model
+        The regression model used to provide responses if `leaf_prediction='model'`.
+    model_selector_decay
+        The exponential decaying factor applied to the learning models' squared errors, that
+        are monitored if `leaf_prediction='adaptive'`. Must be between `0` and `1`. The closer
+        to `1`, the more importance is going to be given to past observations. On the other hand,
+        if its value approaches `0`, the recent observed errors are going to have more influence
+        on the final decision.
+    nominal_attributes
+        List of Nominal attributes identifiers. If empty, then assume that all numeric attributes
+        should be treated as continuous.
+    max_features
         Number of randomly selected features to act as split candidates at each attempt.
-    random_state: int, RandomState instance or None, optional (default=None)
-       If int, random_state is the seed used by the random number generator;
-       If RandomState instance, random_state is the random number generator;
-       If None, the random number generator is the RandomState instance used
-       by `np.random`. Used when leaf_prediction is 'perceptron' and to randomly pick subsets of
-       features at each split.
+    random_state
+            If int, random_state is the seed used by the random number generator;
+            If RandomState instance, random_state is the random number generator;
+            If None, the random number generator is the RandomState instance
+            used by `np.random`.
+    **kwargs
+        Other parameters passed to river.tree.DecisionTree.
 
     This is the base-estimator of the Adaptive Random Forest Regressor ensemble learner (see
-    skmultiflow.meta.adaptive_random_forest_regressor). This Hoeffding Tree Regressor includes a
-    max_features parameter, which defines the number of randomly selected features to be considered
-    at each split.
+    `river.ensemble.AdaptiveRandomForestRegressor`). This Hoeffding Tree Regressor includes a
+    max_features parameter, which defines the number of randomly selected features to be
+    considered at each split.
     """
 
     def __init__(self,
-                 max_byte_size=33554432,
-                 memory_estimate_period=2000000,
-                 grace_period=50,
-                 split_confidence=0.01,
-                 tie_threshold=0.05,
-                 binary_split=False,
-                 stop_mem_management=False,
-                 remove_poor_atts=False,
-                 leaf_prediction="perceptron",
-                 no_preprune=False,
-                 nominal_attributes=None,
-                 learning_ratio_perceptron=0.02,
-                 learning_ratio_decay=0.001,
-                 learning_ratio_const=True,
-                 max_features=2,
-                 random_state=None):
-        super().__init__(max_byte_size=max_byte_size,
-                         memory_estimate_period=memory_estimate_period,
-                         grace_period=grace_period,
+                 grace_period: int = 200,
+                 split_confidence: float = 1e-7,
+                 tie_threshold: float = 0.05,
+                 binary_split: bool = False,
+                 leaf_prediction: str = 'model',
+                 leaf_model: base.Regressor = linear_model.LinearRegression(),
+                 model_selector_decay: float = 0.95,
+                 nominal_attributes: list = None,
+                 max_features: int = 2,
+                 random_state=None,
+                 **kwargs):
+        super().__init__(grace_period=grace_period,
                          split_confidence=split_confidence,
                          tie_threshold=tie_threshold,
                          binary_split=binary_split,
-                         stop_mem_management=stop_mem_management,
-                         remove_poor_atts=remove_poor_atts,
                          leaf_prediction=leaf_prediction,
-                         no_preprune=no_preprune,
+                         leaf_model=leaf_model,
+                         model_selector_decay=model_selector_decay,
                          nominal_attributes=nominal_attributes,
-                         learning_ratio_perceptron=learning_ratio_perceptron,
-                         learning_ratio_decay=learning_ratio_decay,
-                         learning_ratio_const=learning_ratio_const)
+                         **kwargs)
 
         self.max_features = max_features
         self.random_state = random_state
         self._random_state = check_random_state(self.random_state)
 
-    def _new_learning_node(self, initial_stats=None, parent_node=None,
-                           is_active=True):
-        """Create a new learning node. The type of learning node depends on the tree
-        configuration."""
+    def _new_learning_node(self, initial_stats=None, parent=None, is_active=True):
+        """Create a new learning node.
+
+        The type of learning node depends on the tree configuration.
+        """
         if initial_stats is None:
             initial_stats = {}
 
+        if parent is not None:
+            depth = parent.depth + 1
+        else:
+            depth = 0
+
         # Generate a random seed for the new learning node
         random_state = self._random_state.randint(0, 4294967295, dtype='u8')
+
+        if self.leaf_prediction in {self._MODEL, self._ADAPTIVE}:
+            if parent is None:
+                # TODO: change to appropriate 'clone' method
+                leaf_model = self.leaf_model.__class__(**self.leaf_model._get_params())
+            else:
+                leaf_model = deepcopy(parent._leaf_model)
+
         if is_active:
             if self.leaf_prediction == self._TARGET_MEAN:
                 return RandomActiveLearningNodeMean(
-                    initial_stats, max_features=self.max_features, random_state=random_state)
-            elif self.leaf_prediction == self._PERCEPTRON:
-                return RandomActiveLearningNodePerceptron(
-                    initial_stats, max_features=self.max_features, parent_node=parent_node,
-                    random_state=random_state)
+                    initial_stats, depth, self.max_features, random_state)
+            elif self.leaf_prediction == self._MODEL:
+                return RandomActiveLearningNodeModel(
+                    initial_stats, depth, leaf_model, self.max_features, random_state)
+            else:  # adaptive learning node
+                new_adaptive = RandomActiveLearningNodeAdaptive(
+                    initial_stats, depth, leaf_model, self.max_features, random_state)
+                if parent is not None:
+                    new_adaptive._fmse_mean = parent._fmse_mean
+                    new_adaptive._fmse_model = parent._fmse_model
+
+                return new_adaptive
         else:
             if self.leaf_prediction == self._TARGET_MEAN:
-                return InactiveLearningNodeMean(initial_stats)
-            elif self.leaf_prediction == self._PERCEPTRON:
-                return InactiveLearningNodePerceptron(initial_stats, parent_node,
-                                                      random_state=random_state)
+                return InactiveLearningNodeMean(initial_stats, depth)
+            elif self.leaf_prediction == self._MODEL:
+                return InactiveLearningNodeModel(initial_stats, depth, leaf_model)
+            else:  # adaptive learning node
+                new_adaptive = InactiveLearningNodeAdaptive(initial_stats, depth, leaf_model)
+                if parent is not None:
+                    new_adaptive._fmse_mean = parent._fmse_mean
+                    new_adaptive._fmse_mean = parent._fmse_model
+
+                return new_adaptive
 
     def reset(self):
         super().reset()
         self._random_state = check_random_state(self.random_state)
 
     def new_instance(self):
-        return ARFHoeffdingTreeRegressor(max_byte_size=self.max_byte_size,
-                                         memory_estimate_period=self.memory_estimate_period,
-                                         grace_period=self.grace_period,
-                                         split_confidence=self.split_confidence,
-                                         tie_threshold=self.tie_threshold,
-                                         binary_split=self.binary_split,
-                                         stop_mem_management=self.stop_mem_management,
-                                         remove_poor_atts=self.remove_poor_atts,
-                                         leaf_prediction=self.leaf_prediction,
-                                         no_preprune=self.no_preprune,
-                                         nominal_attributes=self.nominal_attributes,
-                                         learning_ratio_perceptron=self.learning_ratio_perceptron,
-                                         learning_ratio_decay=self.learning_ratio_decay,
-                                         learning_ratio_const=self.learning_ratio_const,
-                                         max_features=self.max_features,
-                                         random_state=self.random_state)
+        return self.__class__(**self._get_params())
