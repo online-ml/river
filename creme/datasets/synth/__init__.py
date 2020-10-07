14d91e6facec2bdd2417ab828d777102d59e16b5
Jacob Montiel <17923265+jacobmontiel@users.noreply.github.com>
2020-10-07 19:02:08 +1300

    Add Hyperplane generator
    Former-commit-id: a548fc467813f46170a26ef857bb17246f38089e

--- creme/datasets/synth/__init__.py
+++ creme/datasets/synth/__init__.py
@@ -5,6 +5,7 @@ Each synthetic dataset is an infinite generator.
 """
 from .agrawal import Agrawal
 from .anomaly_sine import AnomalySine
+from .hyper_plane import Hyperplane
 from .friedman import Friedman
 from .sea import SEA
 
@@ -13,5 +14,6 @@ __all__ = [
     'Agrawal',
     'AnomalySine',
     'Friedman',
+    'Hyperplane',
     'SEA'
 ]
