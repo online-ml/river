a97318e5f3d6cc6754c52d826f2d1d894afd1f65
Jacob Montiel <17923265+jacobmontiel@users.noreply.github.com>
2020-10-09 14:57:19 +1300

    Add RandomRBFDrift generator
    Former-commit-id: 50099aaf18f6beaecbb1087ee25aead2edb4e34c

--- creme/datasets/synth/random_rbf.py
+++ creme/datasets/synth/random_rbf.py
@@ -1,5 +1,5 @@
 import numpy as np
-import math
+import warnings
 
 from .. import base
 from creme.utils.skmultiflow_utils import check_random_state
@@ -76,22 +76,25 @@ class RandomRBF(base.SyntheticDataset):
         rng_sample = check_random_state(self.seed_sample)
 
         while True:
-            x = dict()
-            idx = random_index_based_on_weights(self.centroid_weights, rng_sample)
-            current_centroid = self.centroids[idx]
-            att_vals = dict()
-            magnitude = 0.0
-            for key in self.feature_names:
-                att_vals[key] = (rng_sample.rand() * 2.0) - 1.0
-                magnitude += att_vals[key] * att_vals[key]
-            magnitude = math.sqrt(magnitude)
-            desired_mag = rng_sample.normal() * current_centroid.std_dev
-            scale = desired_mag / magnitude
-            for idx, key in enumerate(self.feature_names):
-                x[key] = current_centroid.centre[idx] + att_vals[key] * scale
-            y = current_centroid.class_label
-
+            x, y = self._generate_sample(rng_sample)
             yield x, y
+
+    def _generate_sample(self, rng_sample: np.random.RandomState):
+        x = dict()
+        idx = random_index_based_on_weights(self.centroid_weights, rng_sample)
+        current_centroid = self.centroids[idx]
+        att_vals = dict()
+        magnitude = 0.0
+        for key in self.feature_names:
+            att_vals[key] = (rng_sample.rand() * 2.0) - 1.0
+            magnitude += att_vals[key] * att_vals[key]
+        magnitude = np.sqrt(magnitude)
+        desired_mag = rng_sample.normal() * current_centroid.std_dev
+        scale = desired_mag / magnitude
+        for idx, key in enumerate(self.feature_names):
+            x[key] = current_centroid.centre[idx] + att_vals[key] * scale
+        y = current_centroid.class_label
+        return x, y
 
     def _generate_centroids(self):
         """ Generates centroids
@@ -114,9 +117,127 @@ class RandomRBF(base.SyntheticDataset):
             self.centroid_weights.append(rng_model.rand())
 
 
+class RandomRBFDrift(RandomRBF):
+    """Random Radial Basis Function generator with concept drift.
+
+    This class is an extension from the RandomRBF. It works as the parent
+    class, except that drift can be introduced in instances of this class.
+
+    The drift is created by adding a "speed" to certain centroids. As the
+    samples are generated each of the moving centroids' centers is
+    changed by an amount determined by its speed.
+
+    Parameters
+    ----------
+    seed_model
+        Model's seed to generate centroids
+        If int, `seed` is used to seed the random number generator;
+        If RandomState instance, `seed` is the random number generator;
+        If None, the random number generator is the `RandomState` instance used
+        by `np.random`.
+    seed_sample
+        Sample's seed
+        If int, `seed` is used to seed the random number generator;
+        If RandomState instance, `seed` is the random number generator;
+        If None, the random number generator is the `RandomState` instance used
+        by `np.random`.
+    n_classes
+        The number of class labels to generate.
+    n_features
+        The number of numerical features to generate.
+    n_centroids
+        The number of centroids to generate.
+    change_speed
+        The concept drift speed.
+    n_drift_centroids
+        The number of centroids that will drift.
+
+    Examples
+    --------
+    >>> from creme import synth
+    >>>
+    >>> dataset = synth.RandomRBFDrift(seed_model=42, seed_sample=42,
+    ...                                n_classes=4, n_features=4, n_centroids=20,
+    ...                                change_speed=0.87, n_drift_centroids=10)
+    >>>
+    >>> for x, y in dataset.take(5):
+    ...     print(x, y)
+    {'x_0': 1.196522912133933, 'x_1': 0.5729342869687664, 'x_2': 0.8607633992917512, 'x_3': 0.5888739485921395} 0
+    {'x_0': 0.33834301976555137, 'x_1': 0.8072293546884879, 'x_2': 0.8051013792705997, 'x_3': 0.41400330978417743} 3
+    {'x_0': 0.5362752113124996, 'x_1': -0.28673094640578217, 'x_2': 0.09628403108434666, 'x_3': 0.8974062767425162} 2
+    {'x_0': 1.1875780405113046, 'x_1': 1.0385542779679864, 'x_2': 0.8323812138717979, 'x_3': -0.055321659374305054} 2
+    {'x_0': 0.3256964019833385, 'x_1': 0.9206200355843615, 'x_2': 0.859567372541451, 'x_3': 0.5907446084774316} 2
+
+
+    """
+
+    def __init__(self, seed_model: int or np.random.RandomState = None,
+                 seed_sample: int or np.random.RandomState = None,
+                 n_classes: int = 2, n_features: int = 10, n_centroids: int = 50,
+                 change_speed: float = 0.0, n_drift_centroids: int = 50):
+        super().__init__(seed_model=seed_model,
+                         seed_sample=seed_sample,
+                         n_classes=n_classes,
+                         n_features=n_features,
+                         n_centroids=n_centroids)
+        self.change_speed = change_speed
+        if n_drift_centroids <= n_centroids:
+            self.n_drift_centroids = n_drift_centroids
+        else:
+            warnings.warn(f"n_drift_centroids ({n_drift_centroids}) can not be larger than"
+                          f"n_centroids ({n_centroids}). Will use n_centroids instead.")
+            self.n_drift_centroids = n_centroids
+        self.centroid_speed = None
+
+    def __iter__(self):
+        self._generate_centroids()
+        rng_sample = check_random_state(self.seed_sample)
+
+        while True:
+            # Move centroids
+            for i in range(self.n_drift_centroids):
+                for j in range(self.n_features):
+                    self.centroids[i].centre[j] += self.centroid_speed[i][j] * self.change_speed
+
+                    if (self.centroids[i].centre[j] > 1) or (self.centroids[i].centre[j] < 0):
+                        self.centroids[i].centre[j] = 1 if (self.centroids[i].centre[j] > 1) else 0
+                        self.centroid_speed[i][j] = -self.centroid_speed[i][j]
+
+            x, y = self._generate_sample(rng_sample)
+            yield x, y
+
+    def _generate_centroids(self):
+        """Generates centroids
+
+        The centroids are generated just as it is done in the parent class,
+        an extra step is taken to introduce drift, if there is any.
+
+        To configure the drift, random offset speeds are chosen for
+        `n_drift_centroids` centroids. Finally, the speed is normalized.
+
+        """
+        super()._generate_centroids()
+        rng_model = check_random_state(self.seed_model)
+        self.centroid_speed = []
+
+        for i in range(self.n_drift_centroids):
+            rand_speed = np.zeros(self.n_features)
+            norm_speed = 0.0
+
+            for j in range(self.n_features):
+                rand_speed[j] = rng_model.rand()
+                norm_speed += rand_speed[j] * rand_speed[j]
+
+            norm_speed = np.sqrt(norm_speed)
+
+            for j in range(self.n_features):
+                rand_speed[j] /= norm_speed
+
+            self.centroid_speed.append(rand_speed)
+
+
 class Centroid:
     """ Class that stores a centroid's attributes. """
-
     def __init__(self):
         self.centre = None
         self.class_label = None
