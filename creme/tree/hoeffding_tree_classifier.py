2759b2c8b4634f6d26562ef0c4e1e988592984ce
smastelini <saulomastelini@gmail.com>
2020-10-19 17:49:11 -0300

    htc example start

--- creme/tree/hoeffding_tree_classifier.py
+++ creme/tree/hoeffding_tree_classifier.py
@@ -71,33 +71,22 @@ class HoeffdingTreeClassifier(DecisionTree, base.Classifier):
 
     Examples
     --------
-    >>> # Imports
-    >>> from skmultiflow.data import SEAGenerator
-    >>> from skmultiflow.trees import HoeffdingTreeClassifier
+    >>> from creme.stream import synth
+    >>> from creme import evaluate
+    >>> from creme import metrics
+    >>> from creme import tree
 
-    >>> # Setting up a data stream
-    >>> stream = SEAGenerator(seed=1)
+    >>> dataset = datasets.Phishing()
 
-    >>> # Setup Hoeffding Tree estimator
-    >>> ht = HoeffdingTreeClassifier()
+    >>> model = tree.HoeffdingTreeClassifier(
+    ...     grace_period=100,
+    ...     split_confidence=1e-5,
+    ...     split_criterion='gini'
+    ... )
 
-    >>> # Setup variables to control loop and track performance
-    >>> n_samples = 0
-    >>> correct_cnt = 0
-    >>> max_samples = 200
+    >>> metric = metrics.Accuracy()
 
-    >>> # Train the estimator with the samples provided by the data stream
-    >>> while n_samples < max_samples and stream.has_more_samples():
-    >>>     X, y = stream.next_sample()
-    >>>     y_pred = ht.predict(X)
-    >>>     if y[0] == y_pred[0]:
-    >>>         correct_cnt += 1
-    >>>     ht = ht.partial_fit(X, y)
-    >>>     n_samples += 1
-
-    >>> # Display results
-    >>> print('{} samples analyzed.'.format(n_samples))
-    >>> print('Hoeffding Tree accuracy: {}'.format(correct_cnt / n_samples))
+    >>> evaluate.progressive_val_score(dataset, model, metric)
     """
 
     _GINI_SPLIT = 'gini'
