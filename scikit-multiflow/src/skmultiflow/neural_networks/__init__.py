c927ea8b89a92fd51cff4261576b3ceec5b247ff
Max Halford <maxhalford25@gmail.com>
2020-11-11 13:06:49 +0100

    remove scikit-multiflow directory
    Former-commit-id: d3d8123e04a3fbc977d61264e8722ae40b42926b

--- scikit-multiflow/src/skmultiflow/neural_networks/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-The :mod:`skmultiflow.neural_networks` module includes learning methods based on Neural Networks.
-"""
-
-from .perceptron import PerceptronMask
-
-__all__ = ["PerceptronMask"]
--- scikit-multiflow/src/skmultiflow/prototype/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-The :mod:`skmultiflow.prototype` module includes prototype-based learning methods.
-"""
-
-from .robust_soft_learning_vector_quantization import RobustSoftLearningVectorQuantization
-
-__all__ = ["RobustSoftLearningVectorQuantization"]
