0ccaa025877b538b474057c9a45238d620ad1fa9
Max Halford <maxhalford25@gmail.com>
2020-11-29 18:03:36 +0100

    remove pyproject

--- setup.py
+++ setup.py
@@ -35,7 +35,8 @@ compat_packages = base_packages + ["scikit-learn", "scikit-surprise", "sqlalchem
 
 dev_packages = base_packages + [
     "asv",
-    "black>=20.8b1" "flake8>=3.7.9",
+    "black>=20.8b1",
+    "flake8>=3.7.9",
     "graphviz>=0.10.1",
     "matplotlib>=3.0.2",
     "mypy>=0.761",
