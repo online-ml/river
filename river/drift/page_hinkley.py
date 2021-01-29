9efdb82c76e18acad9031ee2064deeceb5bb92a1
michaelchiucw <mchiu1994@gmail.com>
2021-01-29 15:11:16 +0800

    Correct the calculation in PageHinckleyDM.java
    Correct the calculation in PageHinckleyDM.java according to the description from the survey paper by Gama et al. (Page 15) [1].
    The original PageHinckleyDM.java is not entirely follow the description but following the description of CUSUM.
    
    [1] João Gama, Indrė Žliobaitė, Albert Bifet, Mykola Pechenizkiy, and Abdelhamid Bouchachia. 2014. A survey on concept drift adaptation. <i>ACM Comput. Surv.</i> 46, 4, Article 44 (April 2014), 37 pages. DOI:https://doi.org/10.1145/2523813

--- river/drift/page_hinkley.py
+++ river/drift/page_hinkley.py
@@ -56,6 +56,7 @@ class PageHinkley(DriftDetector):
         self.x_mean = None
         self.sample_count = None
         self.sum = None
+        self.Mint = None
         self.reset()
 
     def reset(self):
@@ -64,6 +65,7 @@ class PageHinkley(DriftDetector):
         self.sample_count = 1
         self.x_mean = 0.0
         self.sum = 0.0
+        self.Mint = float("inf")
 
     def update(self, value) -> tuple:
         """Update the change detector with a single data point.
@@ -86,12 +88,18 @@ class PageHinkley(DriftDetector):
 
         self.sample_count += 1
 
+        if self.sum <= self.Mint:
+            self.Mint = self.sum
+
+        pht = self.sum - self.Mint
+
         self._in_concept_change = False
+        self._in_warning_zone = False
 
         if self.sample_count < self.min_instances:
             return False, False
 
-        if self.sum > self.threshold:
+        if pht > self.threshold:
             self._in_concept_change = True
 
         return self._in_concept_change, self._in_warning_zone
