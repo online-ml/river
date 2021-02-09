292b2bca8927d8ef1a79c719cc0274649c04a141
Hoang Anh Ngo <hoang-anh.ngo@polytechnique.edu>
2021-02-09 12:33:45 +0700

    remove denstream (overlapping errors)

--- river/cluster/__init__.py
+++ river/cluster/__init__.py
@@ -1,6 +1,5 @@
 """Unsupervised clustering."""
 from .clustream import Clustream
-from .denstream import Denstream
 from .k_means import KMeans
 
-__all__ = ["Clustream", "Denstream", "KMeans"]
+__all__ = ["Clustream", "KMeans"]
