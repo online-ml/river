bf3d5a9b96e4ddd24a0984cacb315e1edb111173
hoanganhngo610 <50743576+hoanganhngo610@users.noreply.github.com>
2021-02-22 09:10:56 +0700

    Add CluStream method

--- river/base/clusterer.py
+++ river/base/clusterer.py
@@ -11,13 +11,17 @@ class Clusterer(estimator.Estimator):
         return False
 
     @abc.abstractmethod
-    def learn_one(self, x: dict) -> "Clusterer":
+    def learn_one(self, x: dict, sample_weight: int) -> "Clusterer":
         """Update the model with a set of features `x`.
 
         Parameters
         ----------
         x
             A dictionary of features.
+
+        sample_weight
+            Instance weight. If not provided, uniform weights are assumed.
+            Applicability varies depending on the algorithm.
 
         Returns
         -------
