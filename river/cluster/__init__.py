0fc74b6ba965b8547a35cbbd6c0a9fe5a3f06ec4
Hoang Anh Ngo <hoang-anh.ngo@polytechnique.edu>
2021-02-23 00:13:28 +0700

    Merge Denstream into a single file, improve compliance with codebase and improve documentation
    - Rename `Denstream` into `DenStream`
    - Rewrite documentation and remove unnecessary parts for aesthetic purpose
    - Rewrite the distance method to match with the implementation in`CluStream`
    - No functional change

--- river/cluster/__init__.py
+++ river/cluster/__init__.py
@@ -1,6 +1,6 @@
 """Unsupervised clustering."""
 from .clustream import CluStream
-from .denstream import Denstream
+from .denstream import DenStream
 from .k_means import KMeans
 
-__all__ = ["CluStream", "Denstream", "KMeans"]
+__all__ = ["CluStream", "DenStream", "KMeans"]
