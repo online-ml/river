b8ca5ec7921fbf2cdd0a91b60ed4ebdd2e6ae484
Hoang Anh Ngo <hoang-anh.ngo@polytechnique.edu>
2021-02-22 13:11:29 +0700

    Update __init__.py

--- river/cluster/__init__.py
+++ river/cluster/__init__.py
@@ -3,4 +3,4 @@ from .clustream import CluStream
 from .denstream import Denstream
 from .k_means import KMeans
 
-__all__ = ["Clustream", "Denstream", "KMeans"]
+__all__ = ["CluStream", "Denstream", "KMeans"]
