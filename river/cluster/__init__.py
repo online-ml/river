bc8eae1939228e053738b0726d4d1cef2dce2239
Hoang Anh Ngo <hoang-anh.ngo@polytechnique.edu>
2021-02-27 01:02:00 +0700

    first working version of `DBSTREAM`

--- river/cluster/__init__.py
+++ river/cluster/__init__.py
@@ -1,5 +1,6 @@
 """Unsupervised clustering."""
 from .clustream import CluStream
+from .dbstream import DBSTREAM
 from .k_means import KMeans
 
-__all__ = ["CluStream", "KMeans"]
+__all__ = ["CluStream", "DBSTREAM", "KMeans"]
