7810820687c9319022924a88f3b64c707258e595
smastelini <saulomastelini@gmail.com>
2021-03-08 17:56:17 -0300

    pre-commit actions

--- river/stats/test_mean_var_cov.py
+++ river/stats/test_mean_var_cov.py
@@ -1,7 +1,8 @@
 import functools
 import math
-import statistics
 import random
+import statistics
+
 import numpy as np
 import pytest
 
@@ -9,12 +10,12 @@ from river import stats
 
 
 @pytest.mark.parametrize(
-    'stat1, stat2, func',
+    "stat1, stat2, func",
     [
         (stats.Mean(), stats.Mean(), statistics.mean),
         (stats.Var(ddof=0), stats.Var(ddof=0), np.var),
-        (stats.Var(), stats.Var(), functools.partial(np.var, ddof=1))
-    ]
+        (stats.Var(), stats.Var(), functools.partial(np.var, ddof=1)),
+    ],
 )
 def test_add_mean_var(stat1, stat2, func):
     X = [random.random() for _ in range(30)]
@@ -24,19 +25,21 @@ def test_add_mean_var(stat1, stat2, func):
         stat1.update(x)
         stat2.update(y)
         if i >= 1:
-            assert math.isclose((stat1 + stat2).get(), func(X[:i + 1] + Y[:i + 1]), abs_tol=1e-10)
+            assert math.isclose(
+                (stat1 + stat2).get(), func(X[: i + 1] + Y[: i + 1]), abs_tol=1e-10
+            )
 
     stat1 += stat2
     assert math.isclose(stat1.get(), func(X + Y), abs_tol=1e-10)
 
 
 @pytest.mark.parametrize(
-    'stat1, stat2, func',
+    "stat1, stat2, func",
     [
         (stats.Mean(), stats.Mean(), statistics.mean),
         (stats.Var(ddof=0), stats.Var(ddof=0), np.var),
-        (stats.Var(), stats.Var(), functools.partial(np.var, ddof=1))
-    ]
+        (stats.Var(), stats.Var(), functools.partial(np.var, ddof=1)),
+    ],
 )
 def test_sub_mean_var(stat1, stat2, func):
     X = [random.random() for _ in range(30)]
@@ -47,7 +50,7 @@ def test_sub_mean_var(stat1, stat2, func):
     for i, x in enumerate(X):
         stat2.update(x)
         if i < len(X) - 2:
-            assert math.isclose((stat1 - stat2).get(), func(X[i+1:]), abs_tol=1e-10)
+            assert math.isclose((stat1 - stat2).get(), func(X[i + 1 :]), abs_tol=1e-10)
 
     # Test inplace subtraction
     X.extend(random.random() for _ in range(3))
@@ -59,11 +62,11 @@ def test_sub_mean_var(stat1, stat2, func):
 
 
 @pytest.mark.parametrize(
-    'stat1, stat2, func',
+    "stat1, stat2, func",
     [
         (stats.Cov(ddof=0), stats.Cov(ddof=0), functools.partial(np.cov, ddof=0)),
-        (stats.Cov(ddof=1), stats.Cov(ddof=1), functools.partial(np.cov, ddof=1))
-    ]
+        (stats.Cov(ddof=1), stats.Cov(ddof=1), functools.partial(np.cov, ddof=1)),
+    ],
 )
 def test_add_cov(stat1, stat2, func):
     X = [random.random() for _ in range(30)]
@@ -78,8 +81,8 @@ def test_add_cov(stat1, stat2, func):
         if i >= 1:
             assert math.isclose(
                 (stat1 + stat2).get(),
-                func(X[:i + 1] + W[:i + 1], Y[:i + 1] + Z[:i + 1])[0, 1],
-                abs_tol=1e-10
+                func(X[: i + 1] + W[: i + 1], Y[: i + 1] + Z[: i + 1])[0, 1],
+                abs_tol=1e-10,
             )
 
     stat1 += stat2
@@ -87,11 +90,11 @@ def test_add_cov(stat1, stat2, func):
 
 
 @pytest.mark.parametrize(
-    'stat1, stat2, func',
+    "stat1, stat2, func",
     [
         (stats.Cov(ddof=0), stats.Cov(ddof=0), functools.partial(np.cov, ddof=0)),
-        (stats.Cov(ddof=1), stats.Cov(ddof=1), functools.partial(np.cov, ddof=1))
-    ]
+        (stats.Cov(ddof=1), stats.Cov(ddof=1), functools.partial(np.cov, ddof=1)),
+    ],
 )
 def test_sub_cov(stat1, stat2, func):
     X = [random.random() for _ in range(30)]
@@ -103,7 +106,9 @@ def test_sub_cov(stat1, stat2, func):
     for i, (x, y) in enumerate(zip(X, Y)):
         stat2.update(x, y)
         if i < len(X) - 2:
-            assert math.isclose((stat1 - stat2).get(), func(X[i+1:], Y[i+1:])[0, 1], abs_tol=1e-10)
+            assert math.isclose(
+                (stat1 - stat2).get(), func(X[i + 1 :], Y[i + 1 :])[0, 1], abs_tol=1e-10
+            )
 
     # Test inplace subtraction
     X.extend(random.random() for _ in range(3))
