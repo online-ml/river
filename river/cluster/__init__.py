a7e42aa81bb1258675416da688a0a670c9ab5dd1
Hoang Anh Ngo <hoang-anh.ngo@polytechnique.edu>
2021-03-13 17:14:28 +0700

    first working implementation of evostream

--- river/cluster/__init__.py
+++ river/cluster/__init__.py
@@ -1,5 +1,6 @@
 """Unsupervised clustering."""
 from .clustream import CluStream
+from .evostream import evoStream
 from .k_means import KMeans
 
-__all__ = ["CluStream", "KMeans"]
+__all__ = ["CluStream", "evoStream", "KMeans"]
