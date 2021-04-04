8a9bbad24c9ec180c8b28887222915d0aaf5169c
ogozuacik <ogozuacik@gmail.com>
2021-04-04 13:31:36 +0300

    Change in the implementation

--- river/drift/d3.py
+++ river/drift/d3.py
@@ -1,8 +1,7 @@
-import numpy as np
-from sklearn.linear_model import LogisticRegression
-
+from copy import deepcopy
 from river import metrics
 from river.base import DriftDetector
+from river.tree import HoeffdingTreeClassifier
 
 
 class D3(DriftDetector):
@@ -17,24 +16,11 @@ class D3(DriftDetector):
 
     Parameters
     ----------
-    old_data_window_size
-        The size of the old data window. (int)
-    new_data_percentage
-        Determines the size of the new data window. It is calculated as
-        a fraction of the old data window. The new data window has size
-        new data percentage * old data window size. (float)
-        From 0.0 to 1.0
+    window_size
+        The size of the data window. (int)
     auc_threshold
-        Required AUC score to signal a drift. (float)
+        Required AUC score to signal a drift.
         From 0.5 to 1.0
-    discriminative_classifier
-        Classifier to be used to distinguish old data from the new data.
-        If None, Logistic Regression with default parameters will be used.
-        It is advised to use a simple model as the goal of this classifier
-        is to determine if the old data and the new data are seperable,
-        not to classify them. (sklearn classifier)
-    seed
-        Is used as as random state for numpy
 
 
     Examples
@@ -62,128 +48,113 @@ class D3(DriftDetector):
 
     """
 
-    _DISC_CLF_TRAINING_DATA_PERCENTAGE = 0.7
+    _AUC_NUM_THRESHOLDS = 20
+    _LABEL_FOR_NEW_DATA = True
+    _LABEL_FOR_OLD_DATA = False
 
     def __init__(
         self,
-        old_data_window_size=100,
-        new_data_percentage=0.1,
+        window_size=200,
         auc_threshold=0.7,
-        discriminative_classifier=None,
-        seed=None,
+        discriminative_classifier=HoeffdingTreeClassifier(
+            grace_period=40, max_depth=3),
     ):
         super().__init__()
         self.auc_threshold = auc_threshold
-        self.data_sliding_window = None
+        self.sub_window_size = int(window_size / 2)
         self.discriminative_classifier = discriminative_classifier
-        self.new_data_percentage = new_data_percentage
-        self.feature_names = None
-        self.old_data_window_size = old_data_window_size
-        self.sliding_window_index = 0
-        self.new_data_window_size = int(
-            self.old_data_window_size * self.new_data_percentage
-        )
-        self.full_window_size = self.old_data_window_size + self.new_data_window_size
-        np.random.seed(seed)
+        self.old_data_window = [None] * self.sub_window_size
+        self.new_data_window = [None] * self.sub_window_size
+        self.data_labels = None
+        self.store_labels = False
+        self.old_data_window_index = 0
+        self.new_data_window_index = 0
+        self.auc = metrics.ROCAUC(n_thresholds=D3._AUC_NUM_THRESHOLDS)
+
         super().reset()
 
-    def _is_sliding_window_full(self):
-        """Checks if the sliding window is full."""
-        if self.sliding_window_index < self.full_window_size:
-            return False
-        return True
+    def current_data_and_labels(self):
+        """Returns the data and labels for the current data window.
 
-    def _discriminative_drift_detection(self, old_data, new_data):
-        """Check if old and new data are seperable"""
-        new_train_data_size = int(
-            D3._DISC_CLF_TRAINING_DATA_PERCENTAGE * self.new_data_window_size
-        )
-        old_train_data_size = int(
-            D3._DISC_CLF_TRAINING_DATA_PERCENTAGE * self.old_data_window_size
-        )
-        new_data_train_idx = np.random.randint(
-            self.new_data_window_size, size=new_train_data_size
-        )
-        old_data_train_idx = np.random.randint(
-            self.old_data_window_size, size=old_train_data_size
-        )
-        new_data_labels = np.zeros(self.new_data_window_size)
-        old_data_labels = np.ones(self.old_data_window_size)
-        train_data = np.concatenate(
-            (new_data[new_data_train_idx], old_data[old_data_train_idx]), axis=0
-        )
-        test_data = np.concatenate(
-            (new_data[~new_data_train_idx], old_data[~old_data_train_idx]), axis=0
-        )
-        train_labels = np.concatenate(
-            (new_data_labels[new_data_train_idx], old_data_labels[old_data_train_idx]),
-            axis=0,
-        )
-        test_labels = np.concatenate(
-            (
-                new_data_labels[~new_data_train_idx],
-                old_data_labels[~old_data_train_idx],
-            ),
-            axis=0,
-        )
-        if self.discriminative_classifier is None:
-            self.discriminative_classifier = LogisticRegression(solver="liblinear")
-        self.discriminative_classifier.fit(train_data, train_labels)
-        predictions = self.discriminative_classifier.predict_proba(test_data)[:, 1]
-        auc = metrics.ROCAUC()
-        for yt, yp in zip(test_labels, predictions):
-            auc.update(bool(yt), yp)
-        auc_score = auc.get()
-        if (auc_score > self.auc_threshold) or (auc_score < self.auc_threshold - 0.5):
-            return True
-        return False
+        They can be used when for retraining the stream classifier after drift detection.
+        """
+        return self.old_data_window, self.data_labels
+
+    def update_labels_if_storing_enabled(self, index, label):
+        """Update the labels array if storing is enabled"""
+        if not self.store_labels:
+            return
+        self.data_labels[index] = label
 
     def reset(self):
         """Reset the change detector."""
         super().reset()
-        self.data_sliding_window = None
-        self.sliding_window_index = 0
+        self.old_data_window = [None] * self.sub_window_size
+        self.new_data_window = [None] * self.sub_window_size
+        self.old_data_window_index = 0
+        self.new_data_window_index = 0
+        self.data_labels = None
+        self.store_labels = False
+        self.auc = metrics.ROCAUC(n_thresholds=D3._AUC_NUM_THRESHOLDS)
+        self.discriminative_classifier = self.discriminative_classifier.clone()
 
-    def update(self, sample):
+    def update(self, sample, label=None):
         """Update the change detector with a single sample.
 
         Parameters
         ----------
         sample
             An instance from the data stream (dict) having N items (number of features).
+        label
+            Class label for sample. 
 
+        Notes
+        -----
+        * The label is is not used in the detection process. It can be set to None. 
+        * If the label is not set to None, D3 will store the last window_size/2 
+          class labels of the samples. It is useful for retraining the stream classifier 
+          when a drift is detected.  
         """
         if self._in_concept_change:
             self._in_concept_change = False
 
-        if not self.feature_names:
-            self.feature_names = sample.keys()
+        # Start storing labels if not None
+        if (label is not None) and (self.store_labels == False):
+            self.data_labels = [None] * self.sub_window_size
+            self.store_labels = True
 
-        sample = [sample[feature] for feature in sorted(self.feature_names)]
-
-        if self.data_sliding_window is None:
-            self.data_sliding_window = np.zeros(
-                (self.full_window_size, len(self.feature_names))
-            )
-
-        if not self._is_sliding_window_full():
-            self.data_sliding_window[self.sliding_window_index] = sample
-            self.sliding_window_index += 1
+        if self.old_data_window_index < self.sub_window_size:
+            self.old_data_window[self.old_data_window_index] = sample
+            self.old_data_window_index += 1
             return self._in_concept_change, self._in_warning_zone
 
-        old_data = self.data_sliding_window[: self.old_data_window_size]
-        new_data = self.data_sliding_window[self.old_data_window_size :]
+        self.new_data_window[self.new_data_window_index] = sample
+        self.update_labels_if_storing_enabled(
+            self.new_data_window_index, label)
 
-        if self._discriminative_drift_detection(old_data, new_data):
-            self.sliding_window_index = self.new_data_window_size
-            self.data_sliding_window = np.roll(
-                self.data_sliding_window, -1 * self.old_data_window_size, axis=0
-            )
-            self._in_concept_change = True
-        else:
-            self.sliding_window_index = self.old_data_window_size
-            self.data_sliding_window = np.roll(
-                self.data_sliding_window, -1 * self.new_data_window_size, axis=0
-            )
+        # Updating discriminative classifier with a sample from the old and new data
+        old_data_sample = self.old_data_window[self.new_data_window_index]
+        self.discriminative_classifier.learn_one(
+            sample, D3._LABEL_FOR_NEW_DATA)
+        self.discriminative_classifier.learn_one(
+            old_data_sample, D3._LABEL_FOR_OLD_DATA)
+
+        # Update AUC
+        prob_new = self.discriminative_classifier.predict_proba_one(sample)[1]
+        prob_old = self.discriminative_classifier.predict_proba_one(
+            old_data_sample)[1]
+        self.auc = self.auc.update(D3._LABEL_FOR_NEW_DATA, prob_new)
+        self.auc = self.auc.update(D3._LABEL_FOR_OLD_DATA, prob_old)
+
+        self.new_data_window_index += 1
+
+        if self.new_data_window_index == self.sub_window_size:
+            auc_score = self.auc.get()
+            if (auc_score > self.auc_threshold) or (auc_score < self.auc_threshold - 0.5):
+                self._in_concept_change = True
+            self.old_data_window = deepcopy(self.new_data_window)
+            self.new_data_window_index = 0
+            self.auc = metrics.ROCAUC(n_thresholds=D3._AUC_NUM_THRESHOLDS)
+            self.discriminative_classifier = self.discriminative_classifier.clone()
 
         return self._in_concept_change, self._in_warning_zone
