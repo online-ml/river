ebeb991b4394ea39cbbfe115a01862afd131af2b
ogozuacik <ogozuacik@gmail.com>
2021-04-19 02:27:24 +0300

    Improve doc

--- river/drift/d3.py
+++ river/drift/d3.py
@@ -28,9 +28,10 @@ class D3(DriftDetector):
 
     Notes
     -----
-    * It is advised to use a simple model as the goal of this classifier
-      is to determine if the old data and the new data are seperable,
-      not to classify them.
+    * This method looks for drifts in multi-dimensional data.
+    * It is advised to use a simple model as a discriminative classifier
+      since the goal of it is to determine if the old data and the new data 
+      are seperable, not to classify them.
     * This implementation differs from the original one in the paper,
       making the size of the old and new data windows equal, and allowing
       to use any classifier for discriminating old and new data.
