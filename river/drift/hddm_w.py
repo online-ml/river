e6bd40d62fb191882d6660c9b3881889f1a8c7ea
Michal Pogoda <michalpogoda@hotmail.com>
2021-05-01 21:38:33 +0200

    Make HDDM_W report concept drift during downward changes when two_sided_test is set to True

--- river/drift/hddm_w.py
+++ river/drift/hddm_w.py
@@ -137,6 +137,10 @@ class HDDM_W(DriftDetector):
         self._update_decr_statistics(value, self.drift_confidence)
         if self.two_sided_test and self._monitor_mean_decr(self.drift_confidence):
             self.reset()
+            self._in_concept_change = True
+        elif self._monitor_mean_decr(self.drift_confidence):
+            self._in_warning_zone = True
+        
         self.estimation = self.total.EWMA_estimator
 
         return self._in_concept_change, self._in_warning_zone
