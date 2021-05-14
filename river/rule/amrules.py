ece1c5b0b30dc016d811ed868f963636fdb2dbb5
smastelini <saulomastelini@gmail.com>
2021-05-14 13:52:33 -0300

    init work

---
+++ river/rule/amrules.py
@@ -0,0 +1,72 @@
+import copy
+
+from river import base
+from river import linear_model
+from river import stats
+from river import tree
+
+from .base import BinaryLiteral, NominalLiteral, Rule
+from ..tree.splitter.nominal_splitter_reg import NominalSplitterReg
+
+
+class MeanPredWrapper(base.Regressor):
+    def __init__(self):
+        self.mean = stats.Mean()
+
+    def learn_one(self, x: dict, y: base.typing.RegTarget, w: int = 1):
+        self.mean.update(y, w)
+
+    def predict_one(self, x: dict):
+        return self.mean.get()
+
+
+class RegRule(Rule, base.Regressor):
+    def new_nominal_splitter(self):
+        return NominalSplitterReg()
+
+    def __init__(self, template_splitter, pred_model):
+        super().__init__(template_splitter=template_splitter)
+        self._pred_model = pred_model
+
+        self.stats = stats.Var()
+
+    def _update_stats(self, y, w):
+        self.stats.update(y, w)
+
+    def learn_one(self, x: dict, y: base.typing.RegTarget, w: int = 1):
+        self.update(x, y, w)
+        self._pred_model.learn_one(x, y, w)
+
+    def predict_one(self, x: dict):
+        return self._pred_model.predict_one(x)
+
+
+class AMRules(base.Regressor):
+    _PRED_MEAN = "mean"
+    _PRED_MODEL = "model"
+    _VALID_PRED = [_PRED_MEAN, _PRED_MODEL]
+
+    def __init__(self, grace_period: int = 200, pred_type: str = "model", pred_model: base.Regressor = None, splitter = None):
+        self.grace_period = grace_period
+
+        if splitter is None:
+            self.splitter = tree.splitter.EBSTSplitter()
+        else:
+            self.splitter = splitter
+
+        if pred_type not in self._VALID_PRED:
+            raise ValueError(f"Invalid 'pred_type': {pred_type}")
+        self.pred_type = pred_type
+
+        self.pred_model = pred_model if pred_model else linear_model.LinearRegression()
+
+        self._default_rule = self._new_rule()
+
+    def _new_rule(self):
+        predictor = None
+        if self.pred_type == self._PRED_MEAN:
+            predictor = MeanPredWrapper()
+        else:
+            predictor = copy.deepcopy(self.pred_model)
+
+        return RegRule(self.splitter, predictor)
---
+++ river/rule/base.py
@@ -0,0 +1,100 @@
+import abc
+import copy
+import numbers
+import typing
+
+from river import base
+
+
+class Literal(base.Base):
+    __slots__ = 'on, at'
+
+    def __init__(self, on, at):
+        self.on = on
+        self.at = at
+
+    @abc.abstractmethod
+    def __call__(self, x):
+        pass
+
+    @abc.abstractmethod
+    def describe(self):
+        pass
+
+
+class BinaryLiteral(Literal):
+    def __init__(self, on, at):
+        super().__init__(on, at)
+
+    def __call__(self, x):
+        if self.on in x:
+            return x[self.on] <= self.at
+        return False
+
+    def describe(self):
+        return f"{self.on} ≤ {self.at}"
+
+
+class NominalLiteral(Literal):
+    def __init__(self, on, at):
+        super().__init__(on, at)
+
+    def __call__(self, x):
+        if self.on in x:
+            return x[self.on] == self.at
+        return False
+
+    def describe(self):
+        return f"{self.on} = {self.at}"
+
+
+class Rule(base.Estimator, metaclass=abc.ABCMeta):
+    def __init__(self, template_splitter, **attributes):
+        self.template_splitter = template_splitter
+        self.literals = []
+        self.splitters = []
+
+        self.__dict__.update(attributes)
+
+    def expand(self):
+        # TODO
+        pass
+
+    def covers(self, x):
+        return all(map(lambda lit: lit(x), self.literals))
+
+    @abc.abstractmethod
+    def new_nominal_splitter(self):
+        pass
+
+    def _iter_features(self, x) -> typing.Iterable:
+        """Determine how the input instance is looped through when updating the splitters.
+
+        Parameters
+        ----------
+        x
+            The input instance.
+        """
+        for att_id, att_val in x.items():
+            yield att_id, att_val
+
+    @abc.abstractmethod
+    def _update_stats(self, y, w):
+        pass
+
+    def update(self, x, y, w):
+        self._update_stats(y, w)
+
+        for att_id, att_val in self._iter_features(x):
+            try:
+                splt = self.splitters[att_id]
+            except KeyError:
+                if isinstance(att_val, numbers.Number):
+                    self.splitters[att_id] = copy.deepcopy(self.template_splitter)
+                else:
+                    self.splitters[att_id] = self.new_nominal_splitter()
+                splt = self.splitters[att_id]
+            splt.update(x, y, w)
+
+    def __repr__(self):
+        return f"{' and '.join([lit.describe() for lit in self.literals])}"
