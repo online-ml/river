8ecb8642fc3e5ed8cc78f6eba3d9cc341476a273
smastelini <saulomastelini@gmail.com>
2021-05-15 17:32:07 -0300

    sketch done

--- river/rule/amrules.py
+++ river/rule/amrules.py
@@ -1,75 +1,283 @@
+import collections
 import copy
+import functools
+import math
+import numbers
+import typing
 
-from river import base, linear_model, stats, tree
+from river import base, drift, linear_model, stats, tree
 
 from ..tree.splitter.nominal_splitter_reg import NominalSplitterReg
-from .base import Rule
+from .base import HoeffdingRule
 
 
-class MeanPredWrapper(base.Regressor):
+class MeanRegressor(base.Regressor):
     def __init__(self):
         self.mean = stats.Mean()
 
     def learn_one(self, x: dict, y: base.typing.RegTarget, w: int = 1):
         self.mean.update(y, w)
+        return self
 
     def predict_one(self, x: dict):
         return self.mean.get()
 
 
-class RegRule(Rule, base.Regressor):
+class AdaptiveRegressor(base.Regressor):
+    def __init__(self, model_predictor: base.Regressor, alpha: float):
+        self.model_predictor = model_predictor
+        self.mean_predictor = MeanRegressor()
+        self.alpha = alpha
+
+        self._mae_mean = 0.0
+        self._mae_model = 0.0
+
+    def learn_one(self, x: dict, y: base.typing.RegTarget, w: int = 1):
+        abs_error_mean = abs(y - self.mean_predictor.predict_one(x))  # noqa
+        abs_error_model = abs(y - self.model_predictor.predict_one(x))  # noqa
+
+        self._mae_mean = self.alpha * self._mae_mean + abs_error_mean
+        self._mae_model = self.alpha * self._mae_model + abs_error_model
+
+        self.mean_predictor.learn_one(x, y, w)
+
+        try:
+            self.model_predictor.learn_one(x, y, w)  # noqa
+        except TypeError:
+            for _ in range(int(w)):
+                self.model_predictor.learn_one(x, y)
+
+        return self
+
+    def predict_one(self, x: dict):
+        if self._mae_mean <= self._mae_model:
+            return self.mean_predictor.predict_one(x)
+        else:
+            return self.model_predictor.predict_one(x)
+
+
+class RegRule(HoeffdingRule, base.Regressor):
+    def __init__(
+        self,
+        n_min,
+        tau,
+        delta,
+        template_splitter,
+        split_criterion,
+        pred_model,
+        drift_detector,
+    ):
+        super().__init__(
+            n_min=n_min,
+            delta=delta,
+            tau=tau,
+            template_splitter=template_splitter,
+            split_criterion=split_criterion,
+        )
+        self.pred_model = pred_model
+        self.drift_detector = drift_detector
+
+        self._target_stats = stats.Var()
+        self._feat_stats = collections.defaultdict(functools.partial(stats.Var))
+
     def new_nominal_splitter(self):
         return NominalSplitterReg()
 
-    def __init__(self, template_splitter, pred_model):
-        super().__init__(template_splitter=template_splitter)
-        self._pred_model = pred_model
+    @property
+    def statistics(self):
+        return self._target_stats
 
-        self.stats = stats.Var()
+    @statistics.setter
+    def statistics(self, target_stats):
+        self._target_stats = target_stats
 
-    def _update_stats(self, y, w):
-        self.stats.update(y, w)
+    @property
+    def total_weight(self):
+        return self._target_stats.mean.n
+
+    def _update_target_stats(self, y, w):
+        self._target_stats.update(y, w)
+
+    def _update_feature_stats(self, feat_name, feat_val, w):
+        self._feat_stats[feat_name].update(feat_val, w)
+
+    def drift_test(self, y, y_pred):
+        abs_error = abs(y - y_pred)
+        in_drift, _ = self.drift_detector.update(abs_error)
+
+        return in_drift
+
+    def anomaly_score(self, x):
+        score = 0.0
+        hits = 0
+
+        for feat_name, feat_val in x.items():
+            # Skip nominal features
+            if not isinstance(feat_val, numbers.Number):
+                continue
+
+            mean = self._feat_stats[feat_name].mean.get()
+            var = self._feat_stats[feat_name].get()
+            std = math.sqrt(var)
+
+            if std > 0:
+                # One tailed variant of Chebyshev 's inequality
+                k = abs(feat_val - mean) / std  # noqa
+                proba = (2 * var) / (var + k * k)
+
+                if 0 < proba < 1:
+                    score += math.log(proba) - math.log(1 - proba)
+                    hits += 1
+
+        return score / hits if hits > 0 else 0.0
 
     def learn_one(self, x: dict, y: base.typing.RegTarget, w: int = 1):
         self.update(x, y, w)
-        self._pred_model.learn_one(x, y, w)
+        self.pred_model.learn_one(x, y, w)
+
+        return self
 
     def predict_one(self, x: dict):
-        return self._pred_model.predict_one(x)
+        return self.pred_model.predict_one(x)
 
 
 class AMRules(base.Regressor):
     _PRED_MEAN = "mean"
     _PRED_MODEL = "model"
-    _VALID_PRED = [_PRED_MEAN, _PRED_MODEL]
+    _PRED_ADAPTIVE = "adaptive"
+    _VALID_PRED = [_PRED_MEAN, _PRED_MODEL, _PRED_ADAPTIVE]
 
     def __init__(
         self,
-        grace_period: int = 200,
-        pred_type: str = "model",
+        n_min: int = 200,
+        tau: float = 0.05,
+        delta: float = 1e-7,
+        pred_type: str = "adaptive",
         pred_model: base.Regressor = None,
         splitter=None,
+        drift_detector=None,
+        model_selector_decay: float = 0.99,
+        anomaly_threshold: float = -0.75,
+        m_min: int = 30,
+        ordered_rule_set=False,
     ):
-        self.grace_period = grace_period
+        self.n_min = n_min
+        self.tau = tau
+        self.delta = delta
+
+        if pred_type not in self._VALID_PRED:
+            raise ValueError(f"Invalid 'pred_type': {pred_type}")
+        self.pred_type = pred_type
+        self.pred_model = pred_model if pred_model else linear_model.LinearRegression()
 
         if splitter is None:
             self.splitter = tree.splitter.EBSTSplitter()
         else:
             self.splitter = splitter
 
-        if pred_type not in self._VALID_PRED:
-            raise ValueError(f"Invalid 'pred_type': {pred_type}")
-        self.pred_type = pred_type
+        self.drift_detector = (
+            drift_detector
+            if drift_detector is not None
+            else drift.PageHinkley(threshold=35)
+        )
 
-        self.pred_model = pred_model if pred_model else linear_model.LinearRegression()
+        self.model_selector_decay = model_selector_decay
+        self.anomaly_threshold = anomaly_threshold
+        self.m_min = m_min
+        self.ordered_rule_set = ordered_rule_set
 
         self._default_rule = self._new_rule()
+        self._rules: typing.Dict[typing.Hashable, RegRule] = {}
 
-    def _new_rule(self):
-        predictor = None
+    def _new_rule(self) -> RegRule:
         if self.pred_type == self._PRED_MEAN:
-            predictor = MeanPredWrapper()
-        else:
+            predictor = MeanRegressor()
+        elif self.pred_type == self._PRED_MODEL:
             predictor = copy.deepcopy(self.pred_model)
+        else:
+            predictor = AdaptiveRegressor(
+                model_predictor=copy.deepcopy(self.pred_model),
+                alpha=self.model_selector_decay,
+            )
 
-        return RegRule(self.splitter, predictor)
+        return RegRule(
+            n_min=self.n_min,
+            delta=self.delta,
+            tau=self.tau,
+            template_splitter=self.splitter,
+            split_criterion=None,  # TODO change here
+            pred_model=predictor,
+            drift_detector=copy.deepcopy(self.drift_detector),
+        )
+
+    def learn_one(self, x: dict, y: base.typing.RegTarget, w: int = 1):
+        any_covered = False
+        to_del = set()
+
+        for rule_id, rule in self._rules.items():
+            if not rule.covers(x):
+                continue
+
+            if rule.total_weight > self.m_min:
+                a_score = rule.anomaly_score(x)
+
+                # Anomaly detected skip training
+                if a_score < self.anomaly_threshold:
+                    continue
+
+            y_pred = rule.predict_one(x)
+
+            in_drift = rule.drift_test(y, y_pred)  # noqa
+            if in_drift:
+                to_del.add(rule_id)
+                continue
+
+            any_covered = True
+            rule.learn_one(x, y, w)
+
+            if rule.total_weight - rule.last_expansion_attempt_at >= self.n_min:
+                rule.expand(self.delta, self.tau)
+
+            # Only the first matching rule is updated
+            if self.ordered_rule_set:
+                break
+
+        if not any_covered:
+            self._default_rule.learn_one(x, y, w)
+
+            expanded = False
+            if (
+                self._default_rule.total_weight
+                - self._default_rule.last_expansion_attempt_at
+                >= self.n_min
+            ):
+                expanded = self._default_rule.expand(self.delta, self.tau)
+
+            if expanded:
+                code = hash(self._default_rule)
+                self._rules[code] = self._default_rule
+
+                self._default_rule = self._new_rule()
+
+        # Drop the rules affected by concept drift
+        for rule_id in to_del:
+            del self._rules[rule_id]
+
+        return self
+
+    def predict_one(self, x: dict) -> base.typing.RegTarget:
+        y_pred = 0
+        hits = 0
+
+        for rule in self._rules.values():
+            if rule.covers(x):
+                y_pred += rule.predict_one(x)
+                hits += 1
+
+                if self.ordered_rule_set:
+                    break
+
+        if hits > 0:
+            return y_pred / hits
+        else:
+            return self._default_rule.predict_one(x)
--- river/rule/base.py
+++ river/rule/base.py
@@ -1,5 +1,6 @@
 import abc
 import copy
+import math
 import numbers
 import typing
 
@@ -7,11 +8,12 @@ from river import base
 
 
 class Literal(base.Base):
-    __slots__ = "on, at"
+    __slots__ = "on, at, neg"
 
-    def __init__(self, on, at):
+    def __init__(self, on, at, neg=False):
         self.on = on
         self.at = at
+        self.neg = neg
 
     @abc.abstractmethod
     def __call__(self, x):
@@ -22,43 +24,158 @@ class Literal(base.Base):
         pass
 
 
-class BinaryLiteral(Literal):
-    def __init__(self, on, at):
-        super().__init__(on, at)
+class NumericLiteral(Literal):
+    def __init__(self, on, at, neg):
+        super().__init__(on, at, neg)
 
     def __call__(self, x):
         if self.on in x:
-            return x[self.on] <= self.at
+            if not self.neg:
+                return x[self.on] <= self.at
+            else:
+                return x[self.on] > self.at
+
         return False
 
     def describe(self):
-        return f"{self.on} ≤ {self.at}"
+        if not self.neg:
+            return f"{self.on} ≤ {self.at}"
+        else:
+            return f"{self.on} > {self.at}"
 
 
 class NominalLiteral(Literal):
-    def __init__(self, on, at):
-        super().__init__(on, at)
+    def __init__(self, on, at, neg):
+        super().__init__(on, at, neg)
 
     def __call__(self, x):
         if self.on in x:
-            return x[self.on] == self.at
+            if not self.neg:
+                return x[self.on] == self.at
+            else:
+                return x[self.on] != self.at
+
         return False
 
     def describe(self):
-        return f"{self.on} = {self.at}"
+        if not self.neg:
+            return f"{self.on} = {self.at}"
+        else:
+            return f"{self.on} ≠ {self.at}"
 
 
-class Rule(base.Estimator, metaclass=abc.ABCMeta):
-    def __init__(self, template_splitter, **attributes):
+class HoeffdingRule(base.Estimator, metaclass=abc.ABCMeta):
+    def __init__(self, template_splitter, split_criterion, **attributes):
         self.template_splitter = template_splitter
+        self.split_criterion = split_criterion
         self.literals = []
-        self.splitters = []
+        self.splitters = {}
+
+        self._last_expansion_attempt_at = 0
 
         self.__dict__.update(attributes)
 
-    def expand(self):
-        # TODO
+    def _hoeffding_bound(self, r_heur, delta):
+        r"""Compute the Hoeffding bound, used to decide how many samples are necessary to expand
+        a decision rule.
+
+        Notes
+        -----
+        The Hoeffding bound is defined as:
+
+        $\\epsilon = \\sqrt{\\frac{R^2\\ln(1/\\delta))}{2n}}$
+
+        where:
+
+        $\\epsilon$: Hoeffding bound.
+        $R$: Range of a random variable.
+        $\\delta$: significance level.
+        $n$: Number of samples.
+
+        Parameters
+        ----------
+        r_heur
+            Range of the split heuristic.
+        delta
+            The significance level.
+        """
+        return math.sqrt(
+            (r_heur * r_heur * math.log(1.0 / delta)) / (2.0 * self.total_weight)
+        )
+
+    @property
+    @abc.abstractmethod
+    def statistics(self):
         pass
+
+    @statistics.setter
+    @abc.abstractmethod
+    def statistics(self, target_stats):
+        pass
+
+    @property
+    @abc.abstractmethod
+    def total_weight(self):
+        pass
+
+    @property
+    def last_expansion_attempt_at(self):
+        return self._last_expansion_attempt_at
+
+    def expand(self, delta, tau):
+        suggestions = []
+        for att_id, splitter in self.splitters.items():
+            suggestions.append(
+                splitter.best_evaluated_split_suggestion(
+                    criterion=self.split_criterion,
+                    pre_split_dist=self.statistics,
+                    att_idx=att_id,
+                    binary_only=True,
+                )
+            )
+        suggestions.sort()
+
+        should_expand = False
+        if len(suggestions) < 2:
+            should_expand = True
+        else:
+            b_split = suggestions[-1]
+            sb_split = suggestions[-2]
+
+            hb = self._hoeffding_bound(
+                self.split_criterion.range_of_merit(self.statistics), delta
+            )
+
+            # TODO (1) implement Variance Ratio split criterion to ensure the following expression
+            # TODO (2) holds for both classification and regression
+            if b_split.merit - sb_split.merit > hb or hb < tau:
+                should_expand = True
+
+        if should_expand:
+            b_split = suggestions[-1]
+
+            is_numerical = b_split.is_numerical
+
+            # TODO select which of the binary branches of the test will be kept
+            branch_no = self.split_criterion.select_best_branch(b_split.children_stats)
+
+            if is_numerical:
+                lit = NumericLiteral(
+                    b_split.feature, b_split.split_info, branch_no != 0
+                )
+            else:
+                lit = NominalLiteral(
+                    b_split.feature, b_split.split_info, branch_no != 0
+                )
+
+            # Add a new literal
+            self.literals.append(lit)
+
+            # Set the rule's statistics to match the data partition that was just performed
+            self.statistics = b_split.children_stats[branch_no]
+
+        self._last_expansion_attempt_at = self.total_weight
+        return should_expand
 
     def covers(self, x):
         return all(map(lambda lit: lit(x), self.literals))
@@ -79,21 +196,26 @@ class Rule(base.Estimator, metaclass=abc.ABCMeta):
             yield att_id, att_val
 
     @abc.abstractmethod
-    def _update_stats(self, y, w):
+    def _update_target_stats(self, y, w):
+        pass
+
+    def _update_feature_stats(self, feat_name, feat_val, w):
+        # By default, no feature stats are kept
         pass
 
     def update(self, x, y, w):
-        self._update_stats(y, w)
+        self._update_target_stats(y, w)
 
-        for att_id, att_val in self._iter_features(x):
+        for feat_name, feat_val in self._iter_features(x):
+            self._update_feature_stats(feat_name, feat_val, w)
             try:
-                splt = self.splitters[att_id]
+                splt = self.splitters[feat_name]
             except KeyError:
-                if isinstance(att_val, numbers.Number):
-                    self.splitters[att_id] = copy.deepcopy(self.template_splitter)
+                if isinstance(feat_val, numbers.Number):
+                    self.splitters[feat_name] = copy.deepcopy(self.template_splitter)
                 else:
-                    self.splitters[att_id] = self.new_nominal_splitter()
-                splt = self.splitters[att_id]
+                    self.splitters[feat_name] = self.new_nominal_splitter()
+                splt = self.splitters[feat_name]
             splt.update(x, y, w)
 
     def __repr__(self):
