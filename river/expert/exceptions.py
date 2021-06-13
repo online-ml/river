3a57150be43ce443fb1e52c74225b2d44c9aa838
Max Halford <maxhalford25@gmail.com>
2021-06-13 18:27:48 +0200

    black
    Former-commit-id: 0feb6b65b963c1bb1c5d3e64c508975bbfb6b9f7

--- river/expert/exceptions.py
+++ river/expert/exceptions.py
@@ -1,5 +1,6 @@
 class NotEnoughModels(ValueError):
-
     def __init__(self, n_expected, n_obtained):
-        message = f"At least {n_expected} models are expected, only {n_obtained} were passed"
+        message = (
+            f"At least {n_expected} models are expected, only {n_obtained} were passed"
+        )
         super().__init__(message)
