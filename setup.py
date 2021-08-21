9a78b8f0c14a837b4b1e46ec923f488ce2fb7e98
Max Halford <maxhalford25@gmail.com>
2021-08-21 17:27:38 +0200

    try different format

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ docs_packages = dev_packages + [
     "mkdocs-material",
     "nbconvert",
     "spacy",
-    "yamp @ git+ssh://git@github.com/MaxHalford/yamp.git",
+    "yamp @ git+https://github.com/MaxHalford/yamp",
 ]
 
 here = os.path.abspath(os.path.dirname(__file__))
