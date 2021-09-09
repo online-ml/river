a1aedf75133b63a30e9963c7371c1b9e19a31e45
Cedric Kulbach <kulbach@fzi.de>
2021-09-09 16:09:59 +0200

    Refactor PyTorch integration

--- river/compat/__init__.py
+++ river/compat/__init__.py
@@ -38,14 +38,8 @@ except ModuleNotFoundError:
     pass
 
 try:
-    from .pytorch import (
-        PyTorch2RiverRegressor,
-        PyTorch2RiverClassifier
-    )
+    from .pytorch import PyTorch2RiverClassifier, PyTorch2RiverRegressor
 
-    __all__ += [
-        "PyTorch2RiverRegressor",
-        "PyTorch2RiverClassifier"
-    ]
+    __all__ += ["PyTorch2RiverRegressor", "PyTorch2RiverClassifier"]
 except ModuleNotFoundError:
     pass
