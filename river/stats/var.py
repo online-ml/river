39f3be027ffb3afd773f707e6967e52b97da390f
Max Halford <maxhalford25@gmail.com>
2021-10-11 17:08:07 +0200

    implement stable variant of welford's algorithm

--- river/stats/var.py
+++ river/stats/var.py
@@ -4,7 +4,7 @@ from . import base, mean
 
 
 class Var(base.Univariate):
-    r"""Running variance using Welford's algorithm.
+    """Running variance using Welford's algorithm.
 
     Parameters
     ----------
@@ -14,15 +14,13 @@ class Var(base.Univariate):
 
     Attributes
     ----------
-    mean : stats.Mean
-        The running mean.
-    sigma : float
-        The running variance.
+    mean
+        It is necessary to calculate the mean of the data in order to calculate its variance.
 
     Notes
     -----
     The outcomes of the incremental and parallel updates are consistent with numpy's
-    batch processing when $\\text{ddof} \le 1$.
+    batch processing when $\\text{ddof} \\le 1$.
 
     Examples
     --------
@@ -51,73 +49,59 @@ class Var(base.Univariate):
     def __init__(self, ddof=1):
         self.ddof = ddof
         self.mean = mean.Mean()
-        self.sigma = 0.0
+        self._S = 0
 
     def update(self, x, w=1.0):
-        mean = self.mean.get()
+        mean_old = self.mean.get()
         self.mean.update(x, w)
-        if self.mean.n > self.ddof:
-            self.sigma += (
-                w
-                * ((x - mean) * (x - self.mean.get()) - self.sigma)
-                / (self.mean.n - self.ddof)
-            )
+        mean_new = self.mean.get()
+        self._S += w * (x - mean_old) * (x - mean_new)
         return self
 
     def get(self):
-        return self.sigma
+        if self.mean.n > self.ddof:
+            return self._S / (self.mean.n - self.ddof)
+        return 0.0
 
     def __iadd__(self, other):
-        if other.mean.n <= self.ddof:
-            return self
 
-        old_n = self.mean.n
-        delta = other.mean.get() - self.mean.get()
-
+        S = (
+            self._S
+            + other._S
+            + (self.mean.get() - other.mean.get()) ** 2
+            * self.mean.n
+            * other.mean.n
+            / (self.mean.n + other.mean.n)
+        )
         self.mean += other.mean
-        # scale and merge sigma
-        self.sigma = (old_n - self.ddof) * self.sigma + (
-            other.mean.n - other.ddof
-        ) * other.sigma
-        # apply correction
-        self.sigma = (
-            self.sigma + (delta * delta) * (old_n * other.mean.n) / self.mean.n
-        ) / (self.mean.n - self.ddof)
+        self._S = S
 
         return self
 
     def __add__(self, other):
         result = copy.deepcopy(self)
         result += other
-
         return result
 
     def __isub__(self, other):
-        old_n = self.mean.n
-        delta = 0.0
 
         self.mean -= other.mean
 
-        if self.mean.n > 0 and self.mean.n > self.ddof:
-            delta = other.mean.get() - self.mean.get()
-            # scale both sigma and take the difference
-            self.sigma = (old_n - self.ddof) * self.sigma - (
-                other.mean.n - other.ddof
-            ) * other.sigma
-            # apply the correction
-            self.sigma = (
-                self.sigma - (delta * delta) * (self.mean.n * other.mean.n) / old_n
-            ) / (self.mean.n - self.ddof)
-
-        else:
-            self.sigma = 0.0
+        S = (
+            self._S
+            - other._S
+            - (self.mean.get() - other.mean.get()) ** 2
+            * self.mean.n
+            * other.mean.n
+            / (self.mean.n + other.mean.n)
+        )
+        self._S = S
 
         return self
 
     def __sub__(self, other):
         result = copy.deepcopy(self)
         result -= other
-
         return result
 
 
@@ -126,16 +110,11 @@ class RollingVar(base.RollingUnivariate):
 
     Parameters
     ----------
-    window_size : int
+    window_size
         Size of the rolling window.
-    ddof : int
-        Delta Degrees of Freedom for the variance.
-
-    Attributes
-    ----------
-    sos : float
-        Sum of squares over the current window.
-    rmean : stats.RollingMean
+    ddof
+        Delta Degrees of Freedom. The divisor used in calculations is `n - ddof`, where `n`
+        represents the number of seen elements.
 
     Examples
     --------
@@ -168,31 +147,31 @@ class RollingVar(base.RollingUnivariate):
 
     def __init__(self, window_size, ddof=1):
         self.ddof = ddof
-        self.sos = 0
-        self.rolling_mean = mean.RollingMean(window_size=window_size)
+        self._sos = 0
+        self._rolling_mean = mean.RollingMean(window_size=window_size)
 
     @property
     def window_size(self):
-        return self.rolling_mean.window_size
+        return self._rolling_mean.window_size
 
     def update(self, x):
-        if len(self.rolling_mean) >= self.rolling_mean.size:
-            self.sos -= self.rolling_mean[0] ** 2
+        if len(self._rolling_mean) >= self._rolling_mean.size:
+            self._sos -= self._rolling_mean[0] ** 2
 
-        self.sos += x * x
-        self.rolling_mean.update(x)
+        self._sos += x * x
+        self._rolling_mean.update(x)
         return self
 
     @property
     def correction_factor(self):
-        n = len(self.rolling_mean)
+        n = len(self._rolling_mean)
         if n > self.ddof:
             return n / (n - self.ddof)
         return 1
 
     def get(self):
         try:
-            var = (self.sos / len(self.rolling_mean)) - self.rolling_mean.get() ** 2
+            var = (self._sos / len(self._rolling_mean)) - self._rolling_mean.get() ** 2
             return self.correction_factor * var
         except ZeroDivisionError:
             return 0.0
