05798e57d045854e6e00a24c5e57509c09ce782d
Max Halford <maxhalford25@gmail.com>
2021-10-22 15:34:17 +0200

    add multiplicative formulation with test

--- river/time_series/holt_winters.py
+++ river/time_series/holt_winters.py
@@ -1,3 +1,4 @@
+import operator
 import statistics
 from collections import deque
 
@@ -10,7 +11,7 @@ class Component(deque):
     ...
 
 
-class Level(Component):
+class AdditiveLevel(Component):
     def __init__(self, alpha):
         super().__init__([], maxlen=2)
         self.alpha = alpha
@@ -18,6 +19,18 @@ class Level(Component):
     def update(self, y, trend, season):
         self.append(
             self.alpha * (y - (season[-season.seasonality] if season else 0))
+            + (1 - self.alpha) * (self[-1] + (trend[-1] if trend else 0))
+        )
+
+
+class MultiplicativeLevel(Component):
+    def __init__(self, alpha):
+        super().__init__([], maxlen=2)
+        self.alpha = alpha
+
+    def update(self, y, trend, season):
+        self.append(
+            self.alpha * (y / (season[-season.seasonality] if season else 1))
             + (1 - self.alpha) * (self[-1] + (trend[-1] if trend else 0))
         )
 
@@ -31,7 +44,7 @@ class Trend(Component):
         self.append(self.beta * (level[-1] - level[-2]) + (1 - self.beta) * self[-1])
 
 
-class Season(Component):
+class AdditiveSeason(Component):
     def __init__(self, gamma, seasonality):
         super().__init__([], maxlen=seasonality + 1)
         self.gamma = gamma
@@ -40,6 +53,19 @@ class Season(Component):
     def update(self, y, level, trend):
         self.append(
             self.gamma * (y - level[-2] - trend[-2])
+            + (1 - self.gamma) * self[-self.seasonality]
+        )
+
+
+class MultiplicativeSeason(Component):
+    def __init__(self, gamma, seasonality):
+        super().__init__([], maxlen=seasonality + 1)
+        self.gamma = gamma
+        self.seasonality = seasonality
+
+    def update(self, y, level, trend):
+        self.append(
+            self.gamma * y / (level[-2] + trend[-2])
             + (1 - self.gamma) * self[-self.seasonality]
         )
 
@@ -53,11 +79,12 @@ class HoltWinters(Forecaster):
 
     """
 
-    def __init__(self, alpha, beta=None, gamma=None, seasonality: int = None):
-        self.level = Level(alpha)
+    def __init__(self, alpha, beta=None, gamma=None, seasonality: int = None, multiplicative=False):
+        self.level = MultiplicativeLevel(alpha) if multiplicative else AdditiveLevel(alpha)
         self.trend = Trend(beta) if beta else None
-        self.season = Season(gamma, seasonality) if (gamma or seasonality) else None
+        self.season = (MultiplicativeSeason(gamma, seasonality) if multiplicative else AdditiveSeason(gamma, seasonality)) if (gamma or seasonality) else None
         self.seasonality = seasonality
+        self.multiplicative = multiplicative
         self._first_values = []
         self._initialized = False
 
@@ -87,13 +114,15 @@ class HoltWinters(Forecaster):
         return self
 
     def forecast(self, horizon, xs=None):
+        op = operator.mul if self.multiplicative else operator.add
         return [
-            self.level[-1]
-            + ((h + 1) * self.trend[-1] if self.trend else 0)
-            + (
-                self.season[-self.seasonality + h % self.seasonality]
-                if self.seasonality
-                else 0
+            op(
+                self.level[-1] + ((h + 1) * self.trend[-1] if self.trend else 0),
+                (
+                    self.season[-self.seasonality + h % self.seasonality]
+                    if self.seasonality
+                    else 0
+                )
             )
             for h in range(horizon)
         ]
