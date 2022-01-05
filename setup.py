82a34b66c407880d4acd795059f8f8c9e09c027d
Raphael Sourty <raphael.sourty@gmail.com>
2022-01-05 19:59:52 +0100

    Upgrade version of numpy to avoid ImportError

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ AUTHOR = "Max Halford"
 REQUIRES_PYTHON = ">=3.7.0"
 
 # Package requirements.
-base_packages = ["numpy>=1.18.1", "scipy>=1.4.1", "pandas>=1.0.1"]
+base_packages = ["numpy>=1.20.0", "scipy>=1.4.1", "pandas>=1.0.1"]
 
 compat_packages = base_packages + [
     "scikit-learn",
