37f7dd732fb7f97176a503678a27ab481ecbfae3
Pranab Ghosh <pkghosh99@gmail.com>
2022-01-18 22:25:08 -0800

    online zscore anomaly

--- river/anomaly/__init__.py
+++ river/anomaly/__init__.py
@@ -20,4 +20,5 @@ __all__ = [
     "HalfSpaceTrees",
     "QuantileThresholder",
     "OneClassSVM",
+    "Zscore"
 ]
---
+++ river/anomaly/zscore.py
@@ -0,0 +1,46 @@
+
+from river.stats.var import RollingVar
+
+from base import AnomalyDetector
+
+
+class Zscore(AnomalyDetector):
+    """
+
+    """
+
+    def __init__(self, window_size:int, zs_threshold:float = 3.0):
+        """
+
+        Parameters
+        ----------
+        window_size
+        zs_threshold
+        """
+        self.var = RollingVar(window_size)
+        self.zs_threshold = zs_threshold
+
+        def learn_one(self, x:float):
+            """
+            Parameters
+            ----------
+            x the value
+            Returns
+            -------
+            """
+            self.var.update(x)
+
+        def score_one(self, x:float) -> float :
+            """
+            Parameters
+            ----------
+            x   the value
+            Returns
+            -------
+            anomaly score between 0 and 1
+            """
+            sd = sqrt(self.var.get())
+            mean = self.var._rolling_mean.get()
+            zs = abs(x - mean) / sd
+            score = 1.0 if zs > self.zs_threshold else zs / self.zs_threshold
+            return score
