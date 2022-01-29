aa061c195d939f2a8ca7712edeeef7b2d82ba58f
Max Halford <maxhalford25@gmail.com>
2022-01-29 23:39:25 +0100

    expose forecasting metrics

--- river/time_series/__init__.py
+++ river/time_series/__init__.py
@@ -2,6 +2,14 @@
 from .base import Forecaster
 from .evaluate import evaluate
 from .holt_winters import HoltWinters
+from .metrics import ForecastingMetric, HorizonMetric
 from .snarimax import SNARIMAX
 
-__all__ = ["evaluate", "Forecaster", "HoltWinters", "SNARIMAX"]
+__all__ = [
+    "evaluate",
+    "Forecaster",
+    "ForecastingMetric",
+    "HorizonMetric",
+    "HoltWinters",
+    "SNARIMAX",
+]
