6a2d19f31cf6cc208b8eff0c0aa6f6a2ffa5648d
Max Halford <maxhalford25@gmail.com>
2022-02-09 13:20:23 +0100

    Update test_cov_matrix.py

--- river/misc/test_cov_matrix.py
+++ river/misc/test_cov_matrix.py
@@ -1,17 +1,17 @@
-import itertools
 import math
 
 import numpy as np
 import pandas as pd
-import pytest
 
 from river import misc
 
 
-@pytest.mark.parametrize("ddof", [0, 1, 2], ids=lambda ddof: f"ddof={ddof}")
-def test_cov_matrix(ddof):
+def test_cov_matrix():
 
-    cov = misc.CovMatrix(ddof)
+    # NOTE: this test only works with ddof=1 because pandas ignores it if there are missing values
+    ddof = 1
+
+    cov = misc.CovMatrix(ddof=ddof)
     p = 5
     X_all = pd.DataFrame(columns=range(p))
 
@@ -19,11 +19,8 @@ def test_cov_matrix(ddof):
         n = np.random.randint(1, 31)
         X = pd.DataFrame(np.random.random((n, p))).sample(3, axis="columns")
         cov.update_many(X)
-        X_all = pd.concat((X_all, X))
+        X_all = pd.concat((X_all, X)).astype(float)
+        pd_cov = X_all.cov(ddof=ddof)
 
-        for i, j in itertools.combinations_with_replacement(range(p), r=2):
-            not_null = X_all[i].notnull() & X_all[j].notnull()
-            if not not_null.any():
-                continue
-            np_cov = np.cov(X_all[not_null][[i, j]], rowvar=False, ddof=ddof)
-            assert math.isclose(cov[i, j].get(), np_cov[0, 1])
+        for i, j in cov:
+            assert math.isclose(cov[i, j].get(), pd_cov.loc[i, j])
