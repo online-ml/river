993328ee58fb267facda47ab8eb14c365b41c960
Leo-VK <61838765+Leo-VK@users.noreply.github.com>
2022-03-09 17:43:26 +0100

    Hotfix dotvecmat (#865)
    * Kalman filter optimizer
    
    * Update kalman_filter.py
    
    * General losses
    
    * Update math.py
    
    * Change API
    
    Change API
    
    * Create mini_batch.py
    
    * Update base.py
    
    * Revert Kalman fitler
    
    * Revert "Update math.py"
    
    This reverts commit 19e66d5417c316445513568a8d10d6032f110390.
    
    * Revert Kalman filter
    
    * Adapt to current river master
    
    * Correct index of dotvecmat
    
    * Write tests for dotvecmat with numpy as reference
    
    * Correct doctest
    
    * Reformatting test
    
    * Incorporate reviewer comments
    
    Co-authored-by: Leandro Von Krannichfeldt <vleandro@student.ethz.ch>

---
+++ river/utils/test_math.py
@@ -0,0 +1,110 @@
+import numpy as np
+
+from river import utils
+
+
+def test_dotvecmat_zero_vector_times_matrix_of_ones():
+    A_numpy = np.array([[1, 1], [1, 1]])
+    A_river = {(0, 0): 1, (0, 1): 1, (1, 0): 1, (1, 1): 1}
+    x_numpy = np.array([0, 0])
+    x_river = {0: 0, 1: 0}
+
+    numpy_dotvecmat = x_numpy.dot(A_numpy)
+    river_dotvecmat = utils.math.dotvecmat(x_river, A_river)
+
+    assert numpy_dotvecmat[0] == river_dotvecmat[0]
+    assert numpy_dotvecmat[1] == river_dotvecmat[1]
+
+
+def test_dotvecmat_vector_of_ones_times_zero_matrix():
+    A_numpy = np.array([[0, 0], [0, 0]])
+    A_river = {(0, 0): 0, (0, 1): 0, (1, 0): 0, (1, 1): 0}
+    x_numpy = np.array([1, 1])
+    x_river = {0: 1, 1: 1}
+
+    numpy_dotvecmat = x_numpy.dot(A_numpy)
+    river_dotvecmat = utils.math.dotvecmat(x_river, A_river)
+
+    assert numpy_dotvecmat[0] == river_dotvecmat[0]
+    assert numpy_dotvecmat[1] == river_dotvecmat[1]
+
+
+def test_dotvecmat_filter_first_matrix_row_with_vector():
+    A_numpy = np.array([[1, 1], [1, 1]])
+    A_river = {(0, 0): 1, (0, 1): 1, (1, 0): 1, (1, 1): 1}
+    x_numpy = np.array([1, 0])
+    x_river = {0: 1, 1: 0}
+
+    numpy_dotvecmat = x_numpy.dot(A_numpy)
+    river_dotvecmat = utils.math.dotvecmat(x_river, A_river)
+
+    assert numpy_dotvecmat[0] == river_dotvecmat[0]
+    assert numpy_dotvecmat[1] == river_dotvecmat[1]
+
+
+def test_dotvecmat_filter_second_matrix_row_with_vector():
+    A_numpy = np.array([[1, 1], [1, 1]])
+    A_river = {(0, 0): 1, (0, 1): 1, (1, 0): 1, (1, 1): 1}
+    x_numpy = np.array([0, 1])
+    x_river = {0: 0, 1: 1}
+
+    numpy_dotvecmat = x_numpy.dot(A_numpy)
+    river_dotvecmat = utils.math.dotvecmat(x_river, A_river)
+
+    assert numpy_dotvecmat[0] == river_dotvecmat[0]
+    assert numpy_dotvecmat[1] == river_dotvecmat[1]
+
+
+def test_dotvecmat_vector_times_identity_matrix():
+    A_numpy = np.array([[1, 0], [0, 1]])
+    A_river = {(0, 0): 1, (0, 1): 0, (1, 0): 0, (1, 1): 1}
+    x_numpy = np.array([1, 1])
+    x_river = {0: 1, 1: 1}
+
+    numpy_dotvecmat = x_numpy.dot(A_numpy)
+    river_dotvecmat = utils.math.dotvecmat(x_river, A_river)
+
+    assert numpy_dotvecmat[0] == river_dotvecmat[0]
+    assert numpy_dotvecmat[1] == river_dotvecmat[1]
+
+
+def test_dotvecmat_vector_times_anti_diagonal_identity_matrix():
+    A_numpy = np.array([[0, 1], [1, 0]])
+    A_river = {(0, 0): 0, (0, 1): 1, (1, 0): 1, (1, 1): 0}
+    x_numpy = np.array([1, 1])
+    x_river = {0: 1, 1: 1}
+
+    numpy_dotvecmat = x_numpy.dot(A_numpy)
+    river_dotvecmat = utils.math.dotvecmat(x_river, A_river)
+
+    assert numpy_dotvecmat[0] == river_dotvecmat[0]
+    assert numpy_dotvecmat[1] == river_dotvecmat[1]
+
+
+def test_dotvecmat_three_dimensional_vector_times_non_quadratic_matrix():
+    A_numpy = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]])
+    A_river = {
+        (0, 0): 1,
+        (0, 1): 2,
+        (0, 2): 3,
+        (0, 3): 4,
+        (0, 5): 5,
+        (1, 0): 6,
+        (1, 1): 7,
+        (1, 2): 8,
+        (1, 3): 9,
+        (1, 5): 10,
+        (2, 0): 11,
+        (2, 1): 12,
+        (2, 2): 13,
+        (2, 3): 14,
+        (2, 5): 15,
+    }
+    x_numpy = np.array([1, 2, 3])
+    x_river = {0: 1, 1: 2, 2: 3}
+
+    numpy_dotvecmat = x_numpy.dot(A_numpy)
+    river_dotvecmat = utils.math.dotvecmat(x_river, A_river)
+
+    assert numpy_dotvecmat[0] == river_dotvecmat[0]
+    assert numpy_dotvecmat[1] == river_dotvecmat[1]
