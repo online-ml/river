115ba6364b443b43afede545b0045d3c8f06eb25
smastelini <saulomastelini@gmail.com>
2022-03-14 15:39:25 +0000

    add Chebyshev's inequality-based under- and over-sampling methods

--- river/imblearn/__init__.py
+++ river/imblearn/__init__.py
@@ -1,8 +1,11 @@
 """Sampling methods."""
+from .chebyshev import ChebyshevOverSampler, ChebyshevUnderSampler
 from .hard_sampling import HardSamplingClassifier, HardSamplingRegressor
 from .random import RandomOverSampler, RandomSampler, RandomUnderSampler
 
 __all__ = [
+    "ChebyshevOverSampler",
+    "ChebyshevUnderSampler",
     "HardSamplingClassifier",
     "HardSamplingRegressor",
     "RandomOverSampler",
