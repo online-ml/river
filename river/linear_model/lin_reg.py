0d9a534cc25112bb6970d15d80812dd4f0c85c02
Hoang-Anh Ngo <50743576+hoanganhngo610@users.noreply.github.com>
2022-04-09 10:31:06 +0200

    Minor changes in docstring of LinearRegression class

--- river/linear_model/lin_reg.py
+++ river/linear_model/lin_reg.py
@@ -26,9 +26,11 @@ class LinearRegression(GLM, base.MiniBatchRegressor):
     loss
         The loss function to optimize for.
     l2
-        Amount of L2 regularization used to push weights towards 0. (only one type of penalty can be used for now)
+        Amount of L2 regularization used to push weights towards 0.
+        For now, only one type of penalty can be used. The joint use of L1 and L2 is not explicitly supported.
     l1
-        Amount of L1 regularization used to push weights towards 0. (only one type of penalty can be used for now)
+        Amount of L1 regularization used to push weights towards 0.
+        For now, only one type of penalty can be used. The joint use of L1 and L2 is not explicitly supported.
     intercept_init
         Initial intercept value.
     intercept_lr
