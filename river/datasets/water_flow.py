60872844e6052b5ef20e4075aea30f9031377136
dberardo-com <65530457+dberardo-com@users.noreply.github.com>
2022-05-31 16:30:54 +0200

    Update water_flow.py (#939)
    * Update water_flow.py
    
    * Update water_flow.py

--- river/datasets/water_flow.py
+++ river/datasets/water_flow.py
@@ -9,15 +9,15 @@ class WaterFlow(base.FileDataset):
     The series includes hourly values for about 2 months, March 2022 to May 2022. The values are
     expressed in liters per second. There are four anomalous segments in the series:
 
-    - 3 "low value" moments: this is due to water losses or human intervention for maintenance.
-    - A small peak in the water inflow after the first 2 segments: this is due to a pumping
-        operation into the main pipeline, when more water pressure is needed.
+    * 3 "low value moments": this is due to water losses or human intervention for maintenance
+    * A small peak in the water inflow after the first 2 segments: this is due to a pumping
+        operation into the main pipeline, when more water pressure is needed
 
     This dataset is well suited for time series forecasting models, as well as anomaly detection
     methods. Ideally, the goal is to build a time series forecasting model that is robust to the
     anomalous segments.
 
-    This data has been kindly donated by the TecnojJest s.r.l. company (www.invidea.it) from Italy.
+    This data has been kindly donated by the Tecnojest s.r.l. company (www.invidea.it) from Italy.
 
     """
 
