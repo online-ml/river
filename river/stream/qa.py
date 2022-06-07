7b92cb0b0b2a74e20107f5624c138af7a8210b8a
Max Halford <maxhalford25@gmail.com>
2022-06-08 01:11:38 +0200

    Update qa.py

--- river/stream/qa.py
+++ river/stream/qa.py
@@ -113,27 +113,23 @@ def simulate_qa(
 
     # Determine how to insert mementos into the queue
     queue = (
-        (lambda q, el: bisect.insort(q, el))
-        if callable(delay) or isinstance(delay, str)
-        else (lambda q, el: q.append(el))
+        (lambda q, el: bisect.insort(q, el)) if callable(delay) or isinstance(delay, str) else
+        (lambda q, el: q.append(el))
     )
 
     # Coerce moment to a function
     get_moment = (
-        (lambda _, x: x[moment])
-        if isinstance(moment, str)
-        else (lambda _, x: moment(x))
-        if callable(moment)
-        else (lambda i, _: i)  # type: ignore
+        (lambda _, x: x[moment]) if isinstance(moment, str) else
+        (lambda _, x: moment(x)) if callable(moment) else  # type: ignore
+        (lambda i, _: i)
     )
 
     # Coerce delay to a function
     get_delay = (
-        (lambda i, _: 0)
-        if delay is None
-        else (lambda x, _: x[delay])
-        if isinstance(delay, str)
-        else (lambda _, __: delay)  # type: ignore
+        (lambda i, _: 0) if delay is None else
+        (lambda x, _: x[delay]) if isinstance(delay, str) else
+        (lambda _, __: delay) if not callable(delay) else  # type: ignore
+        delay
     )
 
     mementos: typing.List[Memento] = []
@@ -145,7 +141,7 @@ def simulate_qa(
         kwargs = kwargs[0] if kwargs else None
 
         t = get_moment(i, x)
-        d = get_delay(x, y)
+        d = get_delay(x, y)  # type: ignore
 
         while mementos:
 
