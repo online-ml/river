b37d16d8a88958f5eb412e51f9db17ac35f24b60
Max Halford <maxhalford25@gmail.com>
2022-06-07 21:46:38 +0200

    Log reg benchmarks (#955)
    * Update run.py
    
    * Update CONTRIBUTING.md
    
    * remove old log reg benchmarks

--- benchmarks/run.py
+++ benchmarks/run.py
@@ -4,6 +4,8 @@ import shelve
 import sys
 
 from river import (
+    base,
+    compat,
     drift,
     dummy,
     ensemble,
@@ -12,9 +14,13 @@ from river import (
     metrics,
     naive_bayes,
     neighbors,
+    optim
 )
 from river import neural_net as nn
 from river import optim, preprocessing, rules, stats, tree
+from sklearn.linear_model import SGDClassifier
+import torch
+from vowpalwabbit import pyvw
 
 
 def run_track(models, track, benchmarks):
@@ -54,11 +60,128 @@ tracks = [
     evaluate.RegressionTrack(),
 ]
 
+class PyTorchLogReg(torch.nn.Module):
+
+    def __init__(self, n_features):
+        super().__init__()
+        self.linear = torch.nn.Linear(n_features, 1)
+        self.sigmoid = torch.nn.Sigmoid()
+        torch.nn.init.constant_(self.linear.weight, 0)
+        torch.nn.init.constant_(self.linear.bias, 0)
+
+    def forward(self, x):
+        return self.sigmoid(self.linear(x))
+
+class PyTorchModel:
+    def __init__(self, network_func, loss, optimizer_func):
+        self.network_func = network_func
+        self.loss = loss
+        self.optimizer_func = optimizer_func
+
+        self.network = None
+        self.optimizer = None
+
+    def learn_one(self, x, y):
+
+        # We only know how many features a dataset contains at runtime
+        if self.network is None:
+            self.network = self.network_func(n_features=len(x))
+            self.optimizer = self.optimizer_func(self.network.parameters())
+
+        x = torch.FloatTensor(list(x.values()))
+        y = torch.FloatTensor([y])
+
+        y_pred = self.network(x)
+        loss = self.loss(y_pred, y)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        return self
+
+class PyTorchBinaryClassifier(PyTorchModel, base.Classifier):
+
+    def predict_proba_one(self, x):
+
+        # We only know how many features a dataset contains at runtime
+        if self.network is None:
+            self.network = self.network_func(n_features=len(x))
+            self.optimizer = self.optimizer_func(self.network.parameters())
+
+        x = torch.FloatTensor(list(x.values()))
+        p = self.network(x).item()
+        return {True: p, False: 1.0 - p}
+
+class VW2RiverBase:
+
+    def __init__(self, *args, **kwargs):
+        self.vw = pyvw.vw(*args, **kwargs)
+
+    def _format_x(self, x):
+        return ' '.join(f'{k}:{v}' for k, v in x.items())
+
+
+class VW2RiverClassifier(VW2RiverBase, base.Classifier):
+
+    def learn_one(self, x, y):
+
+        # Convert {False, True} to {-1, 1}
+        y = int(y)
+        y_vw = 2 * y - 1
+
+        ex = self._format_x(x)
+        ex = f'{y_vw} | {ex}'
+        self.vw.learn(ex)
+        return self
+
+    def predict_proba_one(self, x):
+        ex = '| ' + self._format_x(x)
+        y_pred = self.vw.predict(ex)
+        return {True: y_pred, False: 1.0 - y_pred}
+
+LEARNING_RATE = 0.005
+
 models = {
     "Binary classification": {
-        "Logistic regression": preprocessing.StandardScaler() | linear_model.LogisticRegression(),
+        "Logistic regression": (
+            preprocessing.StandardScaler() |
+            linear_model.LogisticRegression(optimizer=optim.SGD(LEARNING_RATE))
+        ),
         "ALMA": preprocessing.StandardScaler() | linear_model.ALMAClassifier(),
         "Stochastic Gradient Tree": tree.SGTClassifier(),
+        'sklearn SGDClassifier': (
+            StandardScaler() |
+            compat.SKL2RiverClassifier(
+                SGDClassifier(
+                    loss='log',
+                    learning_rate='constant',
+                    eta0=LEARNING_RATE,
+                    penalty='none'
+                ),
+                classes=[False, True]
+            )
+        ),
+        'PyTorch logistic regression': (
+            preprocessing.StandardScaler() |
+            PyTorchBinaryClassifier(
+                network_func=PyTorchLogReg,
+                loss=torch.nn.BCELoss(),
+                optimizer_func=lambda params: torch.optim.SGD(params, lr=LEARNING_RATE)
+            )
+        ),
+        'Vowpal Wabbit logistic regression': VW2RiverClassifier(
+            sgd=True,
+            learning_rate=LEARNING_RATE,
+            loss_function='logistic',
+            link='logistic',
+            adaptive=False,
+            normalized=False,
+            invariant=False,
+            l2=0.,
+            l1=0.,
+            power_t=0,
+            quiet=True
+        )
     },
     "Multiclass classification": {
         "Naive Bayes": naive_bayes.GaussianNB(),
