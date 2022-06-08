ff4d78f6072e383cb3b350151d760035231b692f
Max Halford <maxhalford25@gmail.com>
2022-06-08 02:09:51 +0200

    fix elementtree

--- river/tree/viz.py
+++ river/tree/viz.py
@@ -4,8 +4,8 @@ from xml.etree import ElementTree as ET
 from river.tree.base import Branch, Leaf
 
 
-def tree_to_html(tree: Branch) -> ET:  # type: ignore
-    def add_node(node: Union[Branch, Leaf], parent: ET):  # type: ignore
+def tree_to_html(tree: Branch) -> ET.Element:
+    def add_node(node: Union[Branch, Leaf], parent:  ET.Element):
 
         # We're building this:
         #
