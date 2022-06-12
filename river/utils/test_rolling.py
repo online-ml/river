0494ed7e7fa4de0e6850565d33d7ad0fb1fba704
Max Halford <maxhalford25@gmail.com>
2022-06-12 19:06:00 +0200

    Create test_rolling.py

---
+++ river/utils/test_rolling.py
@@ -0,0 +1,22 @@
+from river import utils
+
+def test_with_counter():
+    """
+    >>> import collections
+    >>> collections.Counter.revert = collections.Counter.subtract
+
+    >>> counter = utils.Rolling(collections.Counter(), window_size=3)
+
+    >>> for i in range(5):
+    ...     counter = counter.update([i])
+
+    >>> counter
+    Counter({2: 1, 3: 1, 4: 1, 0: 0, 1: 0})
+
+    >>> counter.most_common(3)
+    [(2, 1), (3, 1), (4, 1)]
+
+    >>> counter[4]
+    1
+
+    """
