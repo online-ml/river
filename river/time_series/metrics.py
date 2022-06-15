7bfd942da52d9efe3ee411ee93556df31130d13f
Max Halford <maxhalford25@gmail.com>
2022-06-15 08:28:59 +0200

    Implement average forecast metric (#969)
    * Update home.html
    
    * Implement HorizonAggMetric
    
    * black
    
    * Update metrics.py
    
    Former-commit-id: 8d4d62958e5b43357d11f086f9eb4ec8eb3eb83b

--- river/time_series/metrics.py
+++ river/time_series/metrics.py
@@ -1,15 +1,17 @@
 from __future__ import annotations
 
 import abc
+import typing
 from numbers import Number
-from typing import List
 
 from river import base, metrics
 
 
 class ForecastingMetric(base.Base, abc.ABC):
     @abc.abstractmethod
-    def update(self, y_true: List[Number], y_pred: List[Number]) -> "ForecastingMetric":
+    def update(
+        self, y_true: typing.List[Number], y_pred: typing.List[Number]
+    ) -> "ForecastingMetric":
         """Update the metric at each step along the horizon.
 
         Parameters
@@ -26,7 +28,7 @@ class ForecastingMetric(base.Base, abc.ABC):
         """
 
     @abc.abstractmethod
-    def get(self) -> List[float]:
+    def get(self) -> typing.List[float]:
         """Return the current performance along the horizon.
 
         Returns
@@ -47,11 +49,33 @@ class HorizonMetric(ForecastingMetric):
     metric
         A regression metric.
 
+    Examples
+    --------
+
+    This is used internally by the `time_series.evaluate` function.
+
+    >>> from river import datasets
+    >>> from river import metrics
+    >>> from river import time_series
+
+    >>> metric = time_series.evaluate(
+    ...     dataset=datasets.AirlinePassengers(),
+    ...     model=time_series.HoltWinters(alpha=0.1),
+    ...     metric=metrics.MAE(),
+    ...     horizon=4
+    ... )
+
+    >>> metric
+    +1 MAE: 40.931286
+    +2 MAE: 42.667998
+    +3 MAE: 44.158092
+    +4 MAE: 43.849617
+
     """
 
     def __init__(self, metric: metrics.base.RegressionMetric):
         self.metric = metric
-        self.metrics: list[int] = []
+        self.metrics: list[metrics.base.RegressionMetric] = []
 
     def update(self, y_true, y_pred):
         for t, (yt, yp) in enumerate(zip(y_true, y_pred)):
@@ -74,3 +98,54 @@ class HorizonMetric(ForecastingMetric):
         return "\n".join(
             f"{prefix:<{prefix_pad}} {metric}" for prefix, metric in zip(prefixes, self.metrics)
         )
+
+
+class HorizonAggMetric(HorizonMetric):
+    """Same as `HorizonMetric`, but aggregates the result based on an provided function.
+
+    This allows, for instance, to measure the average performance of a forecasting model along the
+    horizon.
+
+    Parameters
+    ----------
+    metric
+        A regression metric.
+    agg_func
+        A function that takes as input a list of floats and outputs a single float. You may want to
+        `min`, `max`, as well as `statistics.mean` and `statistics.median`.
+
+    Examples
+    --------
+
+    This is used internally by the `time_series.evaluate` function when you pass an `agg_func`.
+
+    >>> import statistics
+    >>> from river import datasets
+    >>> from river import metrics
+    >>> from river import time_series
+
+    >>> metric = time_series.evaluate(
+    ...     dataset=datasets.AirlinePassengers(),
+    ...     model=time_series.HoltWinters(alpha=0.1),
+    ...     metric=metrics.MAE(),
+    ...     agg_func=statistics.mean,
+    ...     horizon=4
+    ... )
+
+    >>> metric
+    mean(MAE): 42.901748
+
+    """
+
+    def __init__(
+        self, metric: metrics.base.RegressionMetric, agg_func: typing.Callable[[list[float]], float]
+    ):
+        super().__init__(metric)
+        self.agg_func = agg_func
+
+    def get(self):
+        return self.agg_func(super().get())
+
+    def __repr__(self):
+        name = f"{self.agg_func.__name__}({self.metric.__class__.__name__})"
+        return f"{name}: {self.get():{self.metric._fmt}}".rstrip("0")
