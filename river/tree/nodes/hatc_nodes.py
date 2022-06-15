84ceda640ba3175c966f85d05924d298d1fba011
Saulo Martiello Mastelini <mastelini@usp.br>
2022-06-15 09:42:04 +0100

    update comments

--- river/tree/nodes/hatc_nodes.py
+++ river/tree/nodes/hatc_nodes.py
@@ -218,11 +218,11 @@ class AdaBranchClassifier(DTBranch):
                 alt_error_rate = self._alternate_tree._mean_error.get()
 
                 f_delta = 0.05  # TODO make this a parameter
-                f_n = 1.0 / alt_n_obs + 1.0 / n_obs  # TODO Number observations Hoeffding bound
+                f_n = 1.0 / alt_n_obs + 1.0 / n_obs
 
                 bound = math.sqrt(
                     2.0 * old_error_rate * (1.0 - old_error_rate) * math.log(2.0 / f_delta) * f_n
-                )  # TODO This is a form of the Hoeffding Bound
+                )  # TODO Is this is a form of the Hoeffding Bound?
 
                 if bound < (old_error_rate - alt_error_rate):
                     tree._n_active_leaves -= self.n_leaves
