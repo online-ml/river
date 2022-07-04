f5d7c90d529c903f3e28cc8578f3609d00b30e6a
Max Halford <maxhalford25@gmail.com>
2022-07-04 15:13:33 +0200

    init InvCovMatrix

--- river/misc/__init__.py
+++ river/misc/__init__.py
@@ -4,8 +4,9 @@ This module essentially regroups some implementations that have nowhere else to 
 
 """
 from .cov_matrix import CovMatrix
+from .inv_cov_matrix import InvCovMatrix
 from .histogram import Histogram
 from .sdft import SDFT
 from .skyline import Skyline
 
-__all__ = ["CovMatrix", "Histogram", "SDFT", "Skyline"]
+__all__ = ["CovMatrix", "InvCovMatrix", "Histogram", "SDFT", "Skyline"]
---
+++ river/misc/inv_cov_matrix.py
@@ -0,0 +1,10 @@
+class InvCovMatrix:
+    """Inverse covariance matrix.
+
+    References
+
+    ----------
+
+    [^1]: [Fast rank-one updates to matrix inverse?](https://timvieira.github.io/blog/post/2021/03/25/fast-rank-one-updates-to-matrix-inverse/)
+
+    """
