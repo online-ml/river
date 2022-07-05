b676cb003be28d7f98acaed371ea2fb0c087d25e
Max Halford <maxhalford25@gmail.com>
2022-07-05 16:06:20 +0200

    Update inv_cov_matrix.py

--- river/misc/inv_cov_matrix.py
+++ river/misc/inv_cov_matrix.py
@@ -4,7 +4,7 @@ class InvCovMatrix:
     References
 
     ----------
-
-    [^1]: [Fast rank-one updates to matrix inverse?](https://timvieira.github.io/blog/post/2021/03/25/fast-rank-one-updates-to-matrix-inverse/)
+    [^1]: [Online Estimation of the Inverse Covariance Matrix - Markus Thill](https://markusthill.github.io/math/stats/ml/online-estimation-of-the-inverse-covariance-matrix/)
+    [^2]: [Fast rank-one updates to matrix inverse? - Tim Vieira](https://timvieira.github.io/blog/post/2021/03/25/fast-rank-one-updates-to-matrix-inverse/)
 
     """
