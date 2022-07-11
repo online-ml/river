063d9798255e4a4cdb0c3a2cff9a24db4e5bd923
Max Halford <maxhalford25@gmail.com>
2022-07-12 00:44:40 +0200

    wip

--- river/covariance/emp.py
+++ river/covariance/emp.py
@@ -58,7 +58,7 @@ class EmpiricalCovariance(SymmetricMatrix):
 
     >>> import numpy as np
     >>> import pandas as pd
-    >>> from river import misc
+    >>> from river import covariance
 
     >>> np.random.seed(42)
     >>> X = pd.DataFrame(np.random.random((8, 3)), columns=["red", "green", "blue"])
@@ -73,7 +73,7 @@ class EmpiricalCovariance(SymmetricMatrix):
     6  0.431945  0.291229  0.611853
     7  0.139494  0.292145  0.366362
 
-    >>> cov = misc.CovMatrix()
+    >>> cov = covariance.EmpiricalCovariance()
     >>> for x in X.to_dict(orient="records"):
     ...     cov = cov.update(x)
     >>> cov
@@ -84,10 +84,10 @@ class EmpiricalCovariance(SymmetricMatrix):
 
     There is also an `update_many` method to process mini-batches. The results are identical.
 
-    >>> cov = misc.CovMatrix()
+    >>> cov = covariance.EmpiricalCovariance()
     >>> cov = cov.update_many(X)
     >>> cov
-              blue     green    red
+            blue     green    red
      blue    0.076    0.020   -0.010
     green    0.020    0.113   -0.053
       red   -0.010   -0.053    0.079
@@ -95,20 +95,18 @@ class EmpiricalCovariance(SymmetricMatrix):
     The covariances are stored in a dictionary, meaning any one of them can be accessed as such:
 
     >>> cov["blue", "green"]
-    Cov: 0.020292
+    0.020291...
 
     Diagonal entries are variances:
 
     >>> cov["blue", "blue"]
-    Var: 0.076119
+    0.076119...
 
     """
 
-    _fmt = ",.3f"
-
     def __init__(self, ddof=1):
         self.ddof = ddof
-        self._w = 0
+        self._occ = {}
         self._loc = {}
         self._cov = {}
 
@@ -128,16 +126,16 @@ class EmpiricalCovariance(SymmetricMatrix):
 
         # dict -> numpy
         x_vec = np.array(list(x.values()))
-        loc, cov = self._get_loc_and_cov(variables=x.keys())
+        occ, loc, cov = self._get_stats(variables=x.keys())
 
         # update formulas
-        self._w += 1
         diff = x_vec - loc
-        loc += diff / self._w
-        cov += (np.outer(diff, x_vec - loc) - cov) / max(self._w - self.ddof, 1)
+        occ += 1
+        loc += diff / occ
+        cov += (np.outer(diff, x_vec - loc) - cov) / np.maximum(occ - self.ddof, 1)
 
         # numpy -> dict
-        self._set_loc_and_cov(x.keys(), loc, cov)
+        self._set_stats(x.keys(), occ, loc, cov)
 
         return self
 
@@ -153,18 +151,16 @@ class EmpiricalCovariance(SymmetricMatrix):
 
         # dict -> numpy
         x_vec = np.array(list(x.values()))
-        loc, cov = self._get_loc_and_cov(variables=x.keys())
+        occ, loc, cov = self._get_stats(variables=x.keys())
 
         # update formulas
-        self._w -= 1
-        if self._w < 0:
-            raise ValueError("Cannot go below 0")
         diff = x_vec - loc
-        loc -= diff / self._w
-        cov -= (np.outer(diff, x_vec - loc) - cov) / max(self._w - self.ddof, 1)
+        occ -= 1
+        loc -= diff / occ
+        cov -= (np.outer(diff, x_vec - loc) - cov) / np.maximum(occ - self.ddof, 1)
 
         # numpy -> dict
-        self._set_loc_and_cov(x.keys(), loc, cov)
+        self._set_stats(x.keys(), occ, loc, cov)
 
         return self
 
@@ -180,41 +176,42 @@ class EmpiricalCovariance(SymmetricMatrix):
 
         # dict -> numpy
         X_vec = X.values
-        loc, cov = self._get_loc_and_cov(variables=X.columns)
+        occ, loc, cov = self._get_stats(variables=X.columns)
 
         # update formulas
-        n = self._w
-        m = len(X_vec)
-        self._w += m
         diff = X_vec - loc
-        loc = n * loc + m * np.mean(X_vec, axis=0)
-        cov += ((diff.T @ (X_vec - loc)) - cov) / max(self._w - self.ddof, 1)
+        m = len(X_vec)
+        occ += m
+        loc = (occ - m) / occ * loc + m / occ * X_vec.mean(axis=0)
+        cov += (diff.T @ (X_vec - loc) - m * cov) / np.maximum(occ - self.ddof, 1)
 
         # numpy -> dict
-        self._set_loc_and_cov(X.columns, loc, cov)
+        self._set_stats(X.columns, occ, loc, cov)
 
         return self
 
-    def _get_loc_and_cov(self, variables):
+    def _get_stats(self, variables):
         """
 
         Loads means and covariances stored as dictionaries into numpy arrays.
 
         """
+        occ = np.array([self._occ.get(feature, 0.) for feature in variables])
         loc = np.array([self._loc.get(feature, 0.) for feature in variables])
         cov = np.array([
             [self._cov.get((i, j), 0.) for j in variables]
             for i in variables
         ])
-        return loc, cov
+        return occ, loc, cov
 
-    def _set_loc_and_cov(self, variables, loc, cov):
+    def _set_stats(self, variables, occ, loc, cov):
         """
 
         Takes numpy arrays and stores them as dictionaries with features as keys.
 
         """
         for i, fi in enumerate(variables):
+            self._occ[fi] = occ[i]
             self._loc[fi] = loc[i]
             row = cov[i]
             for j, fj in enumerate(variables):
@@ -222,21 +219,57 @@ class EmpiricalCovariance(SymmetricMatrix):
 
 
 
-def _inplace_sherman_morrison(B, u, v):
+def _sherman_morrison_inplace(A, u, v):
     """
 
     From https://timvieira.github.io/blog/post/2021/03/25/fast-rank-one-updates-to-matrix-inverse/
 
     """
-    Bu = B @ u
-    alpha = -1 / (1 + v.T @ Bu)
-    sp.linalg.blas.dger(alpha, Bu, v.T @ B, a=B, overwrite_a=1)
+    Au = A @ u
+    alpha = -1 / (1 + v.T @ Au)
+    sp.linalg.blas.dger(alpha, Au, v.T @ A, a=A, overwrite_a=1)
 
 
 class EmpiricalPrecision(SymmetricMatrix):
     """Empirical precision matrix.
 
     The precision matrix is the inverse of the covariance matrix.
+
+    Parameters
+    ----------
+    ddof
+        Delta Degrees of Freedom.
+
+    Examples
+    --------
+
+    >>> import numpy as np
+    >>> import pandas as pd
+    >>> from river import covariance
+
+    >>> np.random.seed(42)
+    >>> X = pd.DataFrame(np.random.random((8, 3)), columns=["red", "green", "blue"])
+    >>> X
+            red     green      blue
+    0  0.374540  0.950714  0.731994
+    1  0.598658  0.156019  0.155995
+    2  0.058084  0.866176  0.601115
+    3  0.708073  0.020584  0.969910
+    4  0.832443  0.212339  0.181825
+    5  0.183405  0.304242  0.524756
+    6  0.431945  0.291229  0.611853
+    7  0.139494  0.292145  0.366362
+
+    >>> prec = covariance.EmpiricalPrecision()
+    >>> for x in X.to_dict(orient="records"):
+    ...     prec = prec.update(x)
+    >>> prec
+            blue     green    red
+     blue    5.262   -0.387   0.147
+    green   -0.387    4.732   1.119
+      red    0.147    1.119   5.428
+
+    There is also an `update_many` method to process mini-batches. The results are identical.
 
     References
     ----------
@@ -272,13 +305,13 @@ class EmpiricalPrecision(SymmetricMatrix):
         inv_cov = np.array([
             [self._inv_cov.get((i, j), 1. if i == j else 0.) for j in x]
             for i in x
-        ], order='F')
+        ], order='F') / max(self._w, 1)
 
         # update formulas
         self._w += 1
         diff = x_vec - loc
         loc += diff / self._w
-        _inplace_sherman_morrison(inv_cov / max(self._w, 1), diff, x_vec - loc)
+        _sherman_morrison_inplace(A=inv_cov, u=diff, v=x_vec - loc)
 
         # numpy -> dict
         for i, fi in enumerate(x):
--- river/covariance/test_emp.py
+++ river/covariance/test_emp.py
@@ -3,42 +3,60 @@ import math
 import numpy as np
 import pandas as pd
 
-from river import misc
+import pytest
+from river import covariance, stream
+from sklearn import datasets
 
 
-def test_covariance_revert():
+@pytest.mark.parametrize(
+    "ddof",
+    [
+        pytest.param(
+            ddof,
+            id=f"{ddof=}",
+        )
+        for ddof in (0, 1, 2)
+    ],
+)
+def test_covariance_revert(ddof):
 
+    X = np.random.random((100, 5))
     X1 = X[:len(X) // 2]
     X2 = X[len(X) // 2:]
 
-    C1 = covariance.EmpiricalCovariance()
+    C1 = covariance.EmpiricalCovariance(ddof=ddof)
     for x, _ in stream.iter_array(X1):
         C1.update(x)
 
-    C2 = covariance.EmpiricalCovariance()
+    C2 = covariance.EmpiricalCovariance(ddof=ddof)
     for x, _ in stream.iter_array(X):
         C2.update(x)
     for x, _ in stream.iter_array(X2):
         C2.revert(x)
 
-    np.testing.assert_array_almost_equal(C1, C2)
+    for k in C1._cov:
+        assert math.isclose(C1._cov[k], C2._cov[k])
 
 
-def test_cov_matrix():
+def test_covariance_update_many():
 
     # NOTE: this test only works with ddof=1 because pandas ignores it if there are missing values
     ddof = 1
-
-    cov = misc.CovMatrix(ddof=ddof)
+    cov = covariance.EmpiricalCovariance(ddof=ddof)
     p = 5
     X_all = pd.DataFrame(columns=range(p))
 
     for _ in range(5):
         n = np.random.randint(1, 31)
-        X = pd.DataFrame(np.random.random((n, p))).sample(3, axis="columns")
+        X = pd.DataFrame(np.random.random((n, p))).sample(4, axis="columns")
+        print(X.columns)
+        print(n)
+
+        print(_)
         cov.update_many(X)
+
         X_all = pd.concat((X_all, X)).astype(float)
         pd_cov = X_all.cov(ddof=ddof)
 
-        for i, j in cov._covs:
-            assert math.isclose(cov[i, j].get(), pd_cov.loc[i, j])
+        for i, j in cov._cov:
+            assert math.isclose(cov[i, j], pd_cov.loc[i, j])
