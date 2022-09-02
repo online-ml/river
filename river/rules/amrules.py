4fa45cf158e71ad22114a5bf6c3d0cc8c7f8bdd1
Saulo Martiello Mastelini <mastelini@usp.br>
2022-09-02 10:42:23 +0100

    rules

--- river/rules/amrules.py
+++ river/rules/amrules.py
@@ -312,6 +312,10 @@ class AMRules(base.Regressor):
         self._rules: typing.Dict[typing.Hashable, RegRule] = {}
 
         self._n_drifts_detected: int = 0
+    
+    @property
+    def _mutable_attributes(self):
+        return {"n_min", "delta", "tau", "alpha", "anomaly_threshold", "m_min", "ordered_rule_set"}
 
     def __len__(self):
         return len(self._rules) + 1
