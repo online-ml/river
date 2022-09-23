ebaf76f04db239bdbd29f7bb9fed296a8159491b
Dennis1989 <assenmacher.dennis@gmail.com>
2022-09-23 11:37:31 +0200

    fix style issues and implementing single-linkage clustering

--- river/cluster/__init__.py
+++ river/cluster/__init__.py
@@ -4,6 +4,6 @@ from .dbstream import DBSTREAM
 from .denstream import DenStream
 from .k_means import KMeans
 from .streamkmeans import STREAMKMeans
-from .textclust import textclust
+from .textclust import TextClust
 
-__all__ = ["CluStream", "DBSTREAM", "DenStream", "KMeans", "STREAMKMeans", "textclust"]
+__all__ = ["CluStream", "DBSTREAM", "DenStream", "KMeans", "STREAMKMeans", "TextClust"]
