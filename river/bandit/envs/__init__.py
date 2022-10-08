b8b2bc228635ee677a447828d5d27b9ed27799a5
Max Halford <maxhalford25@gmail.com>
2022-10-08 21:45:23 +0200

    Update __init__.py

--- river/bandit/envs/__init__.py
+++ river/bandit/envs/__init__.py
@@ -1,14 +1,19 @@
-import gym
+try:
+    import gym
+    GYM_INSTALLED = True
+except ImportError:
+    GYM_INSTALLED = False
 
-from .candy_cane import CandyCaneContest
+if GYM_INSTALLED:
+    from .candy_cane import CandyCaneContest
 
-__all__ = ["CandyCaneContest"]
+    __all__ = ["CandyCaneContest"]
 
-RIVER_NAMESPACE = "river_bandits"
+    RIVER_NAMESPACE = "river_bandits"
 
-if (env_id := f"{RIVER_NAMESPACE}/CandyCaneContest-v0") not in gym.envs.registry:
-    gym.envs.registration.register(
-        id=env_id,
-        entry_point="river.bandit.envs:CandyCaneContest",
-        max_episode_steps=CandyCaneContest.n_rounds,
-    )
+    if (env_id := f"{RIVER_NAMESPACE}/CandyCaneContest-v0") not in gym.envs.registry:
+        gym.envs.registration.register(
+            id=env_id,
+            entry_point="river.bandit.envs:CandyCaneContest",
+            max_episode_steps=CandyCaneContest.n_rounds,
+        )
