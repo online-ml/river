59f5129d1b8af354b285cb79306299b6d301311a
alexandra <alexandraamani1102@gmail.com>
2023-01-06 22:17:04 +0100

    modify/init_base.basetransformer

--- river/base/transformer.py
+++ river/base/transformer.py
@@ -10,7 +10,7 @@ if typing.TYPE_CHECKING:
 class BaseTransformer:
     def __init__(self, output_dims):
         self.output_dims = output_dims
-        
+
     def __add__(self, other):
         """Fuses with another Transformer into a TransformerUnion."""
         from river import compose
