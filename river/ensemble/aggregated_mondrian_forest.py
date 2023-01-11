73ccce63e617b99d932810a6cf16e24edae7eb54
Saulo Martiello Mastelini <mastelini@usp.br>
2023-01-11 15:43:58 -0300

    more style suggestions

--- river/ensemble/aggregated_mondrian_forest.py
+++ river/ensemble/aggregated_mondrian_forest.py
@@ -1,13 +1,11 @@
-from abc import ABC
+import random
 
 from river import base
-from river.base.classifier import Classifier
-from river.tree.mondrian.mondrian_tree_classifier import MondrianTree, MondrianTreeClassifier
+from river.tree.mondrian import MondrianTreeClassifier
 
 
-class AMFLearner(ABC):
-    """
-    Base class for Aggregated Mondrian Forest classifier and regressors for online learning.
+class AMFLearner(base.Ensemble):
+    """Base class for Aggregated Mondrian Forest classifier and regressors for online learning.
 
     Parameters
     ----------
@@ -25,7 +23,8 @@ class AMFLearner(ABC):
         split ("pure" nodes). Default is `False`, namely pure nodes are not split,
         but `True` can be sometimes better.
     seed
-        Random seed for reproducibility
+        Random seed for reproducibility.
+
     Note
     ----
     This class is not intended for end users but for development only.
@@ -40,9 +39,7 @@ class AMFLearner(ABC):
         split_pure: bool = False,
         seed: int = None,
     ):
-
-        # This is yet to be defined by the dataset since we need to know about the amount of features namely
-        self._forest: list[MondrianTree] = []
+        super().__init__([])  # type: ignore
         self._n_features: int = 0
 
         self.n_estimators = n_estimators
@@ -52,39 +49,39 @@ class AMFLearner(ABC):
         self.split_pure = split_pure
         self.seed = seed
 
-    def is_trained(self) -> bool:
-        """
-        Says whether the model has been trained at least once before.
-        Returns
-        -------
-        trained: bool
-        """
-        return len(self._forest) != 0
+        self._rng = random.Random(self.seed)
 
-    def _check_features_consistency(self, x):
-        """
-        Makes sure that the features are consistent and set it to the first encountered value is no standard is set.
+    def is_trained(self) -> bool:
+        """Indicate whether the model has been trained at least once before."""
+
+        return len(self) != 0
+
+    def _check_features_consistency(self, x: dict):
+        """Make sure that the features are consistent and set it to the first encountered
+        value there is no standard is set.
+
         Parameters
         ----------
         x
-            Feature vector
-
-        Returns
-        -------
+            Feature dictionary.
 
         """
-        n_features = len(list(x.keys()))
+        n_features = len(x)
+
         # First case corresponds to a situation for which the number of features has never been set (0)
         if self._n_features == 0:
             self._n_features = n_features
+
+        # TODO: ideally we want to get rid of this restriction. Features should be allowed to
+        # change in number during learning (at least some might be missing)
+
         # The features have already been set, we make sure they keep being consistent
         elif self._n_features != n_features:
             raise Exception("Number of features must be consistent during learning")
 
 
-class AMFClassifier(AMFLearner, Classifier):
-    """
-    Aggregated Mondrian Forest classifier for online learning. This algorithm
+class AMFClassifier(AMFLearner, base.Classifier):
+    """Aggregated Mondrian Forest classifier for online learning. This algorithm
     is truly online, in the sense that a single pass is performed, and that predictions
     can be produced anytime.
 
@@ -93,7 +90,7 @@ class AMFClassifier(AMFLearner, Classifier):
     with parameter `dirichlet`. For each class with `count` labels in the
     node and `n_samples` samples in it, the prediction of a node is given by
 
-        $\frac{count + dirichlet}{n_{samples} + dirichlet \times n_{classes}}$
+    $\\frac{count + dirichlet}{n_{samples} + dirichlet \\times n_{classes}}$.
 
     The prediction for a sample is computed as the aggregated predictions of all the
     subtrees along the path leading to the leaf node containing the sample. The
@@ -101,10 +98,10 @@ class AMFClassifier(AMFLearner, Classifier):
     when `use_aggregation` is `True`.
 
     This computation is performed exactly thanks to a context tree weighting algorithm.
-    More details can be found in the paper cited in references below.
+    More details can be found in the paper cited in the references below.
 
     The final predictions are the average class probabilities predicted by each of the
-    ``n_estimators`` trees in the forest.
+    `n_estimators` trees in the forest.
 
     Parameters
     ----------
@@ -121,22 +118,22 @@ class AMFClassifier(AMFLearner, Classifier):
         leave it as `True`.
     dirichlet
         Regularization level of the class frequencies used for predictions in each
-        node. Default is dirichlet=0.5 for n_classes=2 and dirichlet=0.01 otherwise.
+        node. Default is dirichlet=0.5 for binary problems and dirichlet=0.01 otherwise.
     split_pure
         Controls if nodes that contains only sample of the same class should be
         split ("pure" nodes). Default is `False`, namely pure nodes are not split,
         but `True` can be sometimes better.
     seed
-        Random seed for reproducibility
+        Random seed for reproducibility.
 
-    Note
-    ----
+    Notes
+    -----
     Only log_loss used for the computation of the aggregation weights is supported for now, namely the log-loss
     for multi-class classification.
 
     References
     ----------
-    J. Mourtada, S. Gaiffas and E. Scornet, *AMF: Aggregated Mondrian Forests for Online Learning*, arXiv:1906.10529, 2019
+    J. Mourtada, S. Gaiffas and E. Scornet, *AMF: Aggregated Mondrian Forests for Online Learning*, arXiv:1906.10529, 2019.
 
     """
 
@@ -172,25 +169,22 @@ class AMFClassifier(AMFLearner, Classifier):
         self._classes: dict[base.typing.ClfTarget, int] = {}
 
     def _initialize_trees(self):
-        """
-        Initialize the forest
-        """
+        """Initialize the forest."""
 
         # If the number of features is 0, it means we don't know the number of features
         if self._n_features == 0:
-            raise Exception(
+            raise RuntimeError(
                 "You can't initialize the forest without knowning the number of features of the problem. "
                 "Please learn a data point first."
             )
 
         self.iteration = 0
-        self._forest: list[MondrianTreeClassifier] = []
+        self.data: list[MondrianTreeClassifier] = []
         for i in range(self.n_estimators):
-            seed = self.seed
             # We don't want to have the same stochastic scheme for each tree, or it'll break the randomness
             # Hence we introduce a new seed for each, that is derived of the given seed by a deterministic process
-            if seed is not None:
-                seed += i
+            seed = self._rng.randint(0, 9999999)
+
             tree = MondrianTreeClassifier(
                 self.n_classes,
                 self._n_features,
@@ -201,24 +195,9 @@ class AMFClassifier(AMFLearner, Classifier):
                 self.iteration,
                 seed,
             )
-            self._forest.append(tree)
+            self.data.append(tree)
 
     def learn_one(self, x, y):
-        """
-        Learns the sample (x, y)
-
-        Parameters
-        ----------
-        x
-            Feature vector of the sample.
-        y
-            Label of the sample.
-
-        Returns
-        -------
-        AMFClassifier
-        """
-
         # Updating the previously seen classes with the new sample
         if y not in self._classes:
             self._classes[y] = len(self._classes)
@@ -229,25 +208,19 @@ class AMFClassifier(AMFLearner, Classifier):
         # Checking if the forest has been created
         if not self.is_trained():
             self._initialize_trees()
+
         # we fit all the trees using the new sample
-        for tree in self._forest:
+        for tree in self:
             tree.learn_one(x, y)
+
         self.iteration += 1
+
         return self
 
     def predict_proba_one(self, x):
-        """
-        Predicts the probability of each class for the sample x
-
-        Parameters
-        ----------
-        x
-            Feature vector
-        """
-
         # Checking that the model has been trained once at least
         if not self.is_trained():
-            raise Exception(
+            raise RuntimeError(
                 "No sample has been learnt yet. You need to train your model before making predictions."
             )
 
@@ -255,12 +228,10 @@ class AMFClassifier(AMFLearner, Classifier):
         classes_name = list(self._classes.keys())
 
         # initialize the scores
-        scores = {}
-        for j in range(self.n_classes):
-            scores[classes_name[j]] = 0
+        scores = {classes_name[j]: 0 for j in range(self.n_classes)}
 
         # Simply computes the prediction for each tree and average it
-        for tree in self._forest:
+        for tree in self:
             tree.use_aggregation = self.use_aggregation
             predictions = tree.predict_proba_one(x)
             for j in range(self.n_classes):
