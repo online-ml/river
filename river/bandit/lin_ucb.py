f580b52c653f8e44a5d2c2cd790e66d2f53f0262
Max Halford <maxhalford25@gmail.com>
2023-04-09 16:18:22 -0300

    wip

---
+++ river/bandit/lin_ucb.py
@@ -0,0 +1,9 @@
+class LinUCB:
+    """
+
+    References
+    ----------
+    [^1]: [A Contextual-Bandit Approach to Personalized News Article Recommendation](http://rob.schapire.net/papers/www10.pdf)
+    [^2]: [Weighted Linear Bandits for Non-Stationary Environments](https://proceedings.neurips.cc/paper_files/paper/2019/file/263fc48aae39f219b4c71d9d4bb4aed2-Paper.pdf)
+
+    """
