488fe4b35685173010d0f6f8c919e71067b16c41
kchardon <katiachardon@outlook.fr>
2023-04-11 14:08:33 +0200

    fixed isort and black

--- river/cluster/__init__.py
+++ river/cluster/__init__.py
@@ -5,10 +5,18 @@ from __future__ import annotations
 from .clustream import CluStream
 from .dbstream import DBSTREAM
 from .denstream import DenStream
+from .hcluster import HierarchicalClustering
 from .k_means import KMeans
 from .odac import ODAC
 from .streamkmeans import STREAMKMeans
 from .textclust import TextClust
-from .hcluster import HierarchicalClustering
 
-__all__ = ["CluStream", "DBSTREAM", "DenStream", "KMeans", "STREAMKMeans", "TextClust", "HierarchicalClustering"]
+__all__ = [
+    "CluStream",
+    "DBSTREAM",
+    "DenStream",
+    "KMeans",
+    "STREAMKMeans",
+    "TextClust",
+    "HierarchicalClustering",
+]
