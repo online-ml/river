636486b96effda7ddc1d1e786872a4ae0eab13e1
Max Halford <maxhalford25@gmail.com>
2023-05-24 09:53:08 +0200

    Update test_product.py

--- river/compose/test_product.py
+++ river/compose/test_product.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import pandas as pd
 
-from river import compose, datasets, stream, preprocessing
+from river import compose, datasets, preprocessing, stream
 
 
 def test_issue_1238():
