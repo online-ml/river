4b9a07e886352fbd9de0bf894e59d463815f4e31
Jooles2001 <jules.soria@gmail.com>
2023-07-19 16:27:47 +0200

    added docstring for Apple dataset

--- river/changepoints/datasets/apple.py
+++ river/changepoints/datasets/apple.py
@@ -6,33 +6,26 @@ from .base import ChangePointDataset
 
 
 class Apple(ChangePointDataset):
+    """Apple Stock
+
+    This dataset concerns the daily close price and volume of Apple stock around the year 2000. The dataset is sampled every 3 observations to reduce the length of the time series.
+    This dataset is retrieved from Yahoo Finance.
+    
+    References
+    ----------
+    [^1]: https://finance.yahoo.com/quote/AAPL/history?period1=850348800&period2=1084579200&interval=1d&filter=history&frequency=1d
+       
+    """
 
     def __init__(self):
         super().__init__(
             annotations={
-		"6": [
-			319
-		],
-		"7": [
-			319
-		],
-		"8": [
-			319
-		],
-		"9": [
-			53,
-			90,
-			197,
-			276,
-			319,
-			403,
-			463,
-			535
-		],
-		"10": [
-			319
-		]
-	},
+                "6": [319],
+                "7": [319],
+                "8": [319],
+                "9": [53, 90, 197, 276, 319, 403, 463, 535],
+                "10": [319],
+            },
             filename="apple.csv",
             task=base.REG,
             n_samples=1867,
@@ -43,7 +36,7 @@ class Apple(ChangePointDataset):
     def __iter__(self):
         return stream.iter_csv(
             self._path,  # TODO: Must be changed for integration into river
-            target=["Open","High","Low","Close","Adj Close","Volume"],
+            target=["Open", "High", "Low", "Close", "Adj Close", "Volume"],
             converters={
                 "Open": float,
                 "High": float,
