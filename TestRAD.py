14da4c424b66718545e1a6cabb60da408009ba48
sebastianwette <sebastian.wette@telekom.de>
2023-11-08 12:09:47 +0000

    changed class to base off of supervised AD

--- TestRAD.py
+++ TestRAD.py
@@ -1,3 +1,6 @@
 from river import anomaly
 
 model = anomaly.ReconstructionAnomalyDetecion()
+
+print(type(model.learn_one({"x": 0}, 1)))
+print(type(model.score_one({"x": 0}, 1)))
--- river/anomaly/rad.py
+++ river/anomaly/rad.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
-from river import anomaly
+from river import anomaly, base
 
 __all__ = ["ReconstructionAnomalyDetecion"]
 
 
-class ReconstructionAnomalyDetecion(anomaly.base.AnomalyDetector):
+class ReconstructionAnomalyDetecion(anomaly.base.SupervisedAnomalyDetector):
     """Reconstruction Anomaly-Detection (RAD).
     This is the place for documentation
     """
@@ -13,8 +13,35 @@ class ReconstructionAnomalyDetecion(anomaly.base.AnomalyDetector):
     def __init__(self):
         print("Success")
 
-    def learn_one(self, x):
+    def learn_one(self, x: dict, y: base.typing.Target):
+        """Update the model.
+
+        Parameters
+        ----------
+        x
+            A dictionary of features.
+
+        Returns
+        -------
+        self
+
+        """
         return self
 
-    def score_one(self, x):
-        return 2
+    def score_one(self, x: dict, y: base.typing.Target):
+        """Return an outlier score.
+
+        A high score is indicative of an anomaly. A low score corresponds a normal observation.
+
+        Parameters
+        ----------
+        x
+            A dictionary of features.
+
+        Returns
+        -------
+        An anomaly score. A high score is indicative of an anomaly. A low score corresponds a
+        normal observation.
+
+        """
+        return 1.0
