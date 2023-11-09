35318a6fdedb1a1c4549a32c9a1de4e4b3f158f6
sebastianwette <sebastian.wette@telekom.de>
2023-11-09 09:59:50 +0000

    tested and fixed for ARIMA, added warmup

--- river/anomaly/rad.py
+++ river/anomaly/rad.py
@@ -16,7 +16,8 @@ class ReconstructionAnomalyDetecion(anomaly.base.SupervisedAnomalyDetector):
         self,
         reconstruction: base.Estimator | None = None,
         horizon: int | None = None,
-        n_std: int | None = None,
+        n_std: float | None = None,
+        warmup_period: int | None = None,
     ):
         self.reconstruction = (
             reconstruction
@@ -25,20 +26,31 @@ class ReconstructionAnomalyDetecion(anomaly.base.SupervisedAnomalyDetector):
         )
 
         self.n_std = n_std if n_std is not None else 3
-
         self.horizon = horizon if horizon is not None else 1
+        self.warmup_period = warmup_period if warmup_period is not None else 0
 
         self.dynamic_mean_squared_error = stats.Mean()
         self.dynamic_squared_error_variance = stats.Var()
         self.predictions: list[float] = []
         self.squared_errors: list[float] = []
         self.thresholds: list[float] = []
+        self.iterations: int = 0
+        self.warmed_up: bool = self.iterations >= self.warmup_period
 
-    def learn_one(self, x: dict, y: base.typing.Target):
-        self.reconstruction.learn_one(x, y)
+    def learn_one(self, x: dict | None, y: float):
+        self.iterations += 1
+        self.warmed_up = self.iterations >= self.warmup_period
+
+        if x is None:
+            self.reconstruction.learn_one(y)
+        else:
+            self.reconstruction.learn_one(x, y)
         return self
 
     def score_one(self, x: dict, y: base.typing.Target):
+        self.iterations += 1
+        self.warmed_up = self.iterations >= self.warmup_period
+
         if isinstance(self.reconstruction, time_series.base.Forecaster):
             y_pred = self.reconstruction.forecast(self.horizon)[0]
         else:
@@ -57,7 +69,8 @@ class ReconstructionAnomalyDetecion(anomaly.base.SupervisedAnomalyDetector):
         self.dynamic_mean_squared_error.update(squared_error)
         self.dynamic_squared_error_variance.update(squared_error)
 
-        self.reconstruction.learn_one(x, y)
+        if not self.warmed_up:
+            return 0.0
 
         if squared_error >= threshold:
             return 1.0
