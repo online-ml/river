8b72d5ad69907ebb0a0bcbc4e173cd19fd4d9a93
Max Halford <maxhalford25@gmail.com>
2023-12-04 18:35:16 +0100

    Update base.py

--- river/metrics/multioutput/base.py
+++ river/metrics/multioutput/base.py
@@ -38,7 +38,7 @@ class MultiOutputClassificationMetric(MultiOutputMetric):
         y_pred: dict[str | int, base.typing.ClfTarget]
         | dict[str | int, dict[base.typing.ClfTarget, float]],
         w=1.0,
-    ) -> MultiOutputClassificationMetric:
+    ):
         """Update the metric."""
         self.cm.update(y_true, y_pred, w)
 
@@ -48,7 +48,7 @@ class MultiOutputClassificationMetric(MultiOutputMetric):
         y_pred: dict[str | int, base.typing.ClfTarget]
         | dict[str | int, dict[base.typing.ClfTarget, float]],
         w=1.0,
-    ) -> MultiOutputClassificationMetric:
+    ):
         """Revert the metric."""
         self.cm.revert(y_true, y_pred, w)
 
@@ -72,7 +72,7 @@ class MultiOutputRegressionMetric(Metric):
         self,
         y_true: dict[str | int, float | int],
         y_pred: dict[str | int, float | int],
-    ) -> MultiOutputRegressionMetric:
+    ):
         """Update the metric."""
 
     @abc.abstractmethod
@@ -80,7 +80,7 @@ class MultiOutputRegressionMetric(Metric):
         self,
         y_true: dict[str | int, float | int],
         y_pred: dict[str | int, float | int],
-    ) -> MultiOutputRegressionMetric:
+    ):
         """Revert the metric."""
 
     def works_with(self, model) -> bool:
