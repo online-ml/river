742aaf1436e3f996a60c15ba7b1733916d66464f
marekwadinger <marekwadinger5506@gmail.com>
2023-12-08 09:04:46 +0100

    FIX: broken statefulness of update_many

--- river/covariance/emp.py
+++ river/covariance/emp.py
@@ -183,12 +183,57 @@ class EmpiricalCovariance(SymmetricMatrix):
             )
         }
 
-        self = self._from_state(n=n, mean=mean, cov=cov, ddof=self.ddof)
+        self._update_from_state(n=n, mean=mean, cov=cov, ddof=self.ddof)
 
-        return self
+    def _update_from_state(self, n: int, mean: dict, cov: float | dict):
+        """Update from state information.
+
+        Parameters
+        ----------
+        n
+            The number of data points.
+        mean
+            A dictionary of variable means.
+        cov
+            A dictionary of covariance or variance values.
+        ddof
+            Degrees of freedom for covariance calculation. Defaults to 1.
+
+        Raises
+        ----------
+            KeyError: If an element in `mean` or `cov` is missing.
+        """
+        for i, j in itertools.combinations(mean.keys(), r=2):
+            try:
+                self[i, j]
+            except KeyError:
+                self._cov[i, j] = stats.Cov(self.ddof)
+                if isinstance(cov, dict):
+                    cov_ = cov.get((i, j), cov.get((j, i)))
+                else:
+                    cov_ = cov
+                self._cov[i, j] += stats.Cov._from_state(
+                    n=n,
+                    mean_x=mean[i],
+                    mean_y=mean[j],
+                    cov=cov_,
+                    ddof=self.ddof,
+                )
+
+        for i in mean.keys():
+            try:
+                self[i, i]
+            except KeyError:
+                self._cov[i, i] = stats.Var(self.ddof)
+            if isinstance(cov, dict):
+                if isinstance(cov, dict):
+                    cov_ = cov[i, i]
+                else:
+                    cov_ = cov
+            self._cov[i, i] += stats.Var._from_state(n=n, m=mean[i], sig=cov_, ddof=self.ddof)
 
     @classmethod
-    def _from_state(cls, n: int, mean: dict, cov: int | dict, *, ddof=1):
+    def _from_state(cls, n: int, mean: dict, cov: float | dict, *, ddof=1):
         """Create a new instance from state information.
 
         Parameters
@@ -207,40 +252,9 @@ class EmpiricalCovariance(SymmetricMatrix):
         Returns
         ----------
             cls: A new instance of the class with updated covariance matrix.
-
-        Raises
-        ----------
-            KeyError: If an element in `mean` or `cov` is missing.
         """
         new = cls(ddof=ddof)
-        for i, j in itertools.combinations(mean.keys(), r=2):
-            try:
-                new[i, j]
-            except KeyError:
-                new._cov[i, j] = stats.Cov(new.ddof)
-                if isinstance(cov, dict):
-                    cov_ = cov.get((i, j), cov.get((j, i)))
-                else:
-                    cov_ = cov
-            new._cov[i, j] += stats.Cov._from_state(
-                n=n,
-                mean_x=mean[i],
-                mean_y=mean[j],
-                cov=cov_,
-                ddof=new.ddof,
-            )
-
-        for i in mean.keys():
-            try:
-                new[i, i]
-            except KeyError:
-                new._cov[i, i] = stats.Var(new.ddof)
-            if isinstance(cov, dict):
-                if isinstance(cov, dict):
-                    cov_ = cov[i, i]
-                else:
-                    cov_ = cov
-            new._cov[i, i] += stats.Var._from_state(n=n, m=mean[i], sig=cov_, ddof=new.ddof)
+        new._update_from_state(n=n, mean=mean, cov=cov)
         return new
 
 
