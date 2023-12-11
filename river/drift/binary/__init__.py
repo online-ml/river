530ad13acfe40a83b9f456d4dd598ea69438698b
gabrieljaguiar <gja.gbrl@gmail.com>
2023-12-11 16:11:56 -0500

    fhddm

--- river/drift/binary/__init__.py
+++ river/drift/binary/__init__.py
@@ -5,5 +5,6 @@ from .ddm import DDM
 from .eddm import EDDM
 from .hddm_a import HDDM_A
 from .hddm_w import HDDM_W
+from .fhddm import FHDDM
 
-__all__ = ["DDM", "EDDM", "HDDM_A", "HDDM_W"]
+__all__ = ["DDM", "EDDM", "HDDM_A", "HDDM_W", "FHDDM"]
--- river/drift/binary/fhddm.py
+++ river/drift/binary/fhddm.py
@@ -43,16 +43,14 @@ class FHDDM(base.BinaryDriftAndWarningDetector):
     >>> # of 1's and 0's
     >>> data_stream = rng.choices([0, 1], k=1000)
     >>> # Increase the probability of 1's appearing in the next 1000 instances
-    >>> data_stream = data_stream + rng.choices([0, 1], k=1000, weights=[0.3, 0.7])
-
-    >>> print_warning = True
+    >>> data_stream = data_stream + rng.choices([0, 1], k=1000, weights=[0.7, 0.3])
     >>> # Update drift detector and verify if change is detected
     >>> for i, x in enumerate(data_stream):
     ...     fhddm.update(x)
     ...     if fhddm.drift_detected:
     ...         print(f"Change detected at index {i}")
-    ...         print_warning = True
-    Change detected at index 1334
+    ...         exit(0)
+    Change detected at index 1057
 
     References
     ----------
@@ -83,19 +81,23 @@ class FHDDM(base.BinaryDriftAndWarningDetector):
         self.u_max = 0
         self.n_one = 0
 
-    def update(self, x: bool):
+    def update(self, x):
         if self.drift_detected:
             self._reset()
-
+        
+        #print (self.epsilon)   
+        #print (self.sliding_window)
+        #print (self.pointer)
         if self.pointer < self.sliding_window_size:
-            self.sliding_window[self.pointer] = 1 if (x == 0) else 0
-            self.n_one += self.win[self.pointer]
+            self.sliding_window[self.pointer] = x
+            self.n_one += self.sliding_window[self.pointer]
             self.pointer += 1
         else:
-            self.n_one -= self.win.pop(0)
-            self.win.append(1 if x == 0 else 0)
+            self.n_one -= self.sliding_window.pop(0)
+            self.sliding_window.append(x)
+            self.n_one += x
 
         if self.pointer == self.sliding_window_size:
             u = self.n_one / self.sliding_window_size
-            u_max = u if (self.u_max < u) else u_max
+            self.u_max = u if (self.u_max < u) else self.u_max
             self._drift_detected = True if (self.u_max - u > self.epsilon) else False
