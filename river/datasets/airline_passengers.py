790b3e6e3731a23ea4983313d7a588b026d2e58d
Max Halford <maxhalford25@gmail.com>
2024-01-20 11:03:22 +0100

    Update airline_passengers.py

--- river/datasets/airline_passengers.py
+++ river/datasets/airline_passengers.py
@@ -14,7 +14,7 @@ class AirlinePassengers(base.FileDataset):
 
     References
     ----------
-    [^1]: [International airline passengers: monthly totals in thousands. Jan 49 – Dec 60](https://datamarket.com/data/set/22u3/international-airline-passengers-monthly-totals-in-thousands-jan-49-dec-60#!ds=22u3&display=line)
+    [^1]: [International airline passengers: monthly totals in thousands. Jan 49 – Dec 60](https://rdrr.io/r/datasets/AirPassengers.html)
 
     """
 
