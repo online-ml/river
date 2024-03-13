173d8d5163a2901eaa8cff0853eefd04ad6f84bf
marekwadinger <marekwadinger5506@gmail.com>
2024-03-13 14:41:45 +0900

    UPDATE: align logic of Hankel with Unsupervised transformer -> 1. learn 2. transform

--- river/preprocessing/hankel.py
+++ river/preprocessing/hankel.py
@@ -20,30 +20,31 @@ class Hankelizer(Transformer):
 
     Examples:
     >>> h = Hankelizer(w=3)
+    >>> h.learn_one({"a": 1, "b": 2})
     >>> h.transform_one({"a": 1, "b": 2})
     {'a_0': 1, 'b_0': 2, 'a_1': 1, 'b_1': 2, 'a_2': 1, 'b_2': 2}
 
     >>> h = Hankelizer(w=3, return_partial=False)
+    >>> h.learn_one({"a": 1, "b": 2})
     >>> h.transform_one({"a": 1, "b": 2})
     Traceback (most recent call last):
         ...
     ValueError: The window is not full yet. Set `return_partial` to True ...
 
     >>> h = Hankelizer(w=3, return_partial=True)
+    >>> h.learn_one({"a": 1, "b": 2})
     >>> h.transform_one({"a": 1, "b": 2})
     {'a_0': nan, 'b_0': nan, 'a_1': nan, 'b_1': nan, 'a_2': 1, 'b_2': 2}
 
-    Transformation is stateless so we lost previous data.
-    >>> h.transform_one({"a": 3, "b": 4})
-    {'a_0': nan, 'b_0': nan, 'a_1': nan, 'b_1': nan, 'a_2': 3, 'b_2': 4}
+    Actually, transform_one does not care about the data as the learn should precede.
+    >>> h.learn_one({"a": 3, "b": 4})
+    >>> h.transform_one({"a": 5, "b": 6})
+    {'a_0': nan, 'b_0': nan, 'a_1': 1, 'b_1': 2, 'a_2': 3, 'b_2': 4}
     >>> h._window
-    deque([], maxlen=2)
-    >>> h.learn_one({"a": 1, "b": 2})
+    deque([{'a': 1, 'b': 2}, {'a': 3, 'b': 4}], maxlen=3)
 
     Transform and learn in one go.
-    >>> h.learn_transform_one({"a": 3, "b": 4})
-    {'a_0': nan, 'b_0': nan, 'a_1': 1, 'b_1': 2, 'a_2': 3, 'b_2': 4}
-    >>> h.transform_one({"a": 5, "b": 6})
+    >>> h.learn_transform_one({"a": 5, "b": 6})
     {'a_0': 1, 'b_0': 2, 'a_1': 3, 'b_1': 4, 'a_2': 5, 'b_2': 6}
     """
 
@@ -53,7 +54,7 @@ class Hankelizer(Transformer):
         self.w = w
         self.return_partial = return_partial
 
-        self._window: deque = deque(maxlen=self.w - 1)
+        self._window: deque = deque(maxlen=self.w)
         self.feature_names_in_: list[str]
         self.n_features_in_: int
 
@@ -61,11 +62,13 @@ class Hankelizer(Transformer):
         if not hasattr(self, "feature_names_in_"):
             self.feature_names_in_ = list(x.keys())
             self.n_features_in_ = len(x)
+        else:
+            assert self.feature_names_in_ == list(x.keys())
 
         self._window.append(x)
 
-    def transform_one(self, x: dict):
-        _window = list(self._window) + [x]
+    def transform_one(self, _: dict):
+        _window = list(self._window)
         w_past_current = len(_window)
         if not self.return_partial and w_past_current < self.w:
             raise ValueError(
@@ -84,6 +87,6 @@ class Hankelizer(Transformer):
             }
 
     def learn_transform_one(self, x: dict):
-        y = self.transform_one(x)
         self.learn_one(x)
+        y = self.transform_one(x)
         return y
