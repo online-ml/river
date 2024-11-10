0c745a23ca8cb19be7ffe8a06912bce214498fcb
kchardon <katiachardon@outlook.fr>
2024-11-10 19:58:25 +0100

    ruff format

--- river/cluster/__init__.py
+++ river/cluster/__init__.py
@@ -7,7 +7,6 @@ from .dbstream import DBSTREAM
 from .denstream import DenStream
 from .hcluster import HierarchicalClustering
 from .k_means import KMeans
-from .odac import ODAC
 from .streamkmeans import STREAMKMeans
 from .textclust import TextClust
 
