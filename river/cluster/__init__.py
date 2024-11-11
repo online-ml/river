9662a7f2c793dac7acca0faff3f9b1532eb4a37c
kchardon <katiachardon@outlook.fr>
2024-11-11 17:59:33 +0100

    remove hcluster

--- river/cluster/__init__.py
+++ river/cluster/__init__.py
@@ -5,7 +5,6 @@ from __future__ import annotations
 from .clustream import CluStream
 from .dbstream import DBSTREAM
 from .denstream import DenStream
-from .hcluster import HierarchicalClustering
 from .k_means import KMeans
 from .odac import ODAC
 from .streamkmeans import STREAMKMeans
@@ -15,7 +14,6 @@ __all__ = [
     "CluStream",
     "DBSTREAM",
     "DenStream",
-    "HierarchicalClustering",
     "KMeans",
     "ODAC",
     "STREAMKMeans",
