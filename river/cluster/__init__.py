10cd5e53d0562c3f3eb664395a852ef49429a725
kchardon <katiachardon@outlook.fr>
2024-11-11 21:28:40 +0100

    hcluster

--- river/cluster/__init__.py
+++ river/cluster/__init__.py
@@ -5,9 +5,10 @@ from __future__ import annotations
 from .clustream import CluStream
 from .dbstream import DBSTREAM
 from .denstream import DenStream
+from .hcluster import HierarchicalClustering
 from .k_means import KMeans
 from .odac import ODAC
 from .streamkmeans import STREAMKMeans
 from .textclust import TextClust
 
-__all__ = ["CluStream", "DBSTREAM", "DenStream", "KMeans", "ODAC", "STREAMKMeans", "TextClust"]
+__all__ = ["CluStream", "DBSTREAM", "DenStream", "HierarchicalClustering", "KMeans", "ODAC", "STREAMKMeans", "TextClust"]
