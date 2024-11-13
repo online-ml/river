02187321b4316178941de4c9e2ea97d2d35dd8e4
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-11-14 04:46:39 +0530

    Clean up setup script slightly

--- build.py
+++ build.py
@@ -1,36 +1,23 @@
 import platform
-from distutils.command.build_ext import build_ext
-from distutils.errors import CCompilerError, DistutilsExecError, DistutilsPlatformError
+
+import numpy
 import setuptools
+from Cython.Build import cythonize
+from setuptools.command.build_ext import build_ext
+from setuptools.errors import CCompilerError
 from setuptools_rust import Binding, RustExtension
-
-try:
-    from numpy import __version__ as numpy_version
-    from numpy import get_include
-except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "numpy"])
-    from numpy import __version__ as numpy_version
-    from numpy import get_include
-
-try:
-    from Cython.Build import cythonize
-except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "Cython"])
-    from Cython.Build import cythonize  # type: ignore
-
 
 ext_modules = cythonize(
     module_list=[
         setuptools.Extension(
             "*",
-            sources=["**/*.pyx"],
-            include_dirs=[get_include()],
+            sources=["river/**/*.pyx"],
+            include_dirs=[numpy.get_include()],
             libraries=[] if platform.system() == "Windows" else ["m"],
             define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
         )
     ],
     compiler_directives={
-        "language_level": 3,
         "binding": True,
         "embedsignature": True,
     },
@@ -47,13 +34,13 @@ class ExtBuilder(build_ext):
     def run(self):
         try:
             build_ext.run(self)
-        except (DistutilsPlatformError, FileNotFoundError):
+        except (FileNotFoundError):
             raise BuildFailed("File not found. Could not compile C extension.")
 
     def build_extension(self, ext):
         try:
             build_ext.build_extension(self, ext)
-        except (CCompilerError, DistutilsExecError, DistutilsPlatformError, ValueError):
+        except (CCompilerError, ValueError):
             raise BuildFailed("Could not compile C extension.")
 
 
