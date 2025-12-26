299a98876bd44d110ff90a85131bd34c9c285653
nnoya <nnoya@fing.edu.uy>
2025-12-26 23:50:34 +0100

    improve code compliance

--- river/anomaly/__init__.py
+++ river/anomaly/__init__.py
@@ -19,10 +19,10 @@ from .filter import QuantileFilter, ThresholdFilter
 from .gaussian import GaussianScorer
 from .hst import HalfSpaceTrees
 from .lof import LocalOutlierFactor
+from .memstream import MemStream, MemStreamAutoencoder, MemStreamPCA
 from .pad import PredictiveAnomalyDetection
 from .sad import StandardAbsoluteDeviation
 from .svm import OneClassSVM
-from .memstream import MemStream, MemStreamAutoencoder, MemStreamPCA
 
 __all__ = [
     "base",
