56d271edd0dcb70ed4ba4a6a117d477ba759b795
Loïc Vital <mugulmotion@gmail.com>
2026-08-05 17:24:53 +0200

    drop unused

--- river/anomaly/base.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from river.base.anomaly_detector import AnomalyDetector, AnomalyFilter, SupervisedAnomalyDetector
-
-__all__ = ["AnomalyDetector", "SupervisedAnomalyDetector", "AnomalyFilter"]
